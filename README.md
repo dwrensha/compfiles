ac280c718a62713f0d1dc86d4fdd7c2035f1bbb0
David Renshaw <dwrenshaw@gmail.com>
2022-12-06 16:21:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Math puzzles formalized in Lean 4,
+using the Lean 3 repo https://github.com/dwrensha/math-puzzles-in-lean
+as a starting point.
---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+
+open Lake DSL
+
+package mathpuzzles
+
+@[default_target]
+lean_lib MathPuzzles
+
+require Mathlib from git "https://github.com/leanprover-community/mathlib4" @ "21bd5f5a735cad66ac1366494b1e4a8f81b8d91d"
+
