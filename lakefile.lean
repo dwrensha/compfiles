ca15dacff4be22bfacdc1d530b685865efc91e3f
David Renshaw <dwrenshaw@gmail.com>
2022-12-29 13:13:30 -0500

    bump mathlib version

--- lakefile.lean
+++ lakefile.lean
@@ -7,5 +7,5 @@ package mathpuzzles
 @[default_target]
 lean_lib MathPuzzles
 
-require Mathlib from git "https://github.com/leanprover-community/mathlib4" @ "4c70fde2d2d8e14a4269c2a5b4d16090c16ca571"
+require Mathlib from git "https://github.com/leanprover-community/mathlib4" @ "882cac36b0f8c3a420b424fd433db972602a3812"
 
