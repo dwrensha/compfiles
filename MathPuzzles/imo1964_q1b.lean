35b27d831e4e06049e0e5d921eb1efa63097984a
David Renshaw <dwrenshaw@gmail.com>
2023-01-24 17:52:24 -0500

    move q1b to its own file

---
+++ MathPuzzles/imo1964_q1b.lean
@@ -0,0 +1,11 @@
+import Mathlib.Data.Nat.Basic
+
+/-!
+# International Mathematical Olympiad 1964, Problem 1b
+
+Prove that there is no positive integer n for which 2ⁿ + 1 is divisible by 7.
+-/
+
+theorem imo_1964_q1b (n : ℕ) : ¬ 7 ∣ (2^n + 1) := by
+  sorry
+
