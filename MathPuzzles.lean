5e1b4ffafa1fd5ea91c3ac7a74b365423bf52820
David Renshaw <dwrenshaw@gmail.com>
2023-01-29 15:13:27 -0500

    start on porting Imo2013Q5

--- MathPuzzles.lean
+++ MathPuzzles.lean
@@ -5,5 +5,6 @@ import MathPuzzles.Imo1964Q1B
 import MathPuzzles.Imo1964Q4
 import MathPuzzles.Imo2011Q3
 import MathPuzzles.Imo2013Q1
+import MathPuzzles.Imo2013Q5
 import MathPuzzles.Imo2018Q3
 import MathPuzzles.Russia1998Q42
