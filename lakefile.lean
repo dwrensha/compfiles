0a83f225c3786d1446b5de6024221e5d4948497f
David Renshaw <dwrenshaw@gmail.com>
2023-02-20 18:18:27 -0500

    Mathlib -> mathlib, so that 'lake exe get cache' works

--- lakefile.lean
+++ lakefile.lean
@@ -7,5 +7,5 @@ package mathpuzzles
 @[default_target]
 lean_lib MathPuzzles
 
-require Mathlib from git "https://github.com/leanprover-community/mathlib4" @ "7e974fd3806866272e9f6d9e44fa04c210a21f87"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "7e974fd3806866272e9f6d9e44fa04c210a21f87"
 
