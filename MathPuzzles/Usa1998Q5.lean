53e39533c6d0949b1ba62450ba5cf9c011346c59
David Renshaw <dwrenshaw@gmail.com>
2023-02-24 11:32:13 -0500

    [Usa1998Q5] add informal proof

--- MathPuzzles/Usa1998Q5.lean
+++ MathPuzzles/Usa1998Q5.lean
@@ -13,4 +13,19 @@ namespace Usa1998Q5
 
 theorem usa1998_q5 (n : ℕ) (hn : 2 ≤ n) :
     ∃ S: Set ℤ, ∀ a b : ℤ, a ∈ S → b ∈ S → a ≠ b → (a - b)^2 ∣ a * b := by
+  -- (Informal proof from Andreescu & Feng.)
+  -- We will prove by induction on n that we can find such a set Sₙ all
+  -- of whose elements are nonnegative.
+  -- For n = 2, we may take S₂ = {0,1}
+  -- Now suppose that for some n ≥ 2, the desired set Sₙ of n nonegative
+  -- integers exists. Let L be the least common multiple of those numbers
+  -- (a-b)² and ab that are nonzero, with (a,b) ranging over pairs of
+  -- distinct elements from Sₙ. Define
+  --   Sₙ₊₁ = { L + a : a ∈ Sₙ } ∪ { 0 }.
+  -- Then Sₙ₊₁ consists of n + 1 nonnegative integers, since L > 0.
+  -- If α,β ∈ Sₙ₊₁ and either α or β is zero, then (α - β)² divides α\B.
+  -- If L + a, L + b ∈ Sₙ₊₁, with a,b distinct elements of Sₙ, then
+  --   (L + a)(L + b) ≡ ab ≡ 0 (mod (a - b)²),
+  -- so [(L + a) - (L + b)]² divides (L + a)(L + b), completing the inductive
+  -- step.
   sorry
