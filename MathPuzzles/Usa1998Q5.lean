f79693080e1a43c3d9f82098ebfd26c28abadaed
David Renshaw <dwrenshaw@gmail.com>
2023-03-01 11:11:38 -0500

    [Usa1998Q5] finish proof

--- MathPuzzles/Usa1998Q5.lean
+++ MathPuzzles/Usa1998Q5.lean
@@ -23,85 +23,66 @@ lemma usa1998_q5_stronger (n : ℕ) :
        S.card = n ∧
        ∀ a ∈ S, ∀ b ∈ S, a ≠ b → (a - b)^2 ∣ a * b := by
   -- (Adapation of informal proof from Andreescu & Feng.)
-  -- We will prove by induction on n that we can find such a set Sₙ all
-  -- of whose elements are nonnegative.
   induction n with
   | zero => use ∅; simp
   | succ n ih =>
     obtain ⟨Sp, sp_nonnegative, sp_card, hsp⟩ := ih
-    -- Now suppose that for some n, the desired set Sₙ of n nonegative
-    -- integers exists. Let L be the product of those numbers
-    -- (a-b)² and ab that are nonzero, with (a,b) ranging over pairs of
-    -- distinct elements from Sₙ.
-
-    let L : ℤ := ∏ s in Sp.erase 0,
-                   ∏ t in (Sp.erase 0).erase s, s*t*(s-t)^2
+    let L : ℤ := ∏ s in Sp, ∏ t in Sp.erase s, (s-t)^2
 
     have all_terms_pos :
-      ∀ s ∈ Sp.erase 0,
-        ∀ t ∈ (Sp.erase 0).erase s, 0 < s*t*(s-t)^2 := by
-          intros s hs t ht
-          obtain ⟨s_ne_zero, h3⟩ := Finset.mem_erase.mp hs
-          have hsn := sp_nonnegative s h3
-          obtain ⟨t_ne_s, h4'⟩ := (Finset.mem_erase.mp ht)
-          obtain ⟨t_ne_zero, h4⟩ := Finset.mem_erase.mp h4'
-          have htn := sp_nonnegative t h4
-          have hsz : 0 < s := Ne.lt_of_le s_ne_zero.symm hsn
-          have htz : 0 < t := Ne.lt_of_le t_ne_zero.symm htn
+      ∀ s ∈ Sp, ∀ t ∈ Sp.erase s, 0 < (s-t)^2 := by
+          intros s _hs t ht
+          obtain ⟨t_ne_s, _⟩ := Finset.mem_erase.mp ht
           have : s - t ≠ 0 := sub_ne_zero_of_ne t_ne_s.symm
           positivity
 
     have inner_prod_pos :
-      ∀ s ∈ Sp.erase 0,
-        0 < ∏ t in (Sp.erase 0).erase s, s*t*(s-t)^2 := by
-          intros s hs
-          exact Finset.prod_pos (all_terms_pos s hs)
+      ∀ s ∈ Sp, 0 < ∏ t in Sp.erase s, (s-t)^2 := by
+        intros s hs
+        exact Finset.prod_pos (all_terms_pos s hs)
 
     have L_pos : 0 < L := Finset.prod_pos inner_prod_pos
-    have L_nonneg : 0 ≤ L := le_of_lt L_pos
 
-    -- Define
-    --   Sₙ₊₁ = { L + a : a ∈ Sₙ } ∪ { 0 }.
-
+    -- Define Sₙ₊₁ = { L + a : a ∈ Sₙ } ∪ { 0 }.
     let S : Finset ℤ := Sp.map (addLeftEmbedding L) ∪ {0}
     use S
 
-    -- Then Sₙ₊₁ consists of n + 1 nonnegative integers, since L > 0.
     constructor
-    · intros x hx
+    · -- all elements are nonnegative
+      intros x hx
       rw[Finset.mem_union] at hx
       cases hx with
-      | inl hx => sorry/-
+      | inl hx =>
         rw[Finset.mem_map] at hx
         obtain ⟨w, hw1, hw2⟩ := hx
         have := sp_nonnegative w hw1
         replace hw2 : L + w = x := hw2
-        linarith-/
+        linarith
       | inr hx => simp_all
     · constructor
-      · have hdisj : Disjoint (Finset.map (addLeftEmbedding L) Sp) {0} := by
-             sorry/-
-             intros X hX h0 y hy
-             have hyy := h0 hy
-             rw[Finset.mem_singleton] at hyy
-             rw[hyy] at hy
-             have hxx := hX hy
-             rw[Finset.mem_map] at hxx
-             obtain ⟨z, hz, hz2⟩ := hxx
-             replace hz2 : L + z = 0 := hz2
-             have := sp_nonnegative z hz
-             clear hX h0 -- Finset ≤ relation confuses linarith
-             linarith-/
+      · --cardinality is n + 1
+        have hdisj : Disjoint (Finset.map (addLeftEmbedding L) Sp) {0} := by
+          intros X hX h0 y hy
+          have hyy := h0 hy
+          rw[Finset.mem_singleton] at hyy
+          rw[hyy] at hy
+          have hxx := hX hy
+          rw[Finset.mem_map] at hxx
+          obtain ⟨z, hz, hz2⟩ := hxx
+          replace hz2 : L + z = 0 := hz2
+          have := sp_nonnegative z hz
+          clear hX h0 -- Finset ≤ relation confuses linarith
+          linarith
         rw[Finset.card_disjoint_union hdisj, Finset.card_singleton,
            Finset.card_map, sp_card]
       · intros α hα β hβ α_ne_β
         rw[Finset.mem_union, Finset.mem_map] at hα hβ
         -- If α,β ∈ Sₙ₊₁ and either α or β is zero, then (α - β)² divides αβ.
         cases hα with
-        | inr hα => sorry --simp_all
+        | inr hα => simp_all
         | inl hα =>
           cases hβ with
-          | inr hβ => sorry --simp_all
+          | inr hβ => simp_all
           | inl hβ =>
             obtain ⟨a, ha, ha2⟩ := hα
             replace ha2 : L + a = α := ha2
@@ -109,13 +90,29 @@ lemma usa1998_q5_stronger (n : ℕ) :
             replace hb2 : L + b = β := hb2
             have a_ne_b : a ≠ b := by aesop
             have ih := hsp a ha b hb a_ne_b
-            sorry
-
-    -- If L + a, L + b ∈ Sₙ₊₁, with a,b distinct elements of Sₙ, then
-    --   (L + a)(L + b) ≡ ab ≡ 0 (mod (a - b)²),
-    -- so [(L + a) - (L + b)]² divides (L + a)(L + b), completing the inductive
-    -- step.
-
+            have h5 : L = (∏ t in Sp.erase a, (a-t)^2) *
+                         ∏ s in Sp.erase a, ∏ t in Sp.erase s, (s-t)^2 :=
+              (Finset.mul_prod_erase Sp _ ha).symm
+            have hbb := Finset.mem_erase.mpr ⟨a_ne_b.symm, hb⟩
+            have h6 : (a-b)^2 * ∏ t in (Sp.erase a).erase b, (a-t)^2 =
+                      ∏ t in Sp.erase a, (a-t)^2 :=
+               Finset.mul_prod_erase (Sp.erase a) (fun x ↦ (a-x)^2) hbb
+            have Lmod : L % (a - b)^2 = 0 := by
+                 rw[h5, ←h6, mul_assoc, Int.mul_emod, Int.emod_self]
+                 norm_num
+            have Lmod' : (L + a) * (L + b) % (a-b)^2 = 0 := by
+               have h7 : (L + a) * (L + b) = L * (L + a + b) + a * b := by ring
+               rw[h7]
+               rw[Int.add_emod]
+               have h8 : L * (L + a + b) % (a - b) ^ 2 = 0 := by
+                 rw[Int.mul_emod, Lmod]
+                 norm_num
+               have h9 :  a * b % (a - b) ^ 2 = 0 := Int.emod_eq_zero_of_dvd ih
+               rw[h8, h9]
+               norm_num
+            have hab : a-b = α - β := by linarith
+            rw[ha2, hb2, hab] at Lmod'
+            exact Int.dvd_of_emod_eq_zero Lmod'
 
 theorem usa1998_q5 (n : ℕ) (_hn : 2 ≤ n) :
     ∃ S : Finset ℤ,
