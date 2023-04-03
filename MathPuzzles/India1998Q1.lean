52d73f3c27620b5dca90bae7ecabaa3b5d296671
David Renshaw <dwrenshaw@gmail.com>
2023-04-03 18:48:23 -0400

    [India1998Q1] finish, except for proving that 7 is prime

--- MathPuzzles/India1998Q1.lean
+++ MathPuzzles/India1998Q1.lean
@@ -99,78 +99,64 @@ theorem india1998_q1b (n a b: ℤ) (hn : a^2 + 3 * b^2 = 7 * n) :
     have h22 : (7:ℤ) * 7 ≠ 0 := by norm_num
     exact (mul_right_inj' h22).mp h20
 
-  · sorry
+  · have h11: (2 * az + (-3) * bz) = 0 := by
+      rw [hen]
+      ring_nf
+      simp[show (7 : ZMod 7) = 0 by rfl]
 
-#exit
-
-  { have h11: (2 * az + (-3) * bz) = 0,
-    { rw [hen], ring_nf,
-      simp,
-      have h12: (7 : zmod 7) = 0 := rfl,
-      rw [h12], simp},
-
-    have h13: 7 ∣ (2 * a + (-3) * b),
-    { have h50 : (2 * az + (-3) * bz).val = (0 : zmod 7).val := congr_arg zmod.val h11,
-      have h51 : (2 * az + (-3) * bz).val = ((2 * az).val + ((-3) * bz).val) % 7 := zmod.val_add _ _,
-      have h52 : (2 * az).val = (2 : zmod 7).val * az.val % 7 := zmod.val_mul _ _,
-      have h53 : ((-3) * bz).val = (-3 : zmod 7).val * bz.val % 7 := zmod.val_mul _ _,
-      rw [h51, h52, h53] at h50,
-      simp at h50,
-      have h51 : (2 * az.val + 4 * bz.val) % 7 = 0 := h50,
-      have h52 : ((2 * (az.val:ℤ) + 4 * (bz.val:ℤ))) % 7 = 0,
-      { norm_cast,
-        exact h51},
-      rw [lemma1 a, lemma1 b] at h52,
-      rw [←int.mod_add_div a 7, ←int.mod_add_div b 7],
+    have h13: 7 ∣ (2 * a + (-3) * b) := by
+      have h50 : (2 * az + (-3) * bz).val = (0 : ZMod 7).val := congr_arg ZMod.val h11
+      have h51 : (2 * az + (-3) * bz).val = ((2 * az).val + ((-3) * bz).val) % 7 := ZMod.val_add _ _
+      have h52 : (2 * az).val = (2 : ZMod 7).val * az.val % 7 := ZMod.val_mul _ _
+      have h53 : ((-3) * bz).val = (-3 : ZMod 7).val * bz.val % 7 := ZMod.val_mul _ _
+      rw[h51, h52, h53] at h50
+      simp at h50
+      have h51 : (2 * az.val + 4 * bz.val) % 7 = 0 := h50
+      have h52 : ((2 * (az.val:ℤ) + 4 * (bz.val:ℤ))) % 7 = 0 := by norm_cast
+      rw [lemma1 a, lemma1 b] at h52
+      rw [←Int.emod_add_ediv a 7, ←Int.emod_add_ediv b 7]
       have h53 : 2 * (a % 7 + 7 * (a / 7)) + (-3) * (b % 7 + 7 * (b / 7)) =
-               2 * (a % 7) + 4 * (b % 7) + 7 * (2 * (a / 7) + (-3) * (b / 7) - (b % 7) ) := by ring,
-      rw [h53],
-      have h54 : 7 ∣ 7 * (2 * (a / 7) + (-3) * (b / 7) - b % 7) := dvd.intro _ rfl,
-      exact dvd_add (int.dvd_of_mod_eq_zero h52) h54 },
+               2 * (a % 7) + 4 * (b % 7) + 7 * (2 * (a / 7) + (-3) * (b / 7) - (b % 7) ) := by ring
+      rw [h53]
+      have h54 : 7 ∣ 7 * (2 * (a / 7) + (-3) * (b / 7) - b % 7) := Dvd.intro _ rfl
+      exact dvd_add (Int.dvd_of_emod_eq_zero h52) h54
 
-    have h14 : (∃ m1, 2 * a + (-3) * b = 7 * m1) := exists_eq_mul_right_of_dvd h13,
-    obtain ⟨m1, hm1⟩ := h14,
+    have h14 : (∃ m1, 2 * a + (-3) * b = 7 * m1) := exists_eq_mul_right_of_dvd h13
+    obtain ⟨m1, hm1⟩ := h14
 
-    have h15: (az + 2 * bz) = 0,
-    { rw [hen], ring_nf },
-    have h16: 7 ∣ (a + 2 * b),
-    { have h50 : (az + 2 * bz).val = (0 : zmod 7).val := congr_arg zmod.val h15,
-      have h51 : (az + 2 * bz).val = (az.val + (2 * bz).val) % 7 := zmod.val_add _ _,
-      have h53 : (2 * bz).val = (2 : zmod 7).val * bz.val % 7 := zmod.val_mul _ _,
-      rw [h51, h53] at h50,
-      simp at h50,
-      have h51 : (az.val + 2 * bz.val) % 7 = 0 := h50,
-      have h52 : (((az.val:ℤ) + 2 * (bz.val:ℤ))) % 7 = 0,
-      { norm_cast,
-        exact h51},
-      rw [lemma1 a, lemma1 b] at h52,
-      --rw [int.add_mod] at h52,
-      have h52' : 7 ∣ a % 7 + 2 * (b % 7) := int.dvd_of_mod_eq_zero h52,
-      rw [←int.mod_add_div a 7, ←int.mod_add_div b 7],
+    have h15: (az + 2 * bz) = 0 := by { rw [hen]; ring_nf }
+
+    have h16: 7 ∣ (a + 2 * b) := by
+      have h50 : (az + 2 * bz).val = (0 : ZMod 7).val := congr_arg ZMod.val h15
+      have h51 : (az + 2 * bz).val = (az.val + (2 * bz).val) % 7 := ZMod.val_add _ _
+      have h53 : (2 * bz).val = (2 : ZMod 7).val * bz.val % 7 := ZMod.val_mul _ _
+      rw [h51, h53] at h50
+      simp at h50
+      have h51 : (az.val + 2 * bz.val) % 7 = 0 := h50
+      have h52 : (((az.val:ℤ) + 2 * (bz.val:ℤ))) % 7 = 0 := by norm_cast
+      rw [lemma1 a, lemma1 b] at h52
+      have h52' : 7 ∣ a % 7 + 2 * (b % 7) := Int.dvd_of_emod_eq_zero h52
+      rw [←Int.emod_add_ediv a 7, ←Int.emod_add_ediv b 7]
       have h53 : a % 7 + 7 * (a / 7) + 2 * (b % 7 + 7 * (b / 7)) =
-              a % 7 + 2 * (b % 7) + 7 * (a / 7 + 2 * (b / 7)) := by ring,
-      rw [h53],
-      have h54 : 7 ∣ 7 * (a / 7 + 2 * (b / 7)) := dvd.intro _ rfl,
-      exact dvd_add h52' h54,
-    },
+              a % 7 + 2 * (b % 7) + 7 * (a / 7 + 2 * (b / 7)) := by ring
+      rw [h53]
+      have h54 : 7 ∣ 7 * (a / 7 + 2 * (b / 7)) := Dvd.intro _ rfl
+      exact dvd_add h52' h54
 
-    have h17 : (∃ m2, a + 2 * b = 7 * m2) := exists_eq_mul_right_of_dvd h16,
-    obtain ⟨m2, hm2⟩ := h17,
+    have h17 : (∃ m2, a + 2 * b = 7 * m2) := exists_eq_mul_right_of_dvd h16
+    obtain ⟨m2, hm2⟩ := h17
 
-    use m1,
-    use m2,
+    use m1; use m2
 
-    have h20 : (7 * m1) ^ 2 + 3 * (7 * m2) ^ 2 = 7 * 7 * n,
-    { rw [←hm1, ←hm2],
-      ring_nf,
-      have h18: 7 * (a ^ 2 + 3 * b ^ 2) = 7 * (7 * n) := congr_arg (has_mul.mul 7) hn,
-      ring_nf at h18,
-      rw [h18]},
+    have h20 : (7 * m1) ^ 2 + 3 * (7 * m2) ^ 2 = 7 * 7 * n := by
+      rw [←hm1, ←hm2]
+      ring_nf
+      have h18: 7 * (a ^ 2 + 3 * b ^ 2) = 7 * (7 * n) := congrArg (HMul.hMul 7) hn
+      ring_nf at h18
+      rw [h18]
 
-    have h21 : (7 * m1) ^ 2 + 3 * (7 * m2) ^ 2 = 7 * 7 * (m1 ^ 2 + 3 * m2 ^ 2) := by ring,
+    have h21 : (7 * m1) ^ 2 + 3 * (7 * m2) ^ 2 = 7 * 7 * (m1 ^ 2 + 3 * m2 ^ 2) := by ring
 
-    rw[h21] at h20,
-    have h22 : (7:ℤ) * 7 ≠ 0 := by norm_num,
-    exact (mul_right_inj' h22).mp h20 },
-end
-
+    rw[h21] at h20
+    have h22 : (7:ℤ) * 7 ≠ 0 := by norm_num
+    exact (mul_right_inj' h22).mp h20
