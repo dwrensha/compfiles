fa6d9d6c57d36feeca9aeaf52903ba9890585b6f
David Renshaw <dwrenshaw@gmail.com>
2023-04-09 12:47:11 -0400

    [Bulgaria1998Q6] lemma_1', in nats

--- MathPuzzles/Bulgaria1998Q6.lean
+++ MathPuzzles/Bulgaria1998Q6.lean
@@ -21,10 +21,21 @@ lemma lemma_0
     (h : a * x^2 + b * x + c = 0) :
     (∃ d : ℤ, d^2 = b^2 - 4 * a * c) := sorry
 
-#check Nat.lt_wfRel
-#check WellFounded.min
 #check WellFounded.min_mem
 #check WellFounded.not_lt_min
+
+lemma lemma_1'
+    (s t u : ℕ)
+    (hs : 0 < s)
+    (ht : 0 < t)
+    (hu : 0 < u)
+    (h : s^4 - t^4 = u^2) : False := by
+  let S := { a : ℕ | 0 < a ∧ ∃ b c : ℕ, a^4 - b^4 = c^2 }
+  have hns : S.Nonempty := ⟨s, hs, t, u, h⟩
+  let a := WellFounded.min Nat.lt_wfRel.wf S hns
+  have ha : a ∈ S := WellFounded.min_mem Nat.lt_wfRel.wf S hns
+  obtain ⟨hap, b, c, habc⟩ := ha
+  sorry
 
 lemma lemma_1
     (s t u : ℤ)
@@ -32,9 +43,6 @@ lemma lemma_1
     (ht : 0 < t)
     (hu : 0 < u)
     (h : s^4 - t^4 = u^2) : False := by
-  let S := { a : ℕ | 0 < a ∧ ∃ b c : ℕ, a^4 - b^4 = c^2 }
-  have hns : S.Nonempty := by sorry
-  have := WellFounded.min Nat.lt_wfRel.wf S hns
   sorry
 
 theorem bulgaria1998_q6
