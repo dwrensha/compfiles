975ce5de61e343ae0a240a342b2295c910af537d
Moritz Firsching <firsching@google.com>
2023-04-10 13:41:43 +0200

    add copyright header
    as suggested by @dwrensha

--- MathPuzzles/Imo2019Q1.lean
+++ MathPuzzles/Imo2019Q1.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2022 Moritz Firsching. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Moritz Firsching
+-/
 import Mathlib.Data.Int.Basic
 import Mathlib.Tactic
 
