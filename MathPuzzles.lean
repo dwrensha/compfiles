c776cef10f6b47c83a969dc30f1fca105c248d8b
David Renshaw <dwrenshaw@gmail.com>
2023-05-09 15:16:22 -0400

    add UpperLowerContinuous

--- MathPuzzles.lean
+++ MathPuzzles.lean
@@ -17,6 +17,7 @@ import MathPuzzles.Imo2018Q3
 import MathPuzzles.Imo2019Q1
 import MathPuzzles.India1998Q1
 import MathPuzzles.Russia1998Q42
+import MathPuzzles.UpperLowerContinuous
 import MathPuzzles.Usa1998Q5
 import MathPuzzles.Usa2022Q1
 import MathPuzzles.ZeroesOnesAndTwos
---
+++ MathPuzzles/UpperLowerContinuous.lean
@@ -0,0 +1,31 @@
+import Mathlib.Algebra.Ring.Equiv
+import Mathlib.Data.Real.Basic
+import Mathlib.Data.Set.Intervals.Basic
+import Mathlib.Topology.Order
+import Mathlib.Topology.MetricSpace.Basic
+
+/-
+Suppose f : ℝ -> ℝ is continuous in both the upper topology (where
+the basic open sets are half-open intervals (a, b]) and lower topology
+(where the basic open sets are half-open intervals [a,b)).
+Then f is continuous in the usual topology (where the basic open sets are
+open intervals (a,b)) and also monotone nondecreasing.
+-/
+
+namespace UpperLowerContinuous
+
+def ℝₗ : Type := ℝ
+def ℝᵤ : Type := ℝ
+
+instance : TopologicalSpace ℝₗ :=
+  TopologicalSpace.generateFrom {s : Set ℝₗ | ∃ a b : ℝ, Set.Ico a b = s}
+
+instance : TopologicalSpace ℝᵤ :=
+  TopologicalSpace.generateFrom {s : Set ℝₗ | ∃ a b : ℝ, Set.Ioc a b = s}
+
+theorem upper_lower_continuous
+    (f : ℝ → ℝ)
+    (hlc : @Continuous ℝₗ ℝₗ _ _ f)
+    (huc : @Continuous ℝᵤ ℝᵤ _ _ f)
+    : @Continuous ℝ ℝ _ _ f ∧ Monotone f := by
+  sorry
