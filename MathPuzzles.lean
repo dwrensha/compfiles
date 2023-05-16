b15625a93d255fabb8147b96086f09cae2c98609
David Renshaw <dwrenshaw@gmail.com>
2023-05-15 22:02:52 -0400

    [Poland1998Q4] start porting

--- MathPuzzles.lean
+++ MathPuzzles.lean
@@ -16,6 +16,7 @@ import MathPuzzles.Imo2013Q5
 import MathPuzzles.Imo2018Q3
 import MathPuzzles.Imo2019Q1
 import MathPuzzles.India1998Q1
+import MathPuzzles.Poland1998Q4
 import MathPuzzles.Russia1998Q42
 import MathPuzzles.UpperLowerContinuous
 import MathPuzzles.Usa1998Q5
