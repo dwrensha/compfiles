5b31c6f4d39d5e046199e6dc78a904783176bb6a
David Renshaw <dwrenshaw@gmail.com>
2023-05-25 09:30:14 -0400

    port Iran1998Q3

--- MathPuzzles.lean
+++ MathPuzzles.lean
@@ -16,6 +16,7 @@ import MathPuzzles.Imo2013Q5
 import MathPuzzles.Imo2018Q3
 import MathPuzzles.Imo2019Q1
 import MathPuzzles.India1998Q1
+import MathPuzzles.Iran1998Q3
 import MathPuzzles.Poland1998Q4
 import MathPuzzles.Romania1998Q12
 import MathPuzzles.Russia1998Q42
