61b413a9c1eca113465f8ae699ff60261daf3bd0
David Renshaw <dwrenshaw@gmail.com>
2023-06-16 21:00:28 -0400

    [KolmogorovStreams] finishing porting

--- MathPuzzles/KolmogorovStreams.lean
+++ MathPuzzles/KolmogorovStreams.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Renshaw
 -/
 
+import Mathlib.Algebra.BigOperators.Basic
 import Mathlib.Data.Stream.Defs
 import Mathlib.Data.Stream.Init
 import Mathlib.Tactic.Ring
@@ -26,7 +27,7 @@ Answer: yes.
 
 
 namespace KolmogorovStreams
-open Stream'
+open Stream' BigOperators
 
 variable {α : Type}
 
@@ -51,6 +52,32 @@ lemma break_into_words_cons
     (break_into_words (first_length::lengths) a).tail =
            break_into_words lengths (a.drop first_length) := by
   simp[break_into_words, corec, tail_map, tail_iterate]
+
+lemma break_into_words_closed_form
+    (lengths : Stream' ℕ)
+    (a : Stream' α)
+   : break_into_words lengths a =
+      (λ i ↦ Stream'.take (lengths i) (Stream'.drop (∑ j in Finset.range i, lengths j) a)) := by
+  funext n
+  convert_to ((Stream'.corec (λ x ↦ Stream'.take (x.fst.head) x.snd)
+                 (λ x ↦ ⟨x.fst.tail, Stream'.drop (x.fst.head) x.snd⟩)) :
+                  Stream' ℕ × Stream' α → Stream' (List α)) ⟨lengths, a⟩ n =
+             Stream'.take (lengths n) (Stream'.drop (∑ j in Finset.range n, lengths j) a)
+  rw [Stream'.corec_def,Stream'.map]
+  congr
+  · revert a lengths
+    induction' n with pn hpn
+    · intros a b; rfl
+    · intros a b
+      rw[Stream'.nth_succ, Stream'.iterate_eq, Stream'.tail_cons, hpn]
+      rfl
+  · revert a lengths
+    induction' n with pn hpn
+    · intros a b; rfl
+    · intros a b
+      rw [Stream'.nth_succ, Stream'.iterate_eq, Stream'.tail_cons, hpn,
+          drop_drop, Finset.sum_range_succ']
+      congr
 
 def all_same_class
     (is_decent : List α → Prop)
@@ -98,6 +125,94 @@ Stream'.corec (λ (acc: decent_accumulator a is_decent) ↦
                       (Classical.choose_spec (hnot acc.start)).2⟩)
              ⟨0, hinit⟩
 
+lemma chosen_decent_closed_form
+    (is_decent : List α → Prop)
+    (a : Stream' α)
+    (hinit: all_prefixes is_decent a)
+    (hnot: ∀ (n : ℕ), ∃ (k : ℕ), 0 < k ∧
+            all_prefixes is_decent (a.drop (n + k)))
+    : ∀ n : ℕ, (((choose_decent_words is_decent a hinit hnot).nth n).start =
+              ∑ j in Finset.range n, ((choose_decent_words is_decent a hinit hnot).nth j).length)
+            := by
+  intro n
+  induction' n with n pn
+  · rfl
+  · rw[Finset.sum_range_succ, ← pn]; rfl
+
+lemma check_decent_words
+    (is_decent : List α → Prop)
+    (a : Stream' α)
+    (hinit: all_prefixes is_decent a)
+    (hnot: ∀ (n : ℕ), ∃ (k : ℕ), 0 < k ∧
+            all_prefixes is_decent (a.drop (n + k)))
+    : Stream'.All
+      is_decent
+      (break_into_words
+          (λ i ↦ ((choose_decent_words is_decent a hinit hnot).nth i).length) a) := by
+  rw [break_into_words_closed_form]
+  simp_rw [←chosen_decent_closed_form]
+  intro j
+  exact ((choose_decent_words is_decent a hinit hnot).nth j).h
+
+structure indecent_word (a : Stream' α) (is_decent : List α → Prop) : Type :=
+  (start : ℕ)
+  (length : ℕ)
+  (nonempty : 0 < length)
+  (h : ¬is_decent ((a.drop start).take length))
+
+lemma not_all_prefixes
+    (is_decent : List α → Prop)
+    (a : Stream' α)
+    (h : ¬ all_prefixes is_decent a) :
+    ∃ n, ¬ is_decent (a.take (Nat.succ n)) := by
+  simp[all_prefixes, Stream'.all_def] at h
+  exact h
+
+/-
+ accumulator is: n, the number of symbols consumed so far
+-/
+noncomputable def choose_indecent_words
+    (is_decent : List α → Prop)
+    (a : Stream' α)
+    (h: ∀ (k : ℕ), ¬all_prefixes is_decent (a.drop k))
+     : Stream' (indecent_word a is_decent) :=
+Stream'.corec (λ n ↦ let hd := not_all_prefixes is_decent (a.drop n) (h n)
+                     let new_word_length := Nat.succ (Classical.choose hd)
+                     let hh := (Classical.choose_spec hd)
+                     ⟨n, new_word_length, Nat.succ_pos _, hh⟩
+              )
+              (λ n ↦ let hd := not_all_prefixes is_decent (a.drop n) (h n)
+                     let new_word_length := Nat.succ (Classical.choose hd)
+                     n + new_word_length)
+              0
+
+lemma chosen_indecent_closed_form
+    (is_decent : List α → Prop)
+    (a : Stream' α)
+    (h: ∀ (k : ℕ), ¬all_prefixes is_decent (a.drop k))
+    : ∀ n : ℕ, (((choose_indecent_words is_decent a h).nth n).start =
+                ∑ j in Finset.range n, ((choose_indecent_words is_decent a h).nth j).length)
+             := by
+  intro n
+  induction' n with n pn
+  · rfl
+  · rw[Finset.sum_range_succ, ← pn]
+    rfl
+
+lemma check_indecent_words
+    (is_decent : List α → Prop)
+    (a : Stream' α)
+    (h: ∀ (k : ℕ), ¬all_prefixes is_decent (a.drop k))
+    : Stream'.All
+      (λ x ↦ ¬ is_decent x)
+      (break_into_words
+          (λ i ↦ ((choose_indecent_words is_decent a h).nth i).length)
+          a) := by
+  rw [break_into_words_closed_form]
+  simp_rw [←chosen_indecent_closed_form]
+  intro j
+  exact ((choose_indecent_words is_decent a h).nth j).h
+
 theorem kolmogorov_streams
     (is_decent : List α → Prop)
     (a : Stream' α)
@@ -114,7 +229,17 @@ theorem kolmogorov_streams
       intro k
       have hnk := hn (k + 1) (Nat.succ_pos _)
       rwa [Stream'.drop_drop, Nat.add_left_comm]
-    sorry
+    let d := choose_indecent_words is_decent a' hn'
+    use n.succ::(λ i ↦ (d.nth i).length)
+    constructor
+    · intro i
+      cases' i with i
+      · exact Nat.succ_pos n
+      · exact (d.nth i).nonempty
+    · right
+      rw [break_into_words_cons]
+      exact check_indecent_words is_decent a' hn'
+
   · push_neg at hnot; push_neg at hnot -- Bug in push_neg?
     obtain ⟨k, hkp, hinit⟩ := hnot 0
     have hdka : a.drop (0 + k) = a.drop k := by { rw [←Stream'.drop_drop]; rfl }
@@ -139,4 +264,4 @@ theorem kolmogorov_streams
       · exact (d.nth i).nonempty
     · left
       rw [break_into_words_cons]
-      sorry
+      exact check_decent_words is_decent a' hinit hnot'
