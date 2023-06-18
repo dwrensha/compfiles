1d84f878fdd95761d008ec951fc78f9ad9ebfd42
David Renshaw <dwrenshaw@gmail.com>
2023-06-18 16:17:06 -0400

    [India1998Q1] even shorter

--- MathPuzzles/India1998Q1.lean
+++ MathPuzzles/India1998Q1.lean
@@ -7,7 +7,6 @@ import Mathlib.Tactic.LinearCombination
 import Mathlib.Tactic.Ring
 import Mathlib.Tactic.NormNum.Prime
 
-
 /-
 Indian Mathematical Olympiad 1998, problem 1
 
@@ -35,6 +34,7 @@ theorem india1998_q1b (n a b: ℤ) (hn : a^2 + 3 * b^2 = 7 * n) :
   rw [h2] at h1
   have h9 : az^2 = (2 * bz)^2 := by linear_combination h1
   have : Fact (Nat.Prime 7) := ⟨by norm_num⟩
+  have h22 : (7:ℤ) * 7 ≠ 0 := by norm_num
   obtain (hep : az = 2 * bz) | (hen : az = - (2 * bz)) := eq_or_eq_neg_of_sq_eq_sq _ _ h9
   · have h11 : 2 * az + 3 * bz = 0 := by rw[h2]; linear_combination 2 * hep
     have h13 : 7 ∣ (2 * a + 3 * b) := by
@@ -43,7 +43,7 @@ theorem india1998_q1b (n a b: ℤ) (hn : a^2 + 3 * b^2 = 7 * n) :
 
     obtain ⟨m1, hm1⟩ := exists_eq_mul_right_of_dvd h13
     have h15 : (az + (- 2) * bz) = 0 := by rw [hep]; ring_nf
-    have h16: 7 ∣ (a + (-2) * b) := by
+    have h16 : 7 ∣ (a + (-2) * b) := by
       have h50 : (((a + (-2) * b):ℤ) : ZMod 7) = 0 := by dsimp at h15; norm_cast at h15
       exact (ZMod.int_cast_zmod_eq_zero_iff_dvd _ 7).mp h50
     obtain ⟨m2, hm2⟩ := exists_eq_mul_right_of_dvd h16
@@ -51,13 +51,10 @@ theorem india1998_q1b (n a b: ℤ) (hn : a^2 + 3 * b^2 = 7 * n) :
     have h20 : (7 * m1) ^ 2 + 3 * (7 * m2) ^ 2 = 7 * 7 * n := by
       rw [←hm1, ←hm2]; linear_combination 7 * hn
 
-    have h21 : (7 * m1) ^ 2 + 3 * (7 * m2) ^ 2 = 7 * 7 * (m1 ^ 2 + 3 * m2 ^ 2) := by ring
-    rw[h21] at h20
-    have h22 : (7:ℤ) * 7 ≠ 0 := by norm_num
-    exact (mul_right_inj' h22).mp h20
+    exact (mul_right_inj' h22).mp (by linear_combination h20)
 
   · have h11 : 2 * az + (-3) * bz = 0 := by rw[h2]; linear_combination 2 * hen
-    have h13: 7 ∣ (2 * a + (-3) * b) := by
+    have h13 : 7 ∣ (2 * a + (-3) * b) := by
       have h50 : (((2 * a + (-3) * b):ℤ) : ZMod 7) = 0 := by
         dsimp at h11; norm_cast at h11
       exact (ZMod.int_cast_zmod_eq_zero_iff_dvd _ 7).mp h50
@@ -70,11 +67,7 @@ theorem india1998_q1b (n a b: ℤ) (hn : a^2 + 3 * b^2 = 7 * n) :
 
     obtain ⟨m2, hm2⟩ := exists_eq_mul_right_of_dvd h16
     use m1; use m2
-
     have h20 : (7 * m1) ^ 2 + 3 * (7 * m2) ^ 2 = 7 * 7 * n := by
       rw [←hm1, ←hm2]; linear_combination 7 * hn
 
-    have h21 : (7 * m1) ^ 2 + 3 * (7 * m2) ^ 2 = 7 * 7 * (m1 ^ 2 + 3 * m2 ^ 2) := by ring
-    rw[h21] at h20
-    have h22 : (7:ℤ) * 7 ≠ 0 := by norm_num
-    exact (mul_right_inj' h22).mp h20
+    exact (mul_right_inj' h22).mp (by linear_combination h20)
