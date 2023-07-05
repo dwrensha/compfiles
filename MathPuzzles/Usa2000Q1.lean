ec3faf1550f6cecc589a532f44766a48c6f16558
David Renshaw <dwrenshaw@gmail.com>
2023-07-05 13:28:02 -0400

    [Usa2000Q1] progress

--- MathPuzzles/Usa2000Q1.lean
+++ MathPuzzles/Usa2000Q1.lean
@@ -69,14 +69,38 @@ theorem usa2000_q1 :
     nth_rewrite 1 [h7] at h2
     exact h2
 
+  have h1' : ∀ n : ℕ,
+      f (-2 ^ (-((n:ℝ) + 1))) + 1 / 2^n ≤ (f 0 + f (-2 ^ (-(n:ℝ)))) / 2 := by
+    intro n
+    have h2 := hc 0 (-2 ^ (-(n:ℝ)))
+    have h2p : (0:ℝ) < 2 := by norm_num
+    have h3 : ((0:ℝ) + -(2:ℝ) ^ (-(n:ℝ))) / 2 = -2 ^ (-((n:ℝ) + 1)) := by
+      rw[zero_add]
+      have h4 : -((n : ℝ) + 1) = - (n : ℝ) - 1 := by ring
+      rw[h4, div_eq_mul_one_div]
+      have h5 : (1:ℝ) / 2 = 2 ^ (-(1:ℝ)) := by
+         rw[Real.rpow_neg (by norm_num)]; norm_num
+      rw[h5]
+      sorry--rw[←Real.rpow_add h2p]
+      --congr
+    rw[h3] at h2; clear h3
+    rw[←neg_eq_zero_sub, abs_neg] at h2
+    have h6 : -(2:ℝ) ^ (-(n:ℝ)) < 0 := sorry
+    rw[abs_of_neg h6, neg_neg] at h2
+    have h7 : (2:ℝ) ^ (- (n : ℝ)) = 1 / 2^(n:ℝ) := by
+      have h9 : (0:ℝ) ≤ (2:ℝ) := by norm_num
+      rw[Real.rpow_neg h9]
+      exact inv_eq_one_div _
+    nth_rewrite 1 [h7] at h2
+    exact h2
+
   -- A straightforward induction shows that f(2⁻ⁿ) ≤ (A - 2n) / 2ⁿ
   -- for all nonnegative integers n.
   have h2 : ∀ n : ℕ, f (2 ^ (- (n : ℝ))) ≤ (A - 2 * (n:ℝ)) / 2^(n:ℝ) := by
     intro n
     induction' n with n hpn
     · simp
-    · have h3 := h1 n
-      have h6 : 2 ^ (n.succ : ℝ) = 2 ^ (n : ℝ) * 2 := by norm_cast
+    · have h6 : 2 ^ (n.succ : ℝ) = 2 ^ (n : ℝ) * 2 := by norm_cast
       replace hpn : f (2 ^ (-(n:ℝ)))/2 ≤ (A - 2 * ↑n) / 2 ^ ↑n.succ := by
          rw[h6, div_mul_eq_div_div]; linarith
       have h4 : f (2 ^ (-↑(Nat.succ n)))
@@ -90,9 +114,31 @@ theorem usa2000_q1 :
       have h8 : A - 2 * ↑n - 2 = A - 2 * ↑n.succ := by
         rw[Nat.succ_eq_add_one, h8']; ring
 
-      calc _ ≤ _ := le_sub_iff_add_le.mpr h3
+      calc _ ≤ _ := le_sub_iff_add_le.mpr (h1 n)
            _ = _ := by rw[hf0, zero_add]
            _ ≤ (A - 2 * ↑n) / 2 ^ (n.succ:ℝ) - 1 / 2 ^ (n:ℝ) := sub_le_sub_right hpn _
+           _ ≤ _ := by rw[h7, ←sub_div, h8]
+
+  have h3 : ∀ n : ℕ, f (- 2 ^ (- (n : ℝ))) ≤ (B - 2 * (n:ℝ)) / 2^(n:ℝ) := by
+    intro n
+    induction' n with n hpn
+    · simp
+    · have h6 : 2 ^ (n.succ : ℝ) = 2 ^ (n : ℝ) * 2 := by norm_cast
+      replace hpn : f (-2 ^ (-(n:ℝ)))/2 ≤ (B - 2 * ↑n) / 2 ^ ↑n.succ := by
+         rw[h6, div_mul_eq_div_div]; linarith
+      have h4 : f (-2 ^ (-↑(Nat.succ n)))
+            = f (-2 ^ (-(↑n + 1))) := by congr; norm_cast
+      rw[h4]
+      have h2ne0 : (2: ℝ) ≠ 0 := by norm_num
+      have h7 : (1:ℝ) / 2 ^ (n:ℝ) = 2 / 2 ^ (n.succ:ℝ) := by
+        rw[h6, div_mul_left h2ne0]
+
+      have h8' : (n.succ : ℝ) = (n:ℝ) + 1 := by norm_cast
+      have h8 : B - 2 * ↑n - 2 = B - 2 * ↑n.succ := by
+        rw[Nat.succ_eq_add_one, h8']; ring
+      calc _ ≤ _ := le_sub_iff_add_le.mpr (h1' n)
+           _ = _ := by rw[hf0, zero_add]
+           _ ≤ (B - 2 * ↑n) / 2 ^ (n.succ:ℝ) - 1 / 2 ^ (n:ℝ) := sub_le_sub_right hpn _
            _ ≤ _ := by rw[h7, ←sub_div, h8]
 
   -- Using a similar line of reasoning as above, f(-2⁻ⁿ) ≤ (B - 2n)/2ⁿ.
