907bdc3e77969f2f023c8bfa7e1b325c9d834634
David Renshaw <dwrenshaw@gmail.com>
2023-07-25 19:16:47 -0400

    [Usa1998Q3] write down part of the informal proof

--- MathPuzzles/Usa1998Q3.lean
+++ MathPuzzles/Usa1998Q3.lean
@@ -7,7 +7,6 @@ Authors: David Renshaw
 import Mathlib.Data.Real.Basic
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Arctan
 
-
 /-!
 # USA Mathematical Olympiad 1998, Problem 3
 
@@ -30,4 +29,17 @@ theorem usa1998_q3
     (ha : ∀ i, i < n + 1 → a n ∈ Set.Ioo 0 (Real.pi / 2))
     (hs : n - 1 ≤ ∑ i in Finset.range (n + 1), Real.tan (a i - (Real.pi / 4)))
     : Real.rpow n (n + 1) ≤ ∏ i in Finset.range (n + 1), Real.tan (a i) := by
+  -- informal solution from artofproblemsolving.com
+  --
+  -- Let yᵢ = tan(aᵢ - π/4), where 0 ≤ i ≤ n.
+  -- Then we have
+  --  y₀ + y₁ + ... + yₙ ≥ n - 1
+  --  1 + yᵢ ≥ ∑_{j ≠ i} (1 - yⱼ)
+  --  (1 + yᵢ)/n ≥ (1/n) ∑_{j ≠ i} (1 - yⱼ)
+  --
+  -- Then, by AM-GM,
+  -- (1/n) ∑_{j ≠ i} (1 - yⱼ) ≥ ∏_{j ≠ i} (1 - yⱼ)^{1/n}
+  -- (1 + yᵢ)/n ≥ ∏_{j ≠ i} (1 - yⱼ)^{1/n}
+  -- ∏ᵢ(1 + yᵢ)/n ≥ ∏ᵢ∏_{j ≠ i} (1 - yⱼ)^{1/n}
+  -- ... a bunch more steps...
   sorry
