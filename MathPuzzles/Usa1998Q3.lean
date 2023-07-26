ae7963a8bf012069970dfa727a23079f75f5031f
David Renshaw <dwrenshaw@gmail.com>
2023-07-25 21:30:06 -0400

    [Usa1998Q3] progress

--- MathPuzzles/Usa1998Q3.lean
+++ MathPuzzles/Usa1998Q3.lean
@@ -6,6 +6,7 @@ Authors: David Renshaw
 
 import Mathlib.Data.Real.Basic
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Arctan
+import Mathlib.Analysis.MeanInequalities
 
 /-!
 # USA Mathematical Olympiad 1998, Problem 3
@@ -23,23 +24,82 @@ Prove that
 namespace Usa1998Q3
 open BigOperators
 
+#check Real.tan_nonneg_of_nonneg_of_le_pi_div_two
+
 theorem usa1998_q3
     (n : ℕ)
     (a : ℕ → ℝ)
-    (ha : ∀ i, i < n + 1 → a n ∈ Set.Ioo 0 (Real.pi / 2))
+    (ha : ∀ i, i < n + 1 → a i ∈ Set.Ioo 0 (Real.pi / 2))
     (hs : n - 1 ≤ ∑ i in Finset.range (n + 1), Real.tan (a i - (Real.pi / 4)))
     : Real.rpow n (n + 1) ≤ ∏ i in Finset.range (n + 1), Real.tan (a i) := by
+
+  obtain h0 | h0 := eq_or_ne n 0
+  · simp_all[h0]
+    exact Real.tan_nonneg_of_nonneg_of_le_pi_div_two
+            (le_of_lt ha.1)
+            (le_of_lt ha.2)
+
   -- informal solution from artofproblemsolving.com
   --
   -- Let yᵢ = tan(aᵢ - π/4), where 0 ≤ i ≤ n.
+  let y : ℕ → ℝ := λ i ↦ Real.tan (a i - Real.pi / 4)
   -- Then we have
   --  y₀ + y₁ + ... + yₙ ≥ n - 1
+  have h1 : n - 1 ≤ ∑ i in Finset.range (n + 1), y i := hs
+
   --  1 + yᵢ ≥ ∑_{j ≠ i} (1 - yⱼ)
+  have h2 : ∀ i, i < n + 1 →
+      ∑ j in (Finset.range (n + 1)).erase i, (1 - y j) ≤ 1 + y i := by
+    intros i hi
+    have hi' : i ∈ Finset.range (n + 1) := Finset.mem_range.mpr hi
+    rw[Finset.sum_erase_eq_sub hi']
+    simp only [Finset.sum_sub_distrib, Finset.sum_const, Finset.card_range,
+               nsmul_eq_mul, Nat.cast_add, Nat.cast_one]
+    linarith
+
   --  (1 + yᵢ)/n ≥ (1/n) ∑_{j ≠ i} (1 - yⱼ)
+  have h3 : ∀ i, i < n + 1 →
+      (1/(n:ℝ)) * ∑ j in (Finset.range (n + 1)).erase i, (1 - y j)
+          ≤ (1 + y i)/n := by
+    intros i hi
+    have h2' := h2 i hi
+    have hn : 0 ≤ (n : ℝ) := Nat.cast_nonneg n
+    rw[div_mul_eq_mul_div, one_mul]
+    exact div_le_div_of_le hn (h2 i hi)
+
   --
   -- Then, by AM-GM,
   -- (1/n) ∑_{j ≠ i} (1 - yⱼ) ≥ ∏_{j ≠ i} (1 - yⱼ)^{1/n}
+  have h4 : ∀ i, i < n + 1 →
+     ∏ j in (Finset.range (n + 1)).erase i, (1 - y i)^(1 / (n : ℝ)) ≤
+     (1/(n:ℝ)) * ∑ j in (Finset.range (n + 1)).erase i, (1 - y j) := by
+    intros i hi
+    have hi' : i ∈ Finset.range (n + 1) := Finset.mem_range.mpr hi
+    let w : ℕ → ℝ := fun i ↦ 1 / (n:ℝ)
+    have hn : (n:ℝ) ≠ 0 := Nat.cast_ne_zero.mpr h0
+    have hw' : ∑ j in (Finset.range (n + 1)).erase i, w i = 1 := by
+       simp[Finset.card_erase_of_mem hi']; field_simp
+    have hw : ∀ j ∈ (Finset.range (n + 1)).erase i, 0 ≤ w j := by
+      intros j hj
+      simp only [one_div, inv_nonneg, Nat.cast_nonneg]
+    have hz : ∀ j ∈ (Finset.range (n + 1)).erase i, 0 ≤ 1 - y j := by
+      intros j hj
+      rw[Finset.mem_erase, Finset.mem_range] at hj
+      have haj := ha j hj.2
+      rw[Set.mem_Ioo] at haj
+      -- use Real.strictMonoOn_tan ...
+      sorry
+    have := Real.geom_mean_le_arith_mean_weighted
+              ((Finset.range (n + 1)).erase i)
+              w (λ j ↦ 1 - y j)
+              hw hw' hz
+    sorry
+
   -- (1 + yᵢ)/n ≥ ∏_{j ≠ i} (1 - yⱼ)^{1/n}
   -- ∏ᵢ(1 + yᵢ)/n ≥ ∏ᵢ∏_{j ≠ i} (1 - yⱼ)^{1/n}
   -- ... a bunch more steps...
   sorry
+
+#check Real.strictMonoOn_tan
+#check Finset.card_erase_of_mem
+#check Real.geom_mean_le_arith_mean_weighted
