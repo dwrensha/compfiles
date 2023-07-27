ea6cedd064e12e7853c8e8fed89e3d585ac3ce54
David Renshaw <dwrenshaw@gmail.com>
2023-07-27 11:26:06 -0400

    update mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -12,4 +12,4 @@ lean_exe buildWebpage where
   root := `scripts.buildWebpage
   supportInterpreter := true
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "03720bd1962af9f667a921afa21dbbcb1c337abb"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "00269a6fe716b08dafb1c2791b935cb96f40aab2"
