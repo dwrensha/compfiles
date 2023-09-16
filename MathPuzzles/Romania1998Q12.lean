596313def99927e46feebcfe147070b21b2caa6e
David Renshaw <dwrenshaw@gmail.com>
2023-09-15 20:59:54 -0400

    [Romania1998Q12] factor out solution_set

--- MathPuzzles/Romania1998Q12.lean
+++ MathPuzzles/Romania1998Q12.lean
@@ -556,13 +556,13 @@ lemma romania1998_q12_mpr (u : ℝ → ℝ) :
            _ = (Real.exp (k * x) - 1) * Real.exp (k * y) +
                   (Real.exp (k * y) - 1) := by ring
 
--- TODO define solution_set
--- def solution_set : Set (ℝ → ℝ) :=
--- { u | ∃ k : ℝ, ∀ x : ℝ, u x = Real.exp (k * x) }
+#[solution_data]
+def solution_set : Set (ℝ → ℝ) :=
+  { u | ∃ k : ℝ, ∀ x : ℝ, u x = Real.exp (k * x) }
 
 #[problem_statement]
 theorem romania1998_q12 (u : ℝ → ℝ) :
     (∃ f : ℝ → ℝ, (StrictMono f ∨ StrictAnti f)
           ∧ ∀ x y : ℝ, f (x + y) = f x * u y + f y) ↔
-    (∃ k : ℝ, ∀ x : ℝ, u x = Real.exp (k * x)) :=
+    u ∈ solution_set :=
 ⟨romania1998_q12_mp u, romania1998_q12_mpr u⟩
