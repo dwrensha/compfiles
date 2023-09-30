af54de06124c7b294900a878061a02d3b474a406
David Renshaw <dwrenshaw@gmail.com>
2023-09-30 18:53:51 -0400

    [Imo1981Q6] start sketching proof

--- MathPuzzles/Imo1981Q6.lean
+++ MathPuzzles/Imo1981Q6.lean
@@ -26,11 +26,14 @@ Determine f (4, 1981).
 
 #[problem_setup] namespace Imo1981Q6
 
-fill_in_the_blank solution_value : ℕ := sorry
+fill_in_the_blank solution_value : ℕ := (2^·)^[1984] 1 - 3
 
 problem Imo1981Q6 (f : ℕ → ℕ → ℕ)
     (h1 : ∀ y, f 0 y = y + 1)
     (h2 : ∀ x, f (x + 1) 0 = f x 1)
     (h3 : ∀ x y, f (x + 1) (y + 1) = f x (f (x + 1) y)) :
     f 4 1981 = solution_value := by
+  have h4 : ∀ y, f 1 y = y + 2 := by sorry
+  have h5 : ∀ y, f 1 (y + 1) = f 1 y + 1 := by sorry
+  have h6 : ∀ y, f 4 y + 3 = 2^(f 4 y + 3) := by sorry
   sorry
