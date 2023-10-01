817e9406a8d45a3c2f700e931eeaeacd18acbd38
David Renshaw <dwrenshaw@gmail.com>
2023-10-01 16:38:24 -0400

    [Imo1990P3] prove easy direction

--- MathPuzzles/Imo1990P3.lean
+++ MathPuzzles/Imo1990P3.lean
@@ -16,7 +16,13 @@ Find all integers n > 1 such that n² divides 2ⁿ + 1.
 
 #[problem_setup] namespace Imo1990P3
 
-fill_in_the_blank solution_set : Set ℕ := sorry
+fill_in_the_blank solution_set : Set ℕ := { 3 }
 
 problem imo1990_p3 (n : ℕ) (hn : 1 < n) : n ∈ solution_set ↔ n^2 ∣ 2^n + 1 := by
-  sorry
+  constructor
+  · intro hs
+    rw [Set.mem_singleton_iff] at hs
+    rw [hs]
+    norm_num
+  · intro hnd
+    sorry
