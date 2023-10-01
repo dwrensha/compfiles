8e8e1789f6b5c60415fb36e7094badc2cf22f562
David Renshaw <dwrenshaw@gmail.com>
2023-09-30 21:13:11 -0400

    [Imo1974Q5] fill in solution_set

--- MathPuzzles/Imo1974Q5.lean
+++ MathPuzzles/Imo1974Q5.lean
@@ -20,11 +20,15 @@ as a,b,c,d range over the positive real numbers?
 
 #[problem_setup] namespace Imo1974Q5
 
-fill_in_the_blank solution_set : Set ℝ := sorry
+fill_in_the_blank solution_set : Set ℝ := Set.Ioo 1 2
 
 problem imo1974_q5 (x : ℝ) :
     x ∈ solution_set ↔
     ∃ a b c d : ℝ, 0 < a ∧ 0 < b ∧ 0 < c ∧ 0 < d ∧
      x = a / (a + b + d) + b / (a + b + c) +
          c / (b + c + d) + d / (a + c + d) := by
-  sorry
+  constructor
+  · intro hx
+    sorry
+  · rintro ⟨a, b, c, d, ha, hb, hc, hd, hs⟩
+    sorry
