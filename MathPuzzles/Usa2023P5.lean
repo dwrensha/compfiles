216dce2dd28b782d633dc99430ad839e05b3ada1
David Renshaw <dwrenshaw@gmail.com>
2023-10-04 13:08:58 -0400

    [Usa2023P5] fix up problem statement

--- MathPuzzles/Usa2023P5.lean
+++ MathPuzzles/Usa2023P5.lean
@@ -27,35 +27,37 @@ the numbers in each row?
 #[problem_setup] namespace Usa2023P5
 
 #[problem_setup]
-def PermutedArithSeq {n : ℕ} (hn : 0 < n) (f : Fin n ↪ Fin (n^2)) : Prop :=
+def PermutedArithSeq {n : ℕ} (hn : 0 < n) (a : Fin n ↪ Fin (n^2)) : Prop :=
     ∃ p : Fin n → Fin n, p.Bijective ∧
-      ∃ k : ℕ, ∀ m : Fin n, (f (p m)).val = f (p ⟨0, hn⟩) + m.val * k
+      ∃ k : ℕ, ∀ m : Fin n, (a (p m)).val = a (p ⟨0, hn⟩) + m.val * k
 
 #[problem_setup]
-def row_valid {n : ℕ} (hn : 0 < n) (f : Fin n → Fin n → Fin (n^2)) (hf : f.Injective2) : Prop :=
-    ∀ m : Fin n, PermutedArithSeq hn ⟨(f · m), Function.Injective2.left hf m⟩
+def row_valid {n : ℕ} (hn : 0 < n) (a : Fin n → Fin n → Fin (n^2)) (ha : a.Injective2) : Prop :=
+    ∀ r : Fin n, PermutedArithSeq hn ⟨(a r ·), Function.Injective2.right ha r⟩
 
 #[problem_setup]
-def col_valid {n : ℕ} (hn : 0 < n) (f : Fin n → Fin n → Fin (n^2)) (hf : f.Injective2) : Prop :=
-    ∀ m : Fin n, PermutedArithSeq hn ⟨(f m ·), Function.Injective2.right hf m⟩
+def col_valid {n : ℕ} (hn : 0 < n) (a : Fin n → Fin n → Fin (n^2)) (ha : a.Injective2) : Prop :=
+    ∀ c : Fin n, PermutedArithSeq hn ⟨(a · c), Function.Injective2.left ha c⟩
 
 #[problem_setup]
 theorem injective_of_permuted_rows {α β γ : Type}
-    {f : α → β → γ} (hf : f.Injective2) {p : α → α} (hp : p.Injective) :
-    Function.Injective2 (fun x ↦ f (p x)) := by
+    {f : α → β → γ} (hf : f.Injective2) {p : α → β → β} (hp : ∀ a, (p a).Injective) :
+    Function.Injective2 (fun r c ↦ f r (p r c)) := by
   intro a1 a2 b1 b2 hab
-  obtain ⟨hp1, hb1⟩ := hf hab
-  exact ⟨hp hp1, hb1⟩
+  obtain ⟨ha1, hp1⟩ := hf hab
+  rw [ha1] at *
+  rw [hp a2 hp1]
+  simp only [and_self]
 
 determine solution_set : Set ℕ := { n | n.Prime }
 
 problem usa2023_p5 (n : ℕ) (hn : 2 < n) :
     n ∈ solution_set ↔
-    (∀ f : (Fin n → Fin n → Fin (n^2)),
-      (hf : f.Injective2) → row_valid (Nat.zero_lt_of_lt hn) f hf →
-        ∃ p : Fin n → Fin n, ∃ hp : p.Bijective,
-            col_valid (Nat.zero_lt_of_lt hn) (fun x ↦ f (p x))
-              (injective_of_permuted_rows hf hp.1)) := by
+    (∀ a : (Fin n → Fin n → Fin (n^2)),
+      (ha : a.Injective2) → row_valid (Nat.zero_lt_of_lt hn) a ha →
+        ∃ p : Fin n → Fin n → Fin n, ∃ hp : (∀ r, (p r).Injective),
+            col_valid (Nat.zero_lt_of_lt hn) (fun r c ↦ a r (p r c))
+              (injective_of_permuted_rows ha hp)) := by
   constructor
   · sorry
   · sorry
