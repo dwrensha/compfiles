2e69e171381bf91d467cc7b4c7247e81d860842e
David Renshaw <dwrenshaw@gmail.com>
2023-10-08 11:30:12 -0400

    [Usa1998P1] reformulate in a nicer way

--- MathPuzzles/Usa1998P1.lean
+++ MathPuzzles/Usa1998P1.lean
@@ -9,6 +9,7 @@ import Mathlib.Algebra.BigOperators.Basic
 import Mathlib.Algebra.BigOperators.Intervals
 import Mathlib.Data.Int.ModEq
 import Mathlib.Data.Finset.Interval
+import Mathlib.Data.Fintype.BigOperators
 
 import MathPuzzles.Meta.ProblemExtraction
 
@@ -38,56 +39,29 @@ lemma mod2_abs (a : ℤ) : |a| % 2 = a % 2 := by
 lemma mod2_diff (a b : ℤ) : |a - b| % 2 = (a + b) % 2 := by
   rw[mod2_abs, Int.sub_eq_add_neg, Int.add_emod, Int.neg_emod_two, ←Int.add_emod]
 
-lemma lemma2
-    {a b c : Finset ℕ}
-    (hab : Disjoint a b)
-    (hc : a.card + b.card = c.card)
-    (hac : a ⊆ c)
-    (hbc : b ⊆ c)
-    : a ∪ b = c := by
-  have habc : a ∪ b ⊆ c := Finset.union_subset hac hbc
-  have h1 : (a ∪ b).card = a.card + b.card := Finset.card_union_eq hab
-  rw[←h1] at hc
-  have h2 := le_of_eq hc.symm
-  exact Iff.mp (Finset.subset_iff_eq_of_card_le h2) habc
-
+/--
+ `ab 0 i` is aᵢ and `ab 1 i` is `bᵢ`
+-/
 problem usa1998_p1
-    (a b : ℕ → ℕ)
-    (ha : Finset.image a (Finset.Icc 1 999) ⊆ Finset.Icc 1 1998)
-    (hb : Finset.image b (Finset.Icc 1 999) ⊆ Finset.Icc 1 1998)
-    (hab : Disjoint (Finset.image a (Finset.Icc 1 999))
-                    (Finset.image b (Finset.Icc 1 999)))
-    (hai : Set.InjOn a (Finset.Icc 1 999))
-    (hbi : Set.InjOn b (Finset.Icc 1 999))
-    (habd : ∀ i ∈ Finset.Icc 1 999, |(a i : ℤ) - b i| = 1 ∨
-                                    |(a i : ℤ) - b i| = 6)
-    : (∑ i in Finset.Icc 1 999, |(a i : ℤ) - b i|) % 10 = 9 := by
-
-  -- Informal solution from https://artofproblemsolving.com:
+    (ab : Fin 2 → Fin 999 → Finset.Icc 1 1998)
+    (hab : (ab.uncurry).Bijective)
+    (habd : ∀ i : Fin 999,
+              |(ab 0 i : ℤ) - (ab 1 i : ℤ)| = 1 ∨
+              |(ab 0 i : ℤ) - (ab 1 i : ℤ)| = 6) :
+    (∑ i : Fin 999, |(ab 0 i : ℤ) - (ab 1 i : ℤ)|) % 10 = 9 := by
+  -- Informal solution from
+  -- https://artofproblemsolving.com/wiki/index.php/1998_USAMO_Problems/Problem_1
   -- Notice that |aᵢ-bᵢ| ≡ 1 MOD 5,
-  have h1 : ∀ i ∈ (Finset.Icc 1 999), |a i - b i| ≡ 1 [ZMOD 5] := fun i hi ↦ by
-    cases' habd i hi with habd habd
-    · rw[habd]
-    · rw[habd]; rfl
+  have h1 : ∀ i : Fin 999, |(ab 0 i : ℤ) - ab 1 i| % 5 = 1 % 5 := fun ⟨i, hi⟩ ↦ by
+    cases' habd ⟨i, hi⟩ with habd habd
+    · rw [habd]
+    · rw [habd]; rfl
 
   -- so S=|a₁-b₁|+|a₂-b₂|+ ⋯ +|a₉₉₉ - b₉₉₉| ≡ 1+1+ ⋯ + 1 ≡ 999 ≡ 4 MOD 5.
-  have h2 : (∑ i in Finset.Icc 1 999, |a i - b i|) ≡ 4 [ZMOD 5] :=
-  by rw[zmod_eq, Finset.sum_int_mod, Finset.sum_congr rfl h1]
-     simp only [gt_iff_lt, Finset.sum_const, Nat.card_Icc,
-                ge_iff_le, add_tsub_cancel_right, nsmul_eq_mul,
-                Nat.cast_ofNat, mul_one]
-
-  have h5 : Finset.image a (Finset.Icc 1 999) ∪
-            Finset.image b (Finset.Icc 1 999) =
-       Finset.Icc 1 1998 := by
-    have h20 : (Finset.Icc 1 999).card = 999 := Nat.card_Icc _ _
-    have h21 : (Finset.image a (Finset.Icc 1 999)).card = 999 :=
-       by rw[Finset.card_image_iff.mpr hai, h20]
-    have h22 : (Finset.image b (Finset.Icc 1 999)).card = 999 :=
-       by rw[Finset.card_image_iff.mpr hbi, h20]
-
-    -- therefore they hit every value in Finset.Icc 1 1998
-    exact lemma2 hab (by simp[h21, h22]) ha hb
+  have h2 : (∑ i : Fin 999, |ab 0 i - ab 1 i|) ≡ 4 [ZMOD 5] :=
+  by rw[zmod_eq, Finset.sum_int_mod, Fintype.sum_congr _ _ h1]
+     rw [Finset.sum_const, Finset.card_univ, Fintype.card_fin]
+     norm_num
 
   --
   -- Also, for integers M,N we have |M-N| ≡ M-N ≡ M+N MOD 2.
@@ -97,33 +71,41 @@ problem usa1998_p1
   -- S ≡ a₁ + b₁ + a₂ + b₂ + ⋯ + a₉₉₉ + b₉₉₉ [MOD 2]
   --   ≡ 1 + 2 + ⋯ + 1998 [MOD 2]
   --   ≡ 999*1999 ≡ 1 [MOD 2]
-  have h3 : ∑ i in Finset.Icc 1 999, |a i - b i| ≡ 1 [ZMOD 2] := by
+  have h3 : ∑ i : Fin 999, |(ab 0 i : ℤ) - ab 1 i| ≡ 1 [ZMOD 2] := by
     rw[zmod_eq, Finset.sum_int_mod]
-    have h4 : ∀ i ∈ Finset.Icc 1 999,
-        |(a i : ℤ) - b i| % 2 = ((a i % 2) + (b i % 2)) % 2 := by
-      intros i _
+    have h4 : ∀ i : Fin 999,
+        |(ab 0 i : ℤ) - ab 1 i| % 2 = ((ab 0 i % 2) + (ab 1 i % 2)) % 2 := by
+      intro i
       rw[mod2_diff, Int.add_emod]
 
-    rw[Finset.sum_congr rfl h4, ←Finset.sum_int_mod, Finset.sum_add_distrib]
+    rw[Fintype.sum_congr _ _ h4, ←Finset.sum_int_mod, Finset.sum_add_distrib]
 
-    have h10 : ∑ i in Finset.Icc 1 999, (a i : ℤ) % 2 =
-        ∑ i in Finset.image a (Finset.Icc 1 999), (i : ℤ) % 2 :=
-      -- specify `f` to avoid "maximum recursion depth has been reached"
-      (Finset.sum_image (f := (fun (i:ℕ) ↦ ((i:ℤ) % 2))) hai).symm
+    have h9 : ∑ x : Fin 999, ((ab 0 x : ℤ) % 2) + ∑ x : Fin 999, ((ab 1 x : ℤ) % 2) =
+         ∑ x : Fin 2 × Fin 999, (↑↑(ab.uncurry x) % 2) := by
 
-    have h11 : ∑ i in Finset.Icc 1 999, (b i : ℤ) % 2 =
-        ∑ i in Finset.image b (Finset.Icc 1 999), (i : ℤ) % 2 :=
-      (Finset.sum_image (f := (fun (i:ℕ) ↦ ((i:ℤ) % 2))) hbi).symm
+      have h12 : (Finset.univ : Finset (Fin 2)) =
+                  Finset.cons 0 (Finset.cons 1 {} (Finset.not_mem_empty _)) (by simp) := by simp
+      have h11 : ∑ x : Fin 999, ((ab 0 x : ℤ) % 2) + ∑ x : Fin 999, ((ab 1 x : ℤ) % 2) =
+          ∑ x : Fin 2, ∑ y : Fin 999, ((ab x y : ℤ) % 2) := by
+        rw [h12, Finset.sum_cons, Finset.sum_cons, Finset.sum_empty]
+        ring
 
-    rw[h10, h11, ←Finset.sum_union hab, h5, ←Finset.sum_int_mod]
+      rw [h11, ←Finset.sum_product']
+      congr
+
+    have h10 : ∑ x : Fin 2 × Fin 999, ((ab.uncurry x : ℤ) % 2) =
+              ∑ x : Finset.Icc 1 1998, (x : ℤ) % 2 :=
+      by apply Fintype.sum_bijective _ hab
+         · exact congr_fun rfl
+    rw [h9, h10]
     norm_cast
 
   --
   -- Combining these facts gives S ≡ 9 MOD 10.
   have hmn : Nat.Coprime (Int.natAbs 2) (Int.natAbs 5) := by norm_cast
-  rw[show (9:ℤ) = 9 % 10 by norm_num,
-     ← zmod_eq,
-     show (10:ℤ) = 2 * 5 by norm_num]
+  rw [show (9:ℤ) = 9 % 10 by norm_num,
+      ← zmod_eq,
+      show (10:ℤ) = 2 * 5 by norm_num]
 
-  rw[←Int.modEq_and_modEq_iff_modEq_mul hmn, zmod_eq, zmod_eq, h3, h2]
+  rw [←Int.modEq_and_modEq_iff_modEq_mul hmn, zmod_eq, zmod_eq, h3, h2]
   norm_num
