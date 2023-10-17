3ea3a33e3c031830aa7d2e7a2b3b76e40c41b4b3
David Renshaw <dwrenshaw@gmail.com>
2023-10-17 13:20:32 -0400

    [Usa2011P4] full proof. easy.

--- Compfiles/Usa2011P4.lean
+++ Compfiles/Usa2011P4.lean
@@ -13,9 +13,9 @@ problem_file
 /-!
 # USA Mathematical Olympiad 2011, Problem 4
 
-For any integer n > 1, let P(n) be the proposition:
+For any integer n ≥ 2, let P(n) be the proposition:
 
-  "2^(2^n) % (2^n - 1) is a power of 4"
+  2^(2^n) % (2^n - 1) is a power of 4
 
 Either prove that P(n) is always true, or find a counterexample.
 -/
@@ -28,10 +28,20 @@ inductive SolutionData where
 | AlwaysTrue : SolutionData
 | CounterExample : ℕ → SolutionData
 
-determine solution_data : SolutionData := sorry
+determine solution_data : SolutionData := SolutionData.CounterExample 25
 
 problem usa2011_p4 :
     match solution_data with
     | .AlwaysTrue => ∀ n, 2 ≤ n → P n
     | .CounterExample m => 2 ≤ m ∧ ¬ P m := by
-  sorry
+  -- See https://web.evanchen.cc/exams/USAMO-2011-notes.pdf for an informal proof.
+  dsimp
+  refine' ⟨by norm_num, _⟩
+  rw [not_exists]
+  intro x hx
+  rw [show 4 = 2^2 by rfl, ←Nat.pow_mul] at hx
+  rw [show 2 ^ 2 ^ 25 % (2 ^ 25 - 1) = 2 ^ 7 by rfl] at hx
+  have h2 : 2 ≤ 2 := by norm_num
+  have h3 := Nat.pow_right_injective h2 hx
+  apply_fun (· % 2) at h3
+  norm_num at h3
