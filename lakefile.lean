aa8246271d232faf78974829f9d6118b64f9f056
David Renshaw <dwrenshaw@gmail.com>
2023-10-22 08:00:26 -0400

    update mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -17,4 +17,4 @@ lean_exe extractProblems where
   root := `scripts.extractProblems
   supportInterpreter := true
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "30c94754f9063d6bc090079e2527087f224f5d6d"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "004800e44b832e4ca875eab23bd21f31a693687a"
