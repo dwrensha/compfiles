936c735ac9d7e0117eff647f3227eac73f95a706
David Renshaw <dwrenshaw@gmail.com>
2023-10-23 18:15:21 -0400

    [Imo1987P4] fix copy/paste error in copyright header

--- Compfiles/Imo1987P4.lean
+++ Compfiles/Imo1987P4.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2023 Moritz Firsching. All rights reserved.
+Copyright (c) 2023 David Renshaw. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Renshaw
 -/
