512f93ab5a86e6a6ca9a251c2945e4356db5620f
David Renshaw <dwrenshaw@gmail.com>
2023-10-24 10:58:42 -0400

    [Imo2022P2] finish the easy direction

--- Compfiles/Imo2022P2.lean
+++ Compfiles/Imo2022P2.lean
@@ -43,7 +43,18 @@ problem imo2022_p2 (f : ℝ+ → ℝ+) :
     · simp [hf]
       suffices h : (1:ℝ+) + 1 = ⟨2, two_pos⟩ by exact Eq.le h
       norm_num [Subtype.ext_iff]
-    · intro y hy
-      rw [hf] at hy
-      sorry
-  · sorry
+    · intro y hxy
+      rw [hf] at hxy
+      change (x * (1 / y) + y * (1 / x)).val ≤ _  at hxy
+      obtain ⟨x, hx⟩ := x
+      obtain ⟨y, hy⟩ := y
+      simp only [Positive.coe_add, Positive.val_mul, one_div, Positive.coe_inv] at hxy
+      rw [Subtype.mk_eq_mk]
+      have hxyp : 0 < y * x := Real.mul_pos hy hx
+      have hxynz : y * x ≠ 0 := ne_of_gt hxyp
+      field_simp at hxy
+      have h1 : (x * x + y * y)  ≤ 2 * (y * x) := (div_le_iff hxyp).mp hxy
+      nlinarith
+  · intro hf
+    rw [Set.mem_singleton_iff]
+    sorry
