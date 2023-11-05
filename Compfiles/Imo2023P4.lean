98240954b770f2fa98a9167dc7e8e08e3f88377f
David Renshaw <dwrenshaw@gmail.com>
2023-11-05 18:22:20 -0500

    [Imo2023P4] make formal problem statement closer to informal one

--- Compfiles/Imo2023P4.lean
+++ Compfiles/Imo2023P4.lean
@@ -13,27 +13,30 @@ problem_file
 /-!
 # International Mathematical Olympiad 2023, Problem 4
 
-Let x₀, x₁, ... x₂₀₂₂ be distinct positive real numbers.
+Let x₁, x₂, ... x₂₀₂₃ be distinct positive real numbers.
 Define
 
-   aₙ := √((x₀ + x₁ + ... + xₙ)(1/x₀ + 1/x₁ + ... + 1/xₙ)).
+   aₙ := √((x₁ + x₂ + ... + xₙ)(1/x₁ + 1/x₂ + ... + 1/xₙ)).
 
-Suppose that aₙ is an integer for all n ∈ {0,1,...,2022}.
-Prove that 3034 ≤ a₂₀₂₂.
+Suppose that aₙ is an integer for all n ∈ {1,...,2023}.
+Prove that 3034 ≤ a₂₀₂₃.
 -/
 
 namespace Imo2023P4
 
 open BigOperators
 
-noncomputable def a (x : Fin 2023 → ℝ) (n : Fin 2023) : ℝ :=
-  Real.sqrt ((∑ i : Fin n.val, x ⟨i, Nat.lt_trans i.prop n.prop⟩) *
-             (∑ i : Fin n.val, (1 / x ⟨i, Nat.lt_trans i.prop n.prop⟩)))
+def iccCastLE {k n m : ℕ} (hn : n ≤ m) (i : Finset.Icc k n) : Finset.Icc k m :=
+  ⟨i.val, Finset.Icc_subset_Icc_right hn i.prop⟩
+
+noncomputable def a (x : Finset.Icc 1 2023 → ℝ) (n : ℕ) (hn : n ≤ 2023) : ℝ :=
+  Real.sqrt ((∑ i : Finset.Icc 1 n, x (iccCastLE hn i)) *
+             (∑ i : Finset.Icc 1 n, (1 / x (iccCastLE hn i))))
 
 problem imo2023_p4
-    (x : Fin 2023 → ℝ)
+    (x : Finset.Icc 1 2023 → ℝ)
     (hxp : ∀ i, 0 < x i)
     (hxi : x.Injective)
-    (hxa : ∀ i, ∃ k : ℤ, a i = k)
-    : 3034 ≤ a x ⟨2022, by norm_num⟩ := by
+    (hxa : ∀ i : Finset.Icc 1 2023, ∃ k : ℤ, a x i.val (by aesop) = k)
+    : 3034 ≤ a x 2023 (by norm_num) := by
   sorry
