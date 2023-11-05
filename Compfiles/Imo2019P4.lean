9fc5a00fe352ed9f4719325c5c0776414c16a341
David Renshaw <dwrenshaw@gmail.com>
2023-11-05 11:03:08 -0500

    [Imo2019P4] tweak problem statement to better constrain solution_set

--- Compfiles/Imo2019P4.lean
+++ Compfiles/Imo2019P4.lean
@@ -85,14 +85,15 @@ snip end
 
 determine solution_set : Set (ℕ × ℕ) := { (1,1), (2,3) }
 
-problem imo2018_p2 (n k : ℕ) (npos : 0 < n) (kpos : 0 < k) :
+problem imo2018_p2 (n k : ℕ) :
     (n, k) ∈ solution_set ↔
+    0 < n ∧ 0 < k ∧
     (k ! : ℤ) = ∏ i in Finset.range n, ((2:ℤ)^n - (2:ℤ)^i) := by
   constructor
   · intro nk
     simp only [Set.mem_insert_iff, Set.mem_singleton_iff, Prod.mk.injEq] at nk
     obtain ⟨rfl, rfl⟩ | ⟨rfl, rfl⟩ := nk <;> norm_num
-  intro h
+  rintro ⟨npos, kpos, h⟩
   -- We know that n < 6.
   have := Imo2019P4.upper_bound kpos h
   interval_cases n
