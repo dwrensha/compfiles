8529841868394b79d27536a8e4f53370cf50b635
David Renshaw <dwrenshaw@gmail.com>
2023-11-05 10:37:18 -0500

    [Imo2019P4] fix bug in factorial notation, and prove easy direction

--- Compfiles/Imo2019P4.lean
+++ Compfiles/Imo2019P4.lean
@@ -20,11 +20,15 @@ Determine all positive integers n,k that satisfy the equation
 
 namespace Imo2019P4
 
-open scoped BigOperators
+open scoped Nat BigOperators
 
 determine solution_set : Set (ℕ × ℕ) := { (1,1), (2,3) }
 
 problem imo2018_p2 (n k : ℕ) (npos : 0 < n) (kpos : 0 < k) :
     (n, k) ∈ solution_set ↔
-    k! = ∏ i in Finset.range n, (2^n - 2^i) := by
-  sorry
+    k ! = ∏ i in Finset.range n, (2^n - 2^i) := by
+  constructor
+  · intro nk
+    simp only [Set.mem_insert_iff, Set.mem_singleton_iff, Prod.mk.injEq] at nk
+    obtain ⟨rfl, rfl⟩ | ⟨rfl, rfl⟩ := nk <;> norm_num
+  · sorry
