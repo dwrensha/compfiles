19c8184c3a53e15f7c7945c522ca23ba74b90b8e
David Renshaw <dwrenshaw@gmail.com>
2023-11-07 17:39:51 -0500

    [Imo2014P1] initial steps

--- Compfiles/Imo2014P1.lean
+++ Compfiles/Imo2014P1.lean
@@ -27,4 +27,18 @@ problem imo2014_p1 (a : ℕ → ℤ) (apos : ∀ i, 0 < a i) (ha : ∀ i, a i < 
     ∃! n : ℕ, 0 < a ∧
               a n < (∑ i in Finset.range (n + 1), a i)/n ∧
               (∑ i in Finset.range (n + 1), a i)/n ≤ a (n + 1) := by
+  -- Informal solution by Fedor Petrov, via Evan Chen:
+  -- https://web.evanchen.cc/exams/IMO-2014-notes.pdf
+
+--  let b : ℕ → ℤ := fun i ↦ ∑ j in Finset.range i, (a (i + 1) - a (j + 1))
+  let b : ℕ → ℤ := fun i ↦ i * a (i + 1) - ∑ j in Finset.range i, a (j + 1)
+  have hb0 : b 0 = 0 := by norm_num
+  have hm : ∀ i, b i < b (i + 1) := by
+    intro i
+    dsimp only [b]
+    rw [Finset.sum_range_succ]
+    have hi := ha i
+    have his := ha (i + 1)
+    push_cast
+    nlinarith
   sorry
