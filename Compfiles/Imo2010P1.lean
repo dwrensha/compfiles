e29a233fbd3086ab4ca8b161728f5e7294ccbfbd
David Renshaw <dwrenshaw@gmail.com>
2023-11-08 15:12:27 -0500

    [Imo2010P1] add problem statement

---
+++ Compfiles/Imo2010P1.lean
@@ -0,0 +1,27 @@
+/-
+Copyright (c) 2023 David Renshaw. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Renshaw
+-/
+
+import Mathlib.Tactic
+
+import ProblemExtraction
+
+problem_file
+
+/-!
+# International Mathematical Olympiad 2010, Problem 1
+
+Determine all functions f : ℝ → ℝ such that for all x,y ∈ ℝ,
+
+  f(⌊x⌋y) = f(x)⌊f(y)⌋.
+-/
+
+namespace Imo2010P1
+
+determine solution_set : Set (ℝ → ℝ) := sorry
+
+problem imo2010_p1 (f : ℝ → ℝ) :
+    f ∈ solution_set ↔ ∀ x y, f (⌊x⌋ * y) = f x * ⌊f y⌋ := by
+  sorry
