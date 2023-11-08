3213151c47814ab60762d1069c08b85789e6f3ac
David Renshaw <dwrenshaw@gmail.com>
2023-11-08 10:56:04 -0500

    [Imo2014P1] simplify via exists_unique_congr

--- Compfiles/Imo2014P1.lean
+++ Compfiles/Imo2014P1.lean
@@ -25,10 +25,19 @@ open scoped BigOperators
 
 snip begin
 
+lemma lemma0 {p : ℕ → Prop} (h : ∃! n, p (n + 1)) : (∃! n, 0 < n ∧ p n) := by
+  obtain ⟨n, hn1, hn2⟩ := h
+  use n + 1
+  refine' ⟨⟨Nat.succ_pos n, hn1⟩, _⟩
+  rintro m ⟨hm1, hm2⟩
+  have hm3 := hn2 (m - 1)
+  dsimp only at hm3
+  rw [Nat.sub_add_cancel hm1] at hm3
+  exact Nat.eq_add_of_sub_eq hm1 (hm3 hm2)
+
 lemma lemma1 (s : ℕ → ℤ) (hs : ∀ i, s i < s (i + 1)) (z : ℤ) (hs0 : s 0 < z) :
     ∃! i, s i < z ∧ z ≤ s (i + 1) := by
   have hmono : StrictMono s := strictMono_nat_of_lt_succ hs
-
   let S := { i | z ≤ s (i + 1) }
   have h3 : ∃ j, j ∈ S := by
     have h1 : 0 < z - s 0 := Int.sub_pos_of_lt hs0
@@ -94,62 +103,31 @@ problem imo2014_p1 (a : ℕ → ℤ) (apos : ∀ i, 0 < a i) (ha : ∀ i, a i < 
     push_cast
     nlinarith
 
+  have h1 : ∀ j,
+    (0 < j ∧ j * a j < (∑ i in Finset.range (j + 1), a i) ∧
+                       (∑ i in Finset.range (j + 1), a i) ≤ j * a (j + 1)) ↔
+    (0 < j ∧ b j < a 0 ∧ a 0 ≤ b (j + 1)) := fun j ↦ by
+    rw [hb, hb]
+    constructor
+    · rintro ⟨hj0, hj1, hj2⟩
+      refine' ⟨hj0, _, _⟩
+      · suffices H : ↑j * a j < ∑ i in Finset.range j, a (i + 1) + a 0 by
+          exact Int.sub_left_lt_of_lt_add H
+        rwa [Finset.sum_range_succ'] at hj1
+      · rw [Finset.sum_range_succ'] at hj2
+        rw [Finset.sum_range_succ]
+        push_cast
+        linarith
+    · rintro ⟨hj0, hj1, hj2⟩
+      refine' ⟨hj0, _, _⟩
+      · have H := Int.lt_add_of_sub_left_lt hj1
+        rwa [Finset.sum_range_succ']
+      · rw [Finset.sum_range_succ']
+        rw [Finset.sum_range_succ] at hj2
+        push_cast at hj2
+        linarith
   have hm' : ∀ i, b (i + 1) < b (i + 1 + 1) := fun i ↦ hm (i + 1) (Nat.succ_pos _)
-  have h1 : ∀ j, (j + 1) * a (j + 1) < (∑ i in Finset.range (j + 2), a i) ↔
-                 b (j + 1) < a 0 := fun j ↦ by
-    rw [hb]
-    constructor
-    · intro hj
-      suffices H : (↑j + 1) * a (j + 1) < ∑ j in Finset.range (j + 1), a (j + 1) + a 0 by
-        exact Int.sub_left_lt_of_lt_add H
-      rwa [Finset.sum_range_succ'] at hj
-    · intro hj
-      have H := Int.lt_add_of_sub_left_lt hj
-      rwa [Finset.sum_range_succ']
-
-  have h2 : ∀ j, (∑ i in Finset.range (j + 2), a i) ≤ (j + 1) * a (j + 2)  ↔
-                 a 0 ≤ b (j + 2) := fun j ↦ by
-    rw [hb]
-    constructor
-    · intro hj
-      rw [Finset.sum_range_succ'] at hj
-      rw [Finset.sum_range_succ]
-      push_cast
-      linarith
-    · intro hj
-      rw [Finset.sum_range_succ']
-      rw [Finset.sum_range_succ] at hj
-      push_cast at hj
-      linarith
-
   have h3 : ∃! n, b (n + 1) < a 0 ∧ a 0 ≤ b (n + 2) :=
     lemma1 (fun i ↦ b (i + 1)) hm' (a 0) (hb1.trans_lt (apos 0))
-  obtain ⟨n, ⟨hn1, hn2⟩, hn3⟩ := h3
-  use n + 1
-  dsimp only
-  constructor
-  · constructor
-    · exact Nat.succ_pos n
-    · constructor
-      · push_cast
-        rw [h1]
-        exact hn1
-      · push_cast
-        rw [h2]
-        exact hn2
-  · rintro m ⟨hm1, hm2, hm3⟩
-    have h5 := hn3 (m - 1)
-    have h6 := h1 (m - 1)
-    have h7 := h2 (m - 1)
-    dsimp only at h5 h6
-    suffices H : m - 1 = n by exact Nat.eq_add_of_sub_eq hm1 H
-    apply h5
-    have h8 : m - 1 + 1 = m := Nat.sub_add_cancel hm1
-    have h9 : m - 1 + 2 = m + 1 := Nat.succ_inj'.mpr h8
-    have h10 : (((m - 1):ℕ):ℤ) + 1 = ↑ m := by norm_cast
-    rw [h8, h9, h10] at h6
-    have h11 := h6.mp hm2
-    rw [h9, h10] at h7
-    have h12 := h7.mp hm3
-    rw [h8, h9]
-    exact ⟨h11, h12⟩
+  replace h3 := lemma0 (p := fun n ↦ b n < a 0 ∧ a 0 ≤ b (n + 1)) h3
+  exact (exists_unique_congr h1).mpr h3
