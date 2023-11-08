79f9e2f17c74875f0c9c51c4021400ebda83d63a
David Renshaw <dwrenshaw@gmail.com>
2023-11-08 07:47:59 -0500

    [Imo2014P1] all that remains is lemma1

--- Compfiles/Imo2014P1.lean
+++ Compfiles/Imo2014P1.lean
@@ -23,6 +23,15 @@ namespace Imo2014P1
 
 open scoped BigOperators
 
+snip begin
+
+lemma lemma1 (s : ℕ → ℤ) (hs : ∀ i, s i < s (i + 1)) (z : ℤ) (hs0 : s 0 < z) :
+    ∃! i, s i < z ∧ z ≤ s (i + 1) := by
+  let S := { i | z ≤ s i }
+  sorry
+
+snip end
+
 problem imo2014_p1 (a : ℕ → ℤ) (apos : ∀ i, 0 < a i) (ha : ∀ i, a i < a (i + 1)) :
     ∃! n : ℕ, 0 < n ∧
               n * a n < (∑ i in Finset.range (n + 1), a i) ∧
@@ -40,9 +49,10 @@ problem imo2014_p1 (a : ℕ → ℤ) (apos : ∀ i, 0 < a i) (ha : ∀ i, a i < 
     rw [hb, hb]
     rw [Finset.sum_range_succ]
     have hi := ha i
-    have his := ha (i + 1)
     push_cast
     nlinarith
+
+  have hm' : ∀ i, b (i + 1) < b (i + 1 + 1) := fun i ↦ hm (i + 1) (Nat.succ_pos _)
   have h1 : ∀ j, (j + 1) * a (j + 1) < (∑ i in Finset.range (j + 2), a i) ↔
                  b (j + 1) < a 0 := fun j ↦ by
     rw [hb]
@@ -70,5 +80,34 @@ problem imo2014_p1 (a : ℕ → ℤ) (apos : ∀ i, 0 < a i) (ha : ∀ i, a i < 
       push_cast at hj
       linarith
 
-  have h3 : ∃! n, b (n + 1) < a 0 ∧ a 0 ≤ b (n + 1) := by sorry
-  sorry
+  have h3 : ∃! n, b (n + 1) < a 0 ∧ a 0 ≤ b (n + 2) :=
+    lemma1 (fun i ↦ b (i + 1)) hm' (a 0) (hb1.trans_lt (apos 0))
+  obtain ⟨n, ⟨hn1, hn2⟩, hn3⟩ := h3
+  use n + 1
+  dsimp only
+  constructor
+  · constructor
+    · exact Nat.succ_pos n
+    · constructor
+      · push_cast
+        rw [h1]
+        exact hn1
+      · push_cast
+        rw [h2]
+        exact hn2
+  · rintro m ⟨hm1, hm2, hm3⟩
+    have h5 := hn3 (m - 1)
+    have h6 := h1 (m - 1)
+    have h7 := h2 (m - 1)
+    dsimp only at h5 h6
+    suffices H : m - 1 = n by exact Nat.eq_add_of_sub_eq hm1 H
+    apply h5
+    have h8 : m - 1 + 1 = m := Nat.sub_add_cancel hm1
+    have h9 : m - 1 + 2 = m + 1 := Nat.succ_inj'.mpr h8
+    have h10 : (((m - 1):ℕ):ℤ) + 1 = ↑ m := by norm_cast
+    rw [h8, h9, h10] at h6
+    have h11 := h6.mp hm2
+    rw [h9, h10] at h7
+    have h12 := h7.mp hm3
+    rw [h8, h9]
+    exact ⟨h11, h12⟩
