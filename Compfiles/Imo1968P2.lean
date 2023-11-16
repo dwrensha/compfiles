46688061d5d92e160472435bd3a6d3564fdacc68
David Renshaw <dwrenshaw@gmail.com>
2023-11-16 10:50:25 -0500

    [Imo1968P2] generalize lemma1 and rename it to prod_digits_le

--- Compfiles/Imo1968P2.lean
+++ Compfiles/Imo1968P2.lean
@@ -32,32 +32,31 @@ lemma lemma0 {α β : Type} {f : ℕ → α → β} (l : List α)
   congr
   exact List.get_length_sub_one _
 
-lemma lemma1 (x : ℕ) (xpos : 0 < x) : List.prod (Nat.digits 10 x) ≤ x := by
-  have h1 : Nat.digits 10 x ≠ [] :=
+lemma prod_digits_le {x b : ℕ} (hb : 2 ≤ b) (xpos : 0 < x) :
+    List.prod (Nat.digits b x) ≤ x := by
+  have h1 : Nat.digits b x ≠ [] :=
     Nat.digits_ne_nil_iff_ne_zero.mpr (Nat.pos_iff_ne_zero.mp xpos)
   rw [←List.dropLast_append_getLast h1, List.prod_append, List.prod_singleton]
 
-  have h3 := Nat.ofDigits_digits 10 x
+  have h3 := Nat.ofDigits_digits b x
   rw [Nat.ofDigits_eq_sum_mapIdx] at h3
-  have h4 : List.mapIdx (fun i a => a * 10 ^ i) (Nat.digits 10 x) ≠ [] := by
-    rw [Nat.digits_of_two_le_of_pos (by norm_num) xpos, List.mapIdx_cons];
+  have h4 : List.mapIdx (fun i a => a * b ^ i) (Nat.digits b x) ≠ [] := by
+    rw [Nat.digits_of_two_le_of_pos hb xpos, List.mapIdx_cons]
     exact List.cons_ne_nil _ _
   have h5 := List.dropLast_append_getLast h4
   rw [←h5, List.sum_append, List.sum_singleton] at h3; clear h5
-  have h6 : List.getLast (List.mapIdx (fun i a => a * 10 ^ i) (Nat.digits 10 x)) h4 ≤ x :=
+  have h6 : List.getLast (List.mapIdx (fun i a => a * b ^ i) (Nat.digits b x)) h4 ≤ x :=
      by nth_rewrite 2 [←h3]; exact Nat.le_add_left _ _
-  have h7 : List.getLast (List.mapIdx (fun i a => a * 10 ^ i) (Nat.digits 10 x)) h4 =
-       10 ^ (List.dropLast (Nat.digits 10 x)).length * List.getLast (Nat.digits 10 x) h1 := by
-    rw [lemma0 _ h4 h1]
-    rw [mul_comm]
+  have h7 : List.getLast (List.mapIdx (fun i a => a * b ^ i) (Nat.digits b x)) h4 =
+       b ^ (List.dropLast (Nat.digits b x)).length * List.getLast (Nat.digits b x) h1 := by
+    rw [lemma0 _ h4 h1, mul_comm]
   rw [h7] at h6; clear h7
 
-  have h8 : List.prod (List.dropLast (Nat.digits 10 x)) ≤
-            10^(List.length (List.dropLast (Nat.digits 10 x))) := by
-    have h10 : ∀ d ∈ List.dropLast (Nat.digits 10 x), d ≤ 10 := by
-      intro d hd
-      exact Nat.le_of_lt (Nat.digits_lt_base' (List.mem_of_mem_dropLast hd))
-    exact List.prod_le_pow_card (List.dropLast (Nat.digits 10 x)) 10 h10
+  have h8 : List.prod (List.dropLast (Nat.digits b x)) ≤
+            b^(List.length (List.dropLast (Nat.digits b x))) := by
+    have h10 : ∀ d ∈ List.dropLast (Nat.digits b x), d ≤ b := fun d hd ↦
+      Nat.le_of_lt (Nat.digits_lt_base hb (List.mem_of_mem_dropLast hd))
+    exact List.prod_le_pow_card (List.dropLast (Nat.digits b x)) b h10
 
   calc _ ≤ _ := Nat.mul_le_mul_right _ h8
        _ ≤ _ := h6
@@ -75,7 +74,8 @@ problem imo1968_p2 (x : ℕ) :
   · rintro rfl; norm_num
   · intro hs
     have h0 : 0 < x := (Nat.eq_zero_or_pos x).resolve_left (fun H ↦ by norm_num [H] at hs)
-    have h2 : x^2 ≤ 10 * x + 22 + x := le_add_of_le_add_left (le_of_eq hs) (lemma1 x h0)
+    have h2 : x^2 ≤ 10 * x + 22 + x := le_add_of_le_add_left (le_of_eq hs)
+      (prod_digits_le (by norm_num) h0)
     have h3 : x < 13 := by qify at h0 h2 ⊢; nlinarith
     rw [Set.mem_singleton_iff]
     interval_cases x <;> norm_num at hs ⊢
