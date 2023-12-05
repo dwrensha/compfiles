9896057936843d38f35473312764d7ce6f328f85
David Renshaw <dwrenshaw@gmail.com>
2023-12-05 13:49:03 -0500

    [Usa2001P3] add problem statement

--- Compfiles.lean
+++ Compfiles.lean
@@ -77,6 +77,7 @@ import Compfiles.Usa1998P5
 import Compfiles.Usa1999P1
 import Compfiles.Usa2000P1
 import Compfiles.Usa2001P1
+import Compfiles.Usa2001P3
 import Compfiles.Usa2002P1
 import Compfiles.Usa2003P1
 import Compfiles.Usa2011P4
---
+++ Compfiles/Usa2001P3.lean
@@ -0,0 +1,31 @@
+/-
+Copyright (c) 2023 David Renshaw. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Renshaw
+-/
+
+import Mathlib.Tactic
+
+import ProblemExtraction
+
+problem_file
+
+/-!
+# USA Mathematical Olympiad 2001, Problem 3
+
+Let a,b,c ≥ 0 be real numbers satsifying
+
+        a² + b² + c² + abc = 4.
+
+Show that
+
+        0 ≤ ab + bc + ca - abc ≤ 2.
+-/
+
+namespace Usa2001P3
+
+problem usa2001_p3 (a b c : ℝ) (ha : 0 ≤ a) (hb : 0 ≤ b) (hc : 0 ≤ c)
+    (h : a^2 + b^2 + c^2 + a * b * c = 4) :
+    0 ≤ a * b + b * c + c * a - a * b * c ∧
+    a * b + b * c + c * a - a * b * c ≤ 2 := by
+  sorry
