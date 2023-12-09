271c68bce3c6a93ff6e350dad3978e60beb96f9e
David Renshaw <dwrenshaw@gmail.com>
2023-12-09 08:14:34 -0500

    [Imo1989P5] add missing copyright header

--- Compfiles/Imo1989P5.lean
+++ Compfiles/Imo1989P5.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2023 David Renshaw. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Renshaw
+-/
+
 import Mathlib.Algebra.IsPrimePow
 import Mathlib.Data.Nat.ModEq
 import Mathlib.Data.Nat.Prime
