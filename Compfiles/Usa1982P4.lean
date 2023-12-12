fc654aa342595aaac0e46d08c3654d96066f6442
David Renshaw <dwrenshaw@gmail.com>
2023-12-12 10:36:40 -0500

    [Usa1982P4] add problem statement

---
+++ Compfiles/Usa1982P4.lean
@@ -0,0 +1,29 @@
+/-
+Copyright (c) 2023 David Renshaw. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Renshaw
+-/
+
+import Mathlib.Tactic
+
+import ProblemExtraction
+
+problem_file
+
+/-!
+# USA Mathematical Olympiad 1982, Problem 4
+
+Prove that there exists a positive integer k such that
+k⬝2ⁿ + 1 is composite for every integer n.
+-/
+
+namespace Usa1982P4
+
+problem usa1982_p4 :
+    ∃ k : ℕ, 0 < k ∧ ∀ n : ℕ, ¬ Prime (k * (2 ^ n) + 1) := by
+  -- solution from
+  -- https://artofproblemsolving.com/wiki/index.php/1982_USAMO_Problems/Problem_4
+  refine ⟨2935363331541925531, by norm_num, ?_⟩
+  intro n
+  sorry
+
