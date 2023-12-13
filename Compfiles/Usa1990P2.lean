5728932309c81074f3f1dd6d73cb87104f298d7d
David Renshaw <dwrenshaw@gmail.com>
2023-12-12 20:53:57 -0500

    [Usa1990P2] finished

--- Compfiles/Usa1990P2.lean
+++ Compfiles/Usa1990P2.lean
@@ -39,24 +39,34 @@ problem usa1990_p2 (n : ℕ) (x : ℝ) : x ∈ solution_set n ↔ f n x = 2 * x 
     · norm_num [f]
     · unfold f; positivity
 
+  have hfnp : ∀ n x, 0 < f n x := fun n x ↦ by
+    induction' n with n ih
+    · norm_num [f]
+    · unfold f
+      positivity
+
   have hx : ∀ n x, f n x = 2 * x → 0 ≤ x := fun n x ↦ by
     specialize hfnn n x
     intro h1
     rw [h1] at hfnn
     linarith
 
-  suffices H : ((4 < x → f n x < 2 * x) ∧ (x = 4 → f n x = 2 * x) ∧
+  suffices H : ∀ x, 0 ≤ x → ((4 < x → f n x < 2 * x) ∧ (x = 4 → f n x = 2 * x) ∧
                      (x < 4 → 2 * x < f n x)) by
     constructor
-    · aesop
+    · intro hxx
+      rw [Set.mem_singleton_iff] at hxx
+      have := H 4 (by norm_num)
+      aesop
     · intro h
+      replace hx := hx n x h
+      replace H := H x hx
       by_contra! H2
       rw [Set.mem_singleton_iff] at H2
       obtain h1 | h2 | h3 := lt_trichotomy x 4 <;> aesop
-  revert x
   induction n with
   | zero =>
-    intro x
+    intro x hx5
     simp only [f]
     obtain h1 | rfl | h3 := lt_trichotomy x 4
     · exact ⟨fun h1' ↦ ((lt_asymm h1) h1').elim,
@@ -68,12 +78,12 @@ problem usa1990_p2 (n : ℕ) (x : ℝ) : x ∈ solution_set n ↔ f n x = 2 * x 
              fun h1' ↦ by linarith⟩
 
   | succ n ih =>
-    intro x
+    intro x hx5
     refine ⟨?_, ?_, ?_⟩
     · intro hx1
       have h2 : f n x < 2 * x := by
         specialize ih x
-        exact ih.1 hx1
+        exact (ih hx5).1 hx1
       have hc :=
         calc x ^ 2 + 6 * f n x < x ^ 2 + 6 * (2 * x) := by gcongr
              _ = x ^ 2 + 3 * (4 * x) := by ring
@@ -90,7 +100,7 @@ problem usa1990_p2 (n : ℕ) (x : ℝ) : x ∈ solution_set n ↔ f n x = 2 * x 
     · intro hx1
       have h2 : f n x = 2 * x := by
         specialize ih x
-        exact ih.2.1 hx1
+        exact (ih hx5).2.1 hx1
       unfold f
       have hc :=
         calc x ^ 2 + 6 * f n x = x ^ 2 + 6 * (2 * x) := by rw [h2]
@@ -110,20 +120,25 @@ problem usa1990_p2 (n : ℕ) (x : ℝ) : x ∈ solution_set n ↔ f n x = 2 * x 
     · intro hx1
       have h2 : 2 * x < f n x := by
         specialize ih x
-        exact ih.2.2 hx1
-      have hxnn : 0 ≤ x := by
-        sorry
-      have hxp : 0 < x := sorry
-      have hc :=
-/-
-        calc x ^ 2 + 6 * f n x > x ^ 2 + 6 * (2 * x) := by gcongr
-             _ = x ^ 2 + 3 * (4 * x) := by ring
-             _ > x ^ 2 + 3 * (x * x) := by sorry
-             _ = 4 * x^2 := by ring -/
+        exact (ih hx5).2.2 hx1
+      obtain rfl | hx6 := LE.le.eq_or_gt hx5
+      · simp [hfnp]
 
+      have hc :=
         calc 4 * x^2 = x ^ 2 + 3 * (x * x) := by ring
                    _ < x ^ 2 + 3 * (4 * x) := by gcongr
                    _ = x ^ 2 + 6 * (2 * x) := by ring
                    _ < x ^ 2 + 6 * f n x := by gcongr
 
-      sorry
+      have hc' : Real.sqrt (4 * x ^ 2) < Real.sqrt (x ^ 2 + 6 * f n x) := by
+        have h4 : 0 < x ^ 2 + 6 * f n x := by
+          have := hfnp n x
+          positivity
+        exact (Real.sqrt_lt_sqrt_iff_of_pos h4).mpr hc
+
+      have h5 : Real.sqrt (4 * x^2) = 2 * x := by
+        rw [show 4 * x^2 = (2 * x)^2 by ring]
+        have h6 : 0 ≤ 2 * x := by positivity
+        exact Real.sqrt_sq h6
+
+      rwa [h5] at hc'
