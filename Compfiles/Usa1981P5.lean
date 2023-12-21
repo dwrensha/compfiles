6962a996df607cc516efa64521148adbdfbc318f
David Renshaw <dwrenshaw@gmail.com>
2023-12-20 21:33:54 -0500

    [Usa1981P5] faster

--- Compfiles/Usa1981P5.lean
+++ Compfiles/Usa1981P5.lean
@@ -36,10 +36,15 @@ problem usa1981_p5 (x : ℝ) (n : ℕ) :
   simp_rw [←Int.self_sub_fract, sub_div, mul_div_right_comm]
   rw [Finset.sum_sub_distrib]
   have h1 : ∀ x1 ∈ Finset.Icc 1 n, (x1 : ℝ) / (x1 : ℝ) * x = x := fun x1 hx ↦ by
-    have h3 : (x1:ℝ) ≠ 0 := by sorry --aesop
+    have h3 : (x1:ℝ) ≠ 0 := by
+      rw [Finset.mem_Icc] at hx; replace hx := hx.1;
+      intro hx1
+      obtain rfl : x1 = 0 := Nat.cast_eq_zero.mp hx1
+      exact Nat.not_succ_le_zero 0 hx
     rw [div_self h3, one_mul]
   rw [Finset.sum_congr rfl h1]
-  have h2 : ∑ x_1 in Finset.Icc 1 n, x = n * x := by sorry --aesop
+  have h2 : ∑ x_1 in Finset.Icc 1 n, x = n * x := by
+    rw [Finset.sum_const, Nat.card_Icc, add_tsub_cancel_right, nsmul_eq_mul]
   rw [h2]
   suffices H : Int.fract (↑n * x) ≤
                ∑ x_1 in Finset.Icc 1 n, Int.fract (↑x_1 * x) / ↑x_1 from
@@ -65,7 +70,8 @@ problem usa1981_p5 (x : ℝ) (n : ℕ) :
     simp
 
   -- Let's take $m$ such that $\frac{a_m}{m}$ is minimal.
-  have : Nonempty (Finset.Icc 1 n) := by use 1; sorry --aesop
+  have : Nonempty (Finset.Icc 1 n) := by
+    use 1; rw [Finset.mem_Icc]; simp only [le_refl, true_and]; exact H
   obtain ⟨⟨m, hm1⟩, hm2⟩ :=
     Finite.exists_min (fun (m : Finset.Icc 1 n) ↦ a m / m)
 
@@ -77,7 +83,10 @@ problem usa1981_p5 (x : ℝ) (n : ℕ) :
     have h7 : ∑ ii in Finset.Icc 1 m, a m / ↑m ≤
               ∑ ii in Finset.Icc 1 m, a ii / ↑ii := Finset.sum_le_sum h6
     have h8 : a m = ∑ ii in Finset.Icc 1 m, a m / ↑m := by
-      have h9 : (m:ℝ) ≠ 0 := by sorry --aesop
+      have h9 : (m:ℝ) ≠ 0 := by
+        intro hm5
+        obtain rfl : m = 0 := Nat.cast_eq_zero.mp hm5
+        exact Nat.not_succ_le_zero 0 hm3
       rw [Finset.sum_const, Nat.card_Icc, add_tsub_cancel_right, nsmul_eq_mul]
       exact (mul_div_cancel' _ h9).symm
     rw [h8]
@@ -85,5 +94,3 @@ problem usa1981_p5 (x : ℝ) (n : ℕ) :
 
   have h9 := ih (n - m) (Nat.sub_lt H hm3)
   sorry
-
-
