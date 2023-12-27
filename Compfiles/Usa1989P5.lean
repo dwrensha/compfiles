7d9d5fda0df904ea0348132fad170f8fbd418e68
David Renshaw <dwrenshaw@gmail.com>
2023-12-26 19:44:54 -0500

    [Usa1989P5] fix copy/paste error

--- Compfiles/Usa1989P5.lean
+++ Compfiles/Usa1989P5.lean
@@ -13,7 +13,6 @@ problem_file
 /-!
 # USA Mathematical Olympiad 1989, Problem 5
 
-
 Let u and v be real numbers such that
 
 (u + u² + u³ + ⋯ + u⁸) + 10u⁹ = (v + v² + v³ + ⋯ + v¹⁰) + 10v¹¹ = 8.
@@ -30,7 +29,7 @@ determine u_is_larger : Bool := sorry
 problem usa1989_p5
     (u v : ℝ)
     (hu : (∑ i in Finset.range 8, u^(i + 1)) + 10 * u^9 = 8)
-    (hv : (∑ i in Finset.range 8, v^(i + 1)) + 10 * v^11 = 8) :
+    (hv : (∑ i in Finset.range 10, v^(i + 1)) + 10 * v^11 = 8) :
     if u_is_larger then v < u else v < u := by
   -- solution from
   -- https://artofproblemsolving.com/wiki/index.php/1989_USAMO_Problems/Problem_5
