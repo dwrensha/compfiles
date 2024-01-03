2656b2e9f572e9d9afdcd894408d8a3847003fca
David Renshaw <dwrenshaw@gmail.com>
2024-01-03 18:49:03 -0500

    [Usa1985P1] positivity bug?

--- Compfiles/Usa1985P1.lean
+++ Compfiles/Usa1985P1.lean
@@ -26,9 +26,11 @@ namespace Usa1985P1
 
 open scoped BigOperators
 
-determine does_exist : Bool := false
+determine does_exist : Bool := true
 
 abbrev is_valid (x : ℕ → ℤ) (y z : ℤ) : Prop :=
+    (∀ i ∈ Finset.range 1985, 0 < x i) ∧
+    0 < y ∧ 0 < z ∧
     ∑ i in Finset.range 1985, x i ^ 2 = y ^ 3 ∧
     ∑ i in Finset.range 1985, x i ^ 3 = z ^ 2 ∧
     ∀ i ∈ Finset.range 1985, ∀ j ∈ Finset.range 1985, i ≠ j → x i ≠ x j
@@ -37,4 +39,21 @@ problem usa1985_p1 :
     if does_exist
     then ∃ x y z, is_valid x y z
     else ¬ ∃ x y z, is_valid x y z := by
-  sorry
+  -- solution from
+  -- https://artofproblemsolving.com/wiki/index.php/1985_USAMO_Problems/Problem_1
+  simp only [ite_true]
+  let j := ∑ ii in Finset.range 1985, (ii + 1)
+  let k := ∑ ii in Finset.range 1985, (ii + 1)^2
+  let x := fun (ii : ℕ) ↦ ((ii + 1):ℤ) * k ^ 4
+  use x, k, j
+  refine ⟨?_, ?_, ?_, ?_, ?_, ?_⟩
+  · intro ii hii
+    dsimp [x]
+    positivity
+    sorry -- comment this out and you get "max recursion depth reached"
+
+  · sorry
+  · sorry
+  · sorry
+  · sorry
+  · sorry
