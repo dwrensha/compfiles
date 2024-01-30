79c5a386f91f6102a57bda0455fe3fd0297dc249
David Renshaw <dwrenshaw@gmail.com>
2024-01-30 07:54:13 -0500

    more missing copyright headers

--- Compfiles/Poland1998P4.lean
+++ Compfiles/Poland1998P4.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2023 David Renshaw. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Renshaw
+-/
+
 import Mathlib.Data.Nat.Basic
 import Mathlib.Data.Nat.ModEq
 import Mathlib.Data.ZMod.Basic
