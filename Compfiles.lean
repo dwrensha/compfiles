2722dd0b5e7a38b3aff0fd88f786ceec7fed894f
David Renshaw <dwrenshaw@gmail.com>
2024-01-30 09:02:06 -0500

    [Imo1959P2] import from mathlib4/Archive

--- Compfiles.lean
+++ Compfiles.lean
@@ -9,6 +9,7 @@ import Compfiles.Canada1998P5
 import Compfiles.CIIM2022P6
 import Compfiles.Hungary1998P6
 import Compfiles.Imo1959P1
+import Compfiles.Imo1959P2
 import Compfiles.Imo1961P1
 import Compfiles.Imo1962P4
 import Compfiles.Imo1964P1
