eadb8d0dcd8cc3895c39e7d7454ca3240cd1bcaf
David Renshaw <dwrenshaw@gmail.com>
2024-02-11 22:46:15 -0500

    [Imo1992P2] finish proof

--- Compfiles/Imo1992P2.lean
+++ Compfiles/Imo1992P2.lean
@@ -58,4 +58,51 @@ problem imo1992_p2 (f : ℝ → ℝ) :
     have h3' := hf x 0
     simp only [h1, zero_add, add_zero] at h3'
     exact h3'
-  sorry
+  have h4 : ∀ x y, f (x^2 + y) = f y + (f x)^2 := fun x y ↦ by
+    have h4' := hf x (f y)
+    rw [h2] at h4'
+    exact h4'
+  have h5 : ∀ x, 0 ≤ x → ∀ y, f (x + y) = f x + f y := fun x hx y ↦ by
+    calc _ = f ((Real.sqrt x)^2 + y) := by rw[Real.sq_sqrt hx]
+         _ = f y + f (Real.sqrt x)^2 := h4 (Real.sqrt x) y
+         _ = f y + f (Real.sqrt x^2) := by rw [h3]
+         _ = f y + f x := by rw[Real.sq_sqrt hx]
+         _ = _ := add_comm _ _
+  have h6 : ∀ x, f (-x) = - f x := fun x ↦ by
+    obtain hx | hx := le_total 0 x
+    · have h6' := h5 x hx (-x)
+      rw [add_right_neg, h1] at h6'
+      exact eq_neg_of_add_eq_zero_right h6'.symm
+    · have hx' : 0 ≤ -x := by linarith
+      have h6' := h5 (-x) hx' x
+      rw [add_left_neg, h1] at h6'
+      exact add_eq_zero_iff_eq_neg.mp h6'.symm
+  have h7 : ∀ x y, f (x + y) = f x + f y := fun x y ↦ by
+    obtain hx | hx := le_total 0 x
+    · exact h5 x hx y
+    · have hx' : 0 ≤ -x := by linarith
+      have h8 := h5 (-x) hx' (-y)
+      rw [show -x + -y = - (x + y) by ring, h6, h6, h6] at h8
+      linarith only [h8]
+  have h8 : ∀ x y, f (x - y) = f x - f y := fun x y ↦ by
+    rw [show x - y = x + -y by ring, show f x - f y = f x + -f y by ring, ← h6]
+    exact h7 x (-y)
+  rw [Set.mem_singleton_iff]
+  ext x
+  by_contra H
+  obtain ⟨z, hz⟩ : ∃ z, 0 < z ∧ f z < 0 := by
+    obtain h9 | h9 := Ne.lt_or_lt H
+    · use x - f x
+      constructor
+      · linarith
+      · rw [h8, h2]; linarith
+    · use f x - x
+      constructor
+      · linarith
+      · rw [h8, h2]; linarith
+  have h10 :=
+    calc f z
+       = f (Real.sqrt z ^ 2) := by rw[Real.sq_sqrt (le_of_lt hz.1)]
+     _ = f (Real.sqrt z)^2 := h3 (Real.sqrt z)
+     _ ≥ 0 := sq_nonneg _
+  linarith only [h10, hz]
