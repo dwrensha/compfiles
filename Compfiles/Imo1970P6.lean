e37c88862db0a8baf19a0552ef0f44b442690b6b
David Renshaw <dwrenshaw@gmail.com>
2024-02-21 09:27:31 -0500

    [Imo1970P6] add problem statement

---
+++ Compfiles/Imo1970P6.lean
@@ -0,0 +1,46 @@
+/-
+Copyright (c) 2024 David Renshaw. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Renshaw
+-/
+
+import Mathlib.Analysis.InnerProductSpace.PiL2
+import Mathlib.Analysis.NormedSpace.PiLp
+import Mathlib.Geometry.Euclidean.Basic
+import Mathlib.Geometry.Euclidean.Inversion.Basic
+import Mathlib.Geometry.Euclidean.Sphere.Basic
+import Mathlib.Geometry.Euclidean.Triangle
+
+import ProblemExtraction
+
+problem_file { tags := [.Combinatorics] }
+
+/-!
+# International Mathematical Olympiad 1970, Problem 6
+
+In a plane there are 100 points, no three of which are collinear.
+Consider all possible triangles having these points as vertices.
+Prove that no more that 70% of these triangles are acute.
+-/
+
+namespace Imo1970P6
+
+open scoped EuclideanGeometry
+
+abbrev Pt := EuclideanSpace ℝ (Fin 2)
+
+def AcuteTriangle (T : Affine.Triangle ℝ Pt) : Prop :=
+  ∠ (T.points 1) (T.points 2) (T.points 3) < Real.pi / 2 ∧
+  ∠ (T.points 2) (T.points 3) (T.points 1) < Real.pi / 2 ∧
+  ∠ (T.points 3) (T.points 1) (T.points 2) < Real.pi / 2
+
+problem imo1970_p6
+    (P : Fin 100 → Pt)
+    (hP : ∀ a b c : Fin 100,
+             List.Nodup [a, b, c] → ¬ Collinear ℝ {P c, P b, P c}) :
+    let cardAll := Nat.card { t : Affine.Triangle ℝ Pt | ∃ a b c : Fin 100, ![P a, P b, P c] = t.points }
+    let cardAcute :=
+      Nat.card { t : Affine.Triangle ℝ Pt | ∃ a b c : Fin 100, ![P a, P b, P c] = t.points ∧
+                                            AcuteTriangle t }
+    (cardAcute : ℚ) / cardAll ≤ 7 / 10 := by
+  sorry
