4d3ee3d143068ca8e6a169ecaeab7f0a0c670127
David Renshaw <dwrenshaw@gmail.com>
2024-02-22 17:20:29 -0500

    [Imo1971P5] add problem statement

--- Compfiles.lean
+++ Compfiles.lean
@@ -24,6 +24,7 @@ import Compfiles.Imo1968P5
 import Compfiles.Imo1969P1
 import Compfiles.Imo1970P4
 import Compfiles.Imo1970P6
+import Compfiles.Imo1971P5
 import Compfiles.Imo1972P5
 import Compfiles.Imo1974P5
 import Compfiles.Imo1975P1
---
+++ Compfiles/Imo1971P5.lean
@@ -0,0 +1,32 @@
+/-
+Copyright (c) 2024 David Renshaw. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Renshaw
+-/
+
+import Mathlib.Geometry.Euclidean.Basic
+import Mathlib.Geometry.Euclidean.Triangle
+
+import ProblemExtraction
+
+problem_file { tags := [.Combinatorics] }
+
+/-!
+# International Mathematical Olympiad 1971, Problem 5
+
+Prove that for every natural number m there exists a finite set S of
+points in the plane with the following property:
+For every point s in S, there are exactly m points which are at a unit
+distance from s.
+-/
+
+namespace Imo1971P5
+
+open scoped EuclideanGeometry
+
+abbrev Pt := EuclideanSpace ℝ (Fin 2)
+
+problem imo1971_p5 (m : ℕ) :
+    ∃ S : Set Pt, S.Finite ∧ ∀ s ∈ S, Nat.card {t | dist s t = 1} = m := by
+  -- https://prase.cz/kalva/imo/isoln/isoln715.html
+  sorry
