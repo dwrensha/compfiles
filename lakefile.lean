e50039ce4a10621c4d16806e3fc0ad35565869a8
David Renshaw <dwrenshaw@gmail.com>
2024-02-22 08:50:43 -0500

    update mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -30,4 +30,4 @@ lean_exe checkSolution where
   root := `scripts.checkSolution
   supportInterpreter := true
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "65d9c6c4d7f2bad2aac31291c2a7dccee4ea8a35"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "1443e142be0d004f0860b96cf9b118a9597a2e2b"
