758c3c69dbf391c0c9fc2e2479388dc00596aa74
David Renshaw <dwrenshaw@gmail.com>
2024-02-28 11:41:29 -0500

    [Imo2008P2] import from mathlib

--- Compfiles.lean
+++ Compfiles.lean
@@ -61,6 +61,7 @@ import Compfiles.Imo2006P3
 import Compfiles.Imo2006P5
 import Compfiles.Imo2007P1
 import Compfiles.Imo2007P5
+import Compfiles.Imo2008P2
 import Compfiles.Imo2008P4
 import Compfiles.Imo2009P5
 import Compfiles.Imo2010P1
