22b6086e7173f556bd1e68f00bf252215975856f
David Renshaw <dwrenshaw@gmail.com>
2024-03-06 11:38:40 -0500

    [Imo2011P3] shorten some hypothesis names

--- Compfiles/Imo2011P3.lean
+++ Compfiles/Imo2011P3.lean
@@ -35,16 +35,16 @@ problem imo2011_p3 (f : ℝ → ℝ) (hf : ∀ x y, f (x + y) ≤ y * f x + f (f
            _ ≤ (t - x) * f x + f (f x)     := hf x (t - x)
            _ = t * f x - x * f x + f (f x) := by rw [sub_mul]
 
-  have h_ab_combined : ∀ a b, a * f a + b * f b ≤ 2 * f a * f b := by
+  have hab : ∀ a b, a * f a + b * f b ≤ 2 * f a * f b := by
     intro a b
     linarith [hxt b (f a), hxt a (f b)]
 
-  have h_f_nonneg_of_pos : ∀ a < 0, 0 ≤ f a := by
+  have f_nonneg_of_pos : ∀ a < 0, 0 ≤ f a := by
     intro a han
-    have : a * f a ≤ 0 := add_le_iff_nonpos_left.mp (h_ab_combined a (2 * f a))
+    have : a * f a ≤ 0 := add_le_iff_nonpos_left.mp (hab a (2 * f a))
     exact nonneg_of_mul_nonpos_right this han
 
-  have h_f_nonpos : ∀ x, f x ≤ 0 := by
+  have f_nonpos : ∀ x, f x ≤ 0 := by
     intro x
     by_contra! hp
     -- If we choose a small enough argument for f, then we get a contradiction.
@@ -52,7 +52,7 @@ problem imo2011_p3 (f : ℝ → ℝ) (hf : ∀ x y, f (x + y) ≤ y * f x + f (f
     have hm : min 0 s - 1 < s := (sub_one_lt _).trans_le (min_le_right 0 s)
     have hml : min 0 s - 1 < 0 := (sub_one_lt _).trans_le (min_le_left 0 s)
     suffices f (min 0 s - 1) < 0 from
-      not_le.mpr this (h_f_nonneg_of_pos (min 0 s - 1) hml)
+      not_le.mpr this (f_nonneg_of_pos (min 0 s - 1) hml)
 
     calc f (min 0 s - 1)
          ≤ (min 0 s - 1) * f x - x * f x + f (f x) := hxt x (min 0 s - 1)
@@ -60,15 +60,15 @@ problem imo2011_p3 (f : ℝ → ℝ) (hf : ∀ x y, f (x + y) ≤ y * f x + f (f
                by linarith [(mul_lt_mul_right hp).mpr hm]
        _ = 0 := by rw [(eq_div_iff hp.ne.symm).mp rfl]; linarith
 
-  have h_fx_zero_of_neg : ∀ x < 0, f x = 0 := by
+  have f_zero_of_neg : ∀ x < 0, f x = 0 := by
     intro x hxz
-    exact (h_f_nonpos x).antisymm (h_f_nonneg_of_pos x hxz)
+    exact (f_nonpos x).antisymm (f_nonneg_of_pos x hxz)
 
   intro x hx
   obtain (h_x_neg : x < 0) | (rfl : x = 0) := hx.lt_or_eq
-  · exact h_fx_zero_of_neg _ h_x_neg
-  · suffices 0 ≤ f 0 from (h_f_nonpos 0).antisymm this
-    have hno : f (-1) = 0 := h_fx_zero_of_neg (-1) neg_one_lt_zero
+  · exact f_zero_of_neg _ h_x_neg
+  · suffices 0 ≤ f 0 from (f_nonpos 0).antisymm this
+    have hno : f (-1) = 0 := f_zero_of_neg (-1) neg_one_lt_zero
     have hp := hxt (-1) (-1)
     rw [hno, mul_zero, sub_zero, zero_add] at hp
     exact hp
