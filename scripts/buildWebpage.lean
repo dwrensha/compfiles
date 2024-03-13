76e53cf2888f4ab589b6be02b885d1f28f1fb487
David Renshaw <dwrenshaw@gmail.com>
2024-03-12 21:52:59 -0400

    generate imo.html page

--- scripts/buildWebpage.lean
+++ scripts/buildWebpage.lean
@@ -1,3 +1,4 @@
+import Std.Data.RBMap.Basic
 import Std.Data.String.Basic
 import Std.Lean.Util.Path
 import Std.Tactic.Lint
@@ -8,6 +9,31 @@ import Lean.Meta.Basic
 import ProblemExtraction
 
 open Lean Core Elab Command Std.Tactic.Lint
+
+def imoProblemCounts :=
+  [(2023, 6), (2022, 6), (2021, 6), (2020, 6),
+   (2019, 6), (2018, 6), (2017, 6), (2016, 6), (2015, 6),
+   (2014, 6), (2013, 6), (2012, 6), (2011, 6), (2010, 6),
+   (2009, 6), (2008, 6), (2007, 6), (2006, 6), (2005, 6),
+   (2004, 6), (2003, 6), (2002, 6), (2001, 6), (2000, 6),
+   (1999, 6), (1998, 6), (1997, 6), (1996, 6), (1995, 6),
+   (1994, 6), (1993, 6), (1992, 6), (1991, 6), (1990, 6),
+   (1989, 6), (1988, 6), (1987, 6), (1986, 6), (1985, 6),
+   (1984, 6), (1983, 6), (1982, 6), (1981, 6), --no contest in 1980
+   (1979, 6), (1978, 6), (1977, 6), (1976, 6), (1975, 6),
+   (1974, 6), (1973, 6), (1972, 6), (1971, 6), (1970, 6),
+   (1969, 6), (1968, 6), (1967, 6), (1966, 6), (1965, 6),
+   (1964, 6), (1963, 6), (1962, 7), (1961, 6), (1960, 7),
+   (1959, 6)]
+
+def totalImoProblemCount : Id Nat := do
+  let mut result := 0
+  for ⟨_, c⟩ in imoProblemCounts do
+    result := result + c
+  return result
+
+def aopsImoUrl (year : Nat) (idx : Nat) : String :=
+  s!"https://artofproblemsolving.com/wiki/index.php/{year}_IMO_Problems/Problem_{idx}"
 
 structure ProblemInfo where
   name : String
@@ -162,8 +188,17 @@ unsafe def main (_args : List String) : IO Unit := do
       h.putStr "<thead><tr><th>problem</th><th>solved?</th><th>tags</th></tr></thead>"
       h.putStr "<tbody>"
       let infos' := sortProblems infos
+      let mut infomap := Std.mkRBMap String ProblemInfo Ord.compare
+      let mut imoSolvedCount := 0
+      let mut imoFormalizedCount := 0
 
       for info in infos' do
+        infomap := infomap.insert info.name info
+        if info.name.startsWith "Imo" ∧ info.name.get ⟨3⟩ ∈ ['1', '2'] then
+          imoFormalizedCount := imoFormalizedCount + 1
+          if info.proved then
+            imoSolvedCount := imoSolvedCount + 1
+          pure ()
         h.putStr s!"<tr>"
 
         -- problem name
@@ -187,6 +222,38 @@ unsafe def main (_args : List String) : IO Unit := do
           h.putStr s!"<span class=\"problem-tag {problemTagClass tg}\">{tg}</span>"
         h.putStr "</td>"
         h.putStr "</tr>"
+      h.putStr "</tbody>"
       h.putStr "</table>"
+      h.putStr "</body></html>"
+
+      let h ← IO.FS.Handle.mk "_site/imo.html" IO.FS.Mode.write
+      h.putStrLn <| ←htmlHeader "IMO problems"
+      h.putStrLn <| "<h2>IMO problems formalized in Lean</h2>"
+      h.putStrLn <| s!"<p>There are {totalImoProblemCount} total IMO problems.</p>"
+      let formalizedPercent : Float := 100.0 *
+        (OfNat.ofNat imoFormalizedCount) / (OfNat.ofNat totalImoProblemCount)
+      h.putStrLn <| s!"<p>{imoFormalizedCount} problems have been formalized ({formalizedPercent}%).</p>"
+      let solvedPercent : Float := 100.0 *
+        (OfNat.ofNat imoSolvedCount) / (OfNat.ofNat totalImoProblemCount)
+      h.putStrLn <| s!"<p>{imoSolvedCount} problems have complete formalized solutions ({solvedPercent}%).</p>"
+      h.putStr "<table class=\"imo-problems\">"
+      for ⟨year, count⟩ in imoProblemCounts do
+        h.putStr s!"<tr><td class=\"year\">{year}</td>"
+        for ii in List.range count do
+          let idx := ii + 1
+          let name := s!"Imo{year}P{idx}"
+          let ⟨url, cls⟩ ← match infomap.find? name with
+          | .some info =>
+            let url := s!"problems/Compfiles.{name}.html"
+            let cls := if info.proved then "proved" else "formalized"
+            pure (url, cls)
+          | .none =>
+            pure (aopsImoUrl year idx, "todo")
+
+          h.putStr s!"<td class=\"{cls}\"><a href=\"{url}\">P{idx}</a></td>"
+          pure ()
+        h.putStrLn "</tr>"
+      h.putStr "</table>"
+
       h.putStr "</body></html>"
       pure ()
