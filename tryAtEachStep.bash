70fadc2e00c83a44085e89059c54db692736fa93
David Renshaw <dwrenshaw@gmail.com>
2024-03-21 13:15:00 -0400

    add tryAtEachStep script, to run it on all files

---
+++ tryAtEachStep.bash
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+
+set -x
+
+TACTIC="${1:-exact?}"
+
+OUTFILE=~/Desktop/tryAtEachStep.txt
+
+rm $OUTFILE
+
+for f in `find Compfiles -name "*.lean" | shuf`
+do
+    echo >> $OUTFILE
+    echo "FILE : " $f >> $OUTFILE
+    lake exe tryAtEachStep $TACTIC $f >> $OUTFILE
+done
