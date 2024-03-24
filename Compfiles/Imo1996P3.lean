6f1c10abd29d95b612edd14ee37eeceff5e123dc
David Renshaw <dwrenshaw@gmail.com>
2024-03-24 13:27:20 -0400

    [Imo1996P3] add problem statement

---
+++ Compfiles/Imo1996P3.lean
@@ -0,0 +1,30 @@
+/-
+Copyright (c) 2023 The Compfiles Authors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors:
+-/
+
+import Mathlib.Tactic
+
+import ProblemExtraction
+
+problem_file { tags := [.Algebra] }
+
+/-!
+# International Mathematical Olympiad 1996, Problem 3
+
+Let S denote the set of nonnegative integers. Find
+all functions f from S to itself such that
+
+ f(m + f(n)) = f(f(m)) + f(n)
+
+for all m,n in S.
+-/
+
+namespace Imo1996P3
+
+determine SolutionSet : Set (ℕ → ℕ) := sorry
+
+problem imo1996_p3 (f : ℕ → ℕ):
+    f ∈ SolutionSet ↔ ∀ m n, f (m + f n) = f (f m) + f n := by
+  sorry
