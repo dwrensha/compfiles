bde24fb2a935405cf20324cd6be39f80de9b4ed2
David Renshaw <dwrenshaw@gmail.com>
2024-03-24 14:06:19 -0400

    [Imo2010P3] update copyright header and naming style

--- Compfiles/Imo2010P3.lean
+++ Compfiles/Imo2010P3.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2023 David Renshaw. All rights reserved.
+Copyright (c) 2023 The Compfiles Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: David Renshaw
+Authors:
 -/
 
 import Mathlib.Tactic
@@ -26,11 +26,10 @@ abbrev PosInt : Type := { x : ℤ // 0 < x }
 
 notation "ℤ>0" => PosInt
 
-determine solution_set : Set (ℤ>0 → ℤ>0) := { f | f = id ∨ ∃ c, ∀ x, f x = x + c }
-
+determine SolutionSet : Set (ℤ>0 → ℤ>0) := { f | f = id ∨ ∃ c, ∀ x, f x = x + c }
 
 problem imo2010_p3 (g : ℤ>0 → ℤ>0) :
-    g ∈ solution_set ↔ ∀ m n, IsSquare ((g m + n) * (g n + m)) := by
+    g ∈ SolutionSet ↔ ∀ m n, IsSquare ((g m + n) * (g n + m)) := by
   constructor
   · rintro (rfl | ⟨c, hc⟩) m n
     · use m + n; rw [id, id]; ac_rfl
