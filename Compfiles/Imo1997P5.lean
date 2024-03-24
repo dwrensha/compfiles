20c976adfa035b954d47f427f9608fc63d226f01
David Renshaw <dwrenshaw@gmail.com>
2024-03-24 13:20:19 -0400

    remove self form Usa1982P2 authors, and start leaving 'authors' blank on stubs

--- Compfiles/Imo1997P5.lean
+++ Compfiles/Imo1997P5.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2023 David Renshaw. All rights reserved.
+Copyright (c) 2023 The Compfiles Authors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: David Renshaw
+Authors:
 -/
 
 import Mathlib.Tactic
