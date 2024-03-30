c97fa7c1cd81b86136c561525eb223373ef0e7ae
Zheng Yuan <mike29@126.com>
2024-03-30 16:24:43 +0800

    Update Imo1993P5.lean
    Fix problem description and problem formal statement with f(f(n)) = f(n) + n instead of f(n) + 2; andd add f(n+1) > f(n)

--- Compfiles/Imo1993P5.lean
+++ Compfiles/Imo1993P5.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 The Compfiles Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors:
+Authors: Zheng Yuan
 -/
 
 import Mathlib.Tactic
@@ -16,14 +16,15 @@ problem_file { tags := [.Algebra] }
 Let ℕ+ be the set of positive integers. Determine whether
 there exists a function f : ℕ+ → ℕ+ such that
   i) f(1) = 2
-  ii) f(f(n)) = f(n) + 2 for all n.
+  ii) f(f(n)) = f(n) + n for all n
+  iii) f(n + 1) > f(n) for all n.
 -/
 
 namespace Imo1993P5
 
 determine DoesExist : Bool := sorry
 
-abbrev Good (f : ℕ+ → ℕ+) : Prop := f 1 = 2 ∧ ∀ n, f (f n) = f n + 2
+abbrev Good (f : ℕ+ → ℕ+) : Prop := f 1 = 2 ∧ ∀ n, f (f n) = f n + n ∧ ∀ n, f n < f (n + 1)
 
 problem imo1993_p5 :
     if DoesExist then ∃ f, Good f else ¬∃ f, Good f := by
