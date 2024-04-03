2a99bb83aaed9dada88f009f5da4b89e420c9d38
David Renshaw <dwrenshaw@gmail.com>
2024-04-02 21:18:39 -0400

    [Imo1972P1] add proof

--- Compfiles/Imo1972P1.lean
+++ Compfiles/Imo1972P1.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 The Compfiles Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors:
+Authors: David Renshaw
 -/
 
 import Mathlib.Tactic
@@ -24,7 +24,75 @@ open scoped BigOperators
 
 problem imo1972_p1 (S : Finset ℕ)
     (Scard : S.card = 10)
-    (Sdigits : ∀ s ∈ S, (Nat.digits 10 s).length = 2) :
+    (Sdigits : ∀ n ∈ S, (Nat.digits 10 n).length = 2) :
     ∃ S1 S2 : Finset ℕ,S1 ⊆ S ∧ S2 ⊆ S ∧
-       Disjoint S1 S2 ∧ ∑ s in S1, s = ∑ s in S2, s :=
-  by sorry
+       Disjoint S1 S2 ∧ ∑ n in S1, n = ∑ n in S2, n := by
+  -- https://prase.cz/kalva/imo/isoln/isoln721.html
+  have one_lt_ten : 1 < 10 := by norm_num
+  replace Sdigits : S ⊆ Finset.Ico 10 100 := by
+     intro n hn
+     have h2 := Sdigits n hn
+     have h3 : n < 10 ^ ((Nat.digits 10 n).length) :=
+       Nat.lt_base_pow_length_digits one_lt_ten
+
+     have h4 : n ≠ 0 := by rintro rfl; norm_num at h2
+     have h5 := Nat.base_pow_length_digits_le _ n one_lt_ten h4
+     rw [h2] at h3 h5
+     rw [sq] at h5
+     have h6 : 10 ≤ n := by omega
+     rw [Finset.mem_Ico]
+     exact ⟨h6, h3⟩
+  have h2 := Finset.card_powerset S
+  rw [Scard] at h2
+  have h3 : ∀ s ∈ Finset.powerset S, ∑ n in s, n ≤ 990 := by
+    intro s hs
+    have h4 : ∀ n ∈ s, n ≤ 99 := by
+      intro n hn
+      have h5 : n ∈ Finset.Ico 10 100 := by
+        rw [Finset.mem_powerset] at hs
+        exact Sdigits (hs hn)
+      rw [Finset.mem_Ico] at h5
+      omega
+
+    have h5 : ∑ n in s, n ≤ ∑ n in s, 99 := Finset.sum_le_sum h4
+    simp only [Finset.sum_const, smul_eq_mul, Scard] at h5
+    have h6 : s.card ≤ S.card := by
+      rw [Finset.mem_powerset] at hs
+      exact Finset.card_le_card hs
+    omega
+  let f : Finset ℕ → ℕ := fun s : Finset ℕ ↦ ∑ n in s, n
+  let t : Finset ℕ := Finset.range 991
+  have h7 : t.card = 991 := Finset.card_range _
+  have hf : ∀ s ∈ S.powerset, f s ∈ t := by
+    intro s hs
+    rw [ Finset.mem_range]
+    simp only [f]
+    exact Nat.lt_succ_of_le (h3 s hs)
+  have h8 : t.card < S.powerset.card := by omega
+  have h9 := Finset.exists_ne_map_eq_of_card_lt_of_maps_to h8 hf
+  obtain ⟨A, hA, B, hB, hAB1, hAB2⟩ := h9
+  let C := A ∩ B
+  let A' := A \ C
+  let B' := B \ C
+  refine ⟨A', B', ?_, ?_, ?_, ?_⟩
+  · have h10 : A' ⊆ A := Finset.sdiff_subset _ _
+    have h11 : A ⊆ S := by
+      intro a ha
+      aesop
+    exact h10.trans h11
+  · have h10 : B' ⊆ B := Finset.sdiff_subset _ _
+    have h11 : B ⊆ S := by
+      intro b hb
+      aesop
+    exact h10.trans h11
+  · rw[Finset.disjoint_iff_ne]
+    intro a ha b hb
+    aesop
+  · have h12 : C ⊆ A := Finset.inter_subset_left _ _
+    have h13 : C ⊆ B := Finset.inter_subset_right _ _
+    have h14 := Finset.sum_sdiff (f := id) h12
+    have h15 := Finset.sum_sdiff (f := id) h13
+    unfold_let A' B'
+    dsimp at h14 h15
+    dsimp only [f] at hAB2
+    omega
