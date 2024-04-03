c3717e5ba899f3cfa6b9933780a9c04f30cbc364
David Renshaw <dwrenshaw@gmail.com>
2024-04-03 10:21:42 -0400

    [Imo2015P2] update style

--- Compfiles/Imo2015P2.lean
+++ Compfiles/Imo2015P2.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2023 David Renshaw. All rights reserved.
+Copyright (c) 2023 The Compfiles Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: David Renshaw
+Authors:
 -/
 
 import Mathlib.Tactic
@@ -19,12 +19,12 @@ ab - c, bc - a, ca - b is a power of two.
 
 namespace Imo2012P2
 
-determine solution_set : Set (ℤ × ℤ × ℤ) := sorry
+determine SolutionSet : Set (ℤ × ℤ × ℤ) := sorry
 
 def is_power_of_two (n : ℤ) : Prop := ∃ m : ℕ, n = 2 ^ m
 
 problem imo2015_p2 (a b c : ℤ) :
-    (a,b,c) ∈ solution_set ↔
+    (a,b,c) ∈ SolutionSet ↔
       0 < a ∧ 0 < b ∧ 0 < c ∧
       is_power_of_two (a * b - c) ∧
       is_power_of_two (b * c - a) ∧
