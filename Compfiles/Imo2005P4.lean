4c80cc31422fb55c085e8a3824fe55b0875edf75
David Renshaw <dwrenshaw@gmail.com>
2024-04-04 08:06:45 -0400

    lean v4.7.0 release

--- Compfiles/Imo2005P4.lean
+++ Compfiles/Imo2005P4.lean
@@ -73,7 +73,7 @@ problem imo2005_p4 {k : ℕ} (hk : 0 < k) :
   have hp : Nat.Prime p := Nat.minFac_prime hk'
   replace h : ∀ n, 1 ≤ n → ¬(p : ℤ) ∣ a n := fun n hn ↦ by
     have : IsCoprime (a n) p :=
-      .of_isCoprime_of_dvd_right (h n hn) (Int.coe_nat_dvd.mpr k.minFac_dvd)
+      .of_isCoprime_of_dvd_right (h n hn) (Int.natCast_dvd_natCast.mpr k.minFac_dvd)
     rwa [isCoprime_comm,(Nat.prime_iff_prime_int.mp hp).coprime_iff_not_dvd] at this
   -- For `p = 2` and `p = 3`, take `n = 1` and `n = 2`, respectively
   by_cases hp2 : p = 2
