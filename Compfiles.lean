c16495af8d08db16bb53b65001ce79ea7e6810d6
David Renshaw <dwrenshaw@gmail.com>
2024-04-11 08:43:20 -0400

    [Imo2016P4] add problem statement

--- Compfiles.lean
+++ Compfiles.lean
@@ -95,6 +95,7 @@ import Compfiles.Imo2014P1
 import Compfiles.Imo2014P4
 import Compfiles.Imo2015P2
 import Compfiles.Imo2015P5
+import Compfiles.Imo2016P4
 import Compfiles.Imo2016P5
 import Compfiles.Imo2017P1
 import Compfiles.Imo2017P2
---
+++ Compfiles/Imo2016P4.lean
@@ -0,0 +1,42 @@
+/-
+Copyright (c) 2024 The Compfiles Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors:
+-/
+
+import Mathlib.Data.Set.Card
+import Mathlib.Tactic
+
+import ProblemExtraction
+
+problem_file { tags := [.NumberTheory] }
+
+/-!
+# International Mathematical Olympiad 2016, Problem 4
+
+A set of positive integers is called *fragrant* if it contains
+at least two elements and each of its elements has a prime
+factor in common with at least one of the other elements.
+Let P(n) = n² + n + 1. What is the least possible value of
+positive integer b such that there exists a non-negative integer
+a for which the set
+
+  { P(a + 1), P(a + 2), ..., P(a + b) }
+
+is fragrant?
+-/
+
+namespace Imo2016P4
+
+abbrev Fragrant (s : Set ℕ+) : Prop :=
+  2 ≤ s.ncard ∧ ∀ m ∈ s, ∃ n ∈ s, ¬Nat.Coprime m n
+
+abbrev P (n : ℕ) : ℕ := n^2 + n + 1
+
+determine Solution : ℕ+ := sorry
+
+problem imo2016_p4 :
+    IsLeast
+      {b : ℕ+ | ∃ a : ℕ, Fragrant {p | ∃ i < b, p = P (a + 1 + i)}}
+      Solution := by
+  sorry
