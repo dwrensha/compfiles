5d7ec8969b5acc9e0452133cb2166dc691e27153
David Renshaw <dwrenshaw@gmail.com>
2024-04-21 22:49:36 -0400

    [Imo2023P1] inline some proofs

--- Compfiles/Imo2023P1.lean
+++ Compfiles/Imo2023P1.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2023 David Renshaw. All rights reserved.
+Copyright (c) 2023 The Compfiles Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: David Renshaw
+Authors:
 -/
 
 import Mathlib.Tactic
@@ -24,10 +24,9 @@ determine solution_set : Set ℕ := { n | n.factors.length = 2 ∧ n.factors.Nod
 
 abbrev P (n : ℕ) : Prop :=
   let divs := n.divisors.sort LE.le
-  ∀ i, (hi2 : i + 2 < divs.length) →
-    have hi1 : i + 1 < divs.length := Nat.lt_of_succ_lt hi2
-    have hi : i < divs.length := Nat.lt_of_succ_lt hi1
-    divs.get ⟨i, hi⟩ ∣ divs.get ⟨i + 1, hi1⟩ + divs.get ⟨i + 2, hi2⟩
+  ∀ i, (h : i + 2 < divs.length) →
+    divs.get ⟨i, Nat.lt_of_succ_lt (Nat.lt_of_succ_lt h)⟩ ∣
+      divs.get ⟨i + 1, Nat.lt_of_succ_lt h⟩ + divs.get ⟨i + 2, h⟩
 
 problem imo2023_p1 : solution_set = { n | 1 < n ∧ ¬n.Prime ∧ P n } := by
   ext x
