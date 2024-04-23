2c7b1c569331812fe13bd67cefa3a1530915d955
Hongyu Ouyang <96765450+casavaca@users.noreply.github.com>
2024-04-23 02:39:03 -0700

    [Imo1972P3] Add proof

--- Compfiles/Imo1972P3.lean
+++ Compfiles/Imo1972P3.lean
@@ -1,10 +1,11 @@
 /-
 Copyright (c) 2024 The Compfiles Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors:
+Authors: Hongyu Ouyang
 -/
 
 import Mathlib.Tactic
+import Mathlib.Data.Nat.Choose.Basic
 
 import ProblemExtraction
 
@@ -26,4 +27,44 @@ open scoped Nat
 
 problem imo1972_p3 (m n : ℕ) :
     m ! * n ! * (m + n)! ∣ (2 * m)! * (2 * n)! := by
-  sorry
+  let f := fun m n => ((2 * m)! * (2 * n)!) / (m ! * n ! * (m + n)!)
+  revert m
+  induction' n with n ih
+  · intro m
+    simp
+    use Nat.choose (2*m) m
+    rw [← Nat.choose_mul_factorial_mul_factorial (n := 2 * m) (k := m)]
+    rw [show 2*m-m = m from by omega]
+    ring_nf
+    omega
+  · intro m
+    zify
+    use (4 * f m n - f (m + 1) n)
+    rify
+    unfold_let f; dsimp only
+    rw [Nat.cast_div, Nat.cast_div]
+    · rw [show 2 * Nat.succ n = Nat.succ (Nat.succ (2 * n)) from by omega]
+      rw [show m + Nat.succ n = Nat.succ (m + n) from by omega]
+      rw [show 2 * (m + 1) = Nat.succ (Nat.succ (2 * m)) from by omega]
+      rw [show (m + 1 + n) = Nat.succ (m + n) from by omega]
+      simp only [Nat.factorial_succ]
+      push_cast
+      have : m ! ≠ 0 := by positivity
+      have : n ! ≠ 0 := by positivity
+      have : m + 1 ≠ 0 := by positivity
+      have : m + 1 + n ≠ 0 := by positivity
+      field_simp
+      trans (↑m + ↑n + 1) * ↑(m + n)! * (↑m + 1) * (2 * ↑n + 1 + 1) * (2 * ↑n + 1) *
+      ↑m ! * ↑n ! * ↑(m + n)! * ↑m ! * ↑n ! * ↑(2 * m)! * ↑(2 * n)!; ring_nf
+      trans (↑n + 1) * (↑m + ↑n + 1) *
+        (4 * (↑m + 1) * (↑m + ↑n + 1) * ↑(m + n)! - ↑(m + n)! * (2 * ↑m + 1 + 1) * (2 * ↑m + 1)) *
+         ↑m ! * ↑n ! * ↑(m + n)! * ↑m ! * ↑n ! * ↑(2 * m)! * ↑(2 * n)!; rotate_left; ring_nf
+      congr 7
+      trans (↑m + ↑n + 1) * (↑m + 1) * (2 * ↑n + 1 + 1) * (2 * ↑n + 1) * ↑(m + n)!; ring_nf
+      trans (↑n + 1) * (↑m + ↑n + 1) * (4 * (↑m + 1) * (↑m + ↑n + 1) - (2 * ↑m + 1 + 1) * (2 * ↑m + 1)) * ↑(m + n)!; rotate_left; ring_nf
+      congr 1
+      ring
+    · apply ih
+    · positivity
+    · apply ih
+    · positivity
