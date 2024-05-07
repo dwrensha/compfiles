4d227824f3f216e082b7221ee0c02da85a621d7f
Seasawher <tellagoodstory@icloud.com>
2024-05-08 04:00:33 +0900

    edit authors

--- Compfiles/Imo2012P4.lean
+++ Compfiles/Imo2012P4.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 David Renshaw. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: David Renshaw
+Authors: spinylobster, ondanaoto, Seasawher
 -/
 
 import Mathlib.Tactic
