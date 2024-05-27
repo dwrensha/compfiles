63963b0ebccf5a57334c87b5ff681c23f977b1f9
David Renshaw <dwrenshaw@gmail.com>
2024-05-27 15:16:25 -0400

    [Usa1992P1] progress

--- Compfiles/Usa1992P1.lean
+++ Compfiles/Usa1992P1.lean
@@ -30,7 +30,9 @@ section digits
 
 open Nat
 
-theorem digits_append_zeroes_append_digits {b k m n : ℕ} (hm : 0 < m) (hb : 1 < b) :
+-- TODO add this to mathlib
+theorem digits_append_zeroes_append_digits {b k m n : ℕ} (hm : 0 < m)
+    (hb : 1 < b) :
     digits b n ++ List.replicate k 0 ++ digits b m =
     digits b (n + b ^ (k + (digits b n).length) * m) := by
   revert m
@@ -54,6 +56,7 @@ theorem digits_append_zeroes_append_digits {b k m n : ℕ} (hm : 0 < m) (hb : 1 
 
 end digits
 
+-- TODO add a generalization of this mathlib?
 lemma digits_pow (m : ℕ) : (Nat.digits 10 (10^m)).length = m + 1 := by
   induction' m with m ih
   · simp
@@ -116,6 +119,26 @@ lemma digits_sum (m x y : ℕ)
   rw [digits_sum_mul_pow]
   ring
 
+lemma Finset.range_prod_odd
+    {n : ℕ} {f : ℕ → ℕ} (hs : ∀ i ∈ Finset.range n, Odd (f i)) :
+    Odd (∏ i ∈ Finset.range n, f i) := by
+  revert hs
+  induction n
+  case zero => simp
+  case succ n ih =>
+    intro hs
+    rw [Finset.prod_range_succ, Nat.odd_mul]
+    constructor
+    · apply ih
+      intro i hi
+      have : i ∈ Finset.range (n + 1) := by
+        rw [Finset.mem_range] at *
+        exact Nat.lt_add_right 1 hi
+      exact hs i this
+    · apply hs
+      rw [Finset.mem_range]
+      exact lt_add_one n
+
 snip end
 
 determine solution : ℕ → ℕ := fun n ↦ 9 * 2 ^ n
@@ -123,91 +146,178 @@ determine solution : ℕ → ℕ := fun n ↦ 9 * 2 ^ n
 problem usa1992_p1 (n : ℕ) :
     (Nat.digits 10
      (∏ i ∈ Finset.range (n + 1), (10^(2^i) - 1))).sum = solution n := by
-    -- we follow the informal proof from
-    -- https://prase.cz/kalva/usa/usoln/usol921.html
+  -- we follow the informal proof from
+  -- https://prase.cz/kalva/usa/usoln/usol921.html
 
-    -- Induction on n.
-    induction' n with n ih
-    · simp; done
+  -- Induction on n.
+  induction' n with n ih
+  · simp; done
 
-    -- Assume it is true for n-1.
-    -- Obviously a_n < 10 to the power of 2^n
-    let a i := 10^(2^i) - 1
-    have h1 : ∀ i, a i < 10 ^ (2 ^ i) := fun i ↦ by
-      dsimp [a]
-      have h2 : 0 < 10 ^ 2 ^ i := by positivity
-      omega
+  -- Assume it is true for n-1.
+  -- Obviously a_n < 10 to the power of 2^n
+  let a i := 10^(2^i) - 1
+  have h1 : ∀ i, a i < 10 ^ (2 ^ i) := fun i ↦ by
+    dsimp [a]
+    have h2 : 0 < 10 ^ 2 ^ i := by positivity
+    omega
 
-    have ha1 : ∀ i, 1 ≤ a i := fun i ↦ by
-      dsimp [a]
-      have h3 : 1 ≤ 2 ^ i := Nat.one_le_two_pow
-      have h4 : 10 ^ 1 ≤ 10 ^ (2 ^ i) :=
-        Nat.pow_le_pow_of_le_right (by norm_num) h3
-      dsimp at h4
-      calc
-        _ < 10 - 1 := by norm_num
-        _ = 10^1 - 1 := by omega
-        _ ≤ 10^(2^i) - 1 := Nat.sub_le_sub_right h4 _
+  have ha1 : ∀ i, 1 ≤ a i := fun i ↦ by
+    dsimp [a]
+    have h3 : 1 ≤ 2 ^ i := Nat.one_le_two_pow
+    have h4 : 10 ^ 1 ≤ 10 ^ (2 ^ i) :=
+      Nat.pow_le_pow_of_le_right (by norm_num) h3
+    dsimp at h4
+    calc
+      _ < 10 - 1 := by norm_num
+      _ = 10^1 - 1 := by omega
+      _ ≤ 10^(2^i) - 1 := Nat.sub_le_sub_right h4 _
 
-    let b : ℕ → ℕ := fun m ↦ ∏ i ∈ Finset.range (m + 1), a i
-    -- So b_{n-1} < 10 to the power of (1 + 2 + 2^2 + ... + 2^{n-1}).
-    have h2 : ∀ m, b m < 10^(∑ i ∈ Finset.range (m + 1), 2^i) := fun m ↦ by
-      dsimp [b]
-      rw [←Finset.prod_pow_eq_pow_sum]
-      refine Finset.prod_lt_prod_of_nonempty ?_ ?_ Finset.nonempty_range_succ
-      · intro i hi
-        exact ha1 i
-      · intro i hi
-        exact h1 i
+  let b : ℕ → ℕ := fun m ↦ ∏ i ∈ Finset.range (m + 1), a i
+  -- So b_{n-1} < 10 to the power of (1 + 2 + 2^2 + ... + 2^{n-1}).
+  have h2 : ∀ m, b m < 10^(∑ i ∈ Finset.range (m + 1), 2^i) := fun m ↦ by
+    dsimp [b]
+    rw [←Finset.prod_pow_eq_pow_sum]
+    refine Finset.prod_lt_prod_of_nonempty ?_ ?_ Finset.nonempty_range_succ
+    · intro i hi
+      exact ha1 i
+    · intro i hi
+      exact h1 i
 
-    -- ... < 10 to the power of 2^n.
-    have h3 : ∀ m, 10^(∑ i ∈ Finset.range m, 2^i) < 10^(2^m) := fun m ↦ by
-      have h4 : ∑ i ∈ Finset.range m, 2 ^ i < 2 ^ m :=
-        Nat.geomSum_lt (le_refl _) fun _ hk ↦ Finset.mem_range.mp hk
-      exact (Nat.pow_lt_pow_iff_right (by norm_num)).mpr h4
+  -- ... < 10 to the power of 2^n.
+  have h3 : ∀ m, 10^(∑ i ∈ Finset.range m, 2^i) < 10^(2^m) := fun m ↦ by
+    have h4 : ∑ i ∈ Finset.range m, 2 ^ i < 2 ^ m :=
+      Nat.geomSum_lt (le_refl _) fun _ hk ↦ Finset.mem_range.mp hk
+    exact (Nat.pow_lt_pow_iff_right (by norm_num)).mpr h4
 
-    -- Now b_n = b_{n-1}10^N - b_{n-1}, where N = 2^n.
-    have h4 : b (n + 1) = b n * 10^(2^(n+1)) - b n := by
-      nth_rewrite 2 [←mul_one (b n)]
-      rw [←Nat.mul_sub_left_distrib]
-      dsimp [b]
+  have bn_ge_nine : 9 ≤ b n := by
+    clear ih
+    dsimp [b, a]
+    induction n
+    case zero => simp
+    case succ n ih2 =>
       rw [Finset.prod_range_succ]
+      have h10 : 1 ≤ (10 ^ 2 ^ (n + 1) - 1) := by
+        have h11 : 2 ≤ 2 ^ (n + 1) := by
+          rw [pow_add, pow_one]
+          have h12 : 1 ≤ 2 ^ n := Nat.one_le_two_pow
+          exact Nat.le_mul_of_pos_left 2 h12
+        have one_le_ten : 1 ≤ 10 := by norm_num
+        have h13 : 10 ^ 2 ≤ 10 ^ 2 ^ (n + 1) :=
+          Nat.pow_le_pow_of_le_right one_le_ten h11
+        omega
+      exact le_mul_of_le_of_one_le ih2 (ha1 (n + 1))
 
-    -- But b_{n-1} < 10^N,
-    have h5 : b n < 10 ^ 2 ^ (n + 1) := by
-      calc _ < 10 ^ ∑ i ∈ Finset.range (n + 1), 2 ^ i := h2 _
-           _ < 10 ^ 2 ^ (n + 1) := by
+  -- Now b_n = b_{n-1}10^N - b_{n-1}, where N = 2^n.
+  have h4 : b (n + 1) = b n * 10^(2^(n+1)) - b n := by
+    nth_rewrite 2 [←mul_one (b n)]
+    rw [←Nat.mul_sub_left_distrib]
+    dsimp [b]
+    rw [Finset.prod_range_succ]
+
+  -- But b_{n-1} < 10^N,
+  have h5 : b n < 10 ^ 2 ^ (n + 1) := by
+    calc _ < 10 ^ ∑ i ∈ Finset.range (n + 1), 2 ^ i := h2 _
+         _ < 10 ^ 2 ^ (n + 1) := by
              refine (Nat.pow_lt_pow_iff_right (by norm_num)).mpr ?_
              exact Nat.geomSum_lt (le_refl _) fun _ hk ↦ Finset.mem_range.mp hk
 
-    have h7 : 1 ≤ b n := by
-      dsimp [b]
-      exact Finset.one_le_prod' fun i a ↦ ha1 i
+  have h7 : 1 ≤ b n := by
+    dsimp [b]
+    exact Finset.one_le_prod' fun i a ↦ ha1 i
 
-    -- so b_n = (b_{n-1} - 1)10^N + (10^N - b_{n-1})
-    have h6 : b (n + 1) = (b n - 1) * 10 ^(2^(n+1)) + (10 ^(2^(n+1)) - b n) := by
-      rw [h4]
-      -- TODO: simpler version via zify
-      have h5' := h5.le
-      have h8 : 10 ^ 2 ^ (n + 1) ≤ b n * 10 ^ 2 ^ (n + 1) :=
-        Nat.le_mul_of_pos_left (10 ^ 2 ^ (n + 1)) h7
-      rw [←Nat.add_sub_assoc h5']
-      nth_rewrite 2 [add_comm]
-      rw [Nat.mul_sub_right_distrib, one_mul, Nat.add_sub_of_le h8]
+  -- so b_n = (b_{n-1} - 1)10^N + (10^N - b_{n-1})
+  have h6 : b (n + 1) = (b n - 1) * 10 ^(2^(n+1)) + (10 ^(2^(n+1)) - b n) := by
+    rw [h4]
+    -- TODO: simpler version via zify
+    have h5' := h5.le
+    have h8 : 10 ^ 2 ^ (n + 1) ≤ b n * 10 ^ 2 ^ (n + 1) :=
+      Nat.le_mul_of_pos_left (10 ^ 2 ^ (n + 1)) h7
+    rw [←Nat.add_sub_assoc h5']
+    nth_rewrite 2 [add_comm]
+    rw [Nat.mul_sub_right_distrib, one_mul, Nat.add_sub_of_le h8]
 
-    -- and the digit sum of b_n is just
-    -- the digit sum of (b_{n-1} - 1)10^N plus the digit sum of (10^N - b_{n-1}).
-    have h8 : (Nat.digits 10 (b (n + 1))).sum =
-              (Nat.digits 10 ((b n - 1) * 10 ^ 2 ^ (n+1))).sum +
-              (Nat.digits 10 (10^2^(n+1) - b n)).sum := by
-     have h9 : 0 < b n := h7
-     have h10 := digits_sum (2^(n+1)) (b n - 1) (10^2^(n+1) - b n)
-               (Nat.sub_lt_self h9 h5.le) -- ..
-     rw [←h10, h4]
-     congr 2
-     omega
+  -- and the digit sum of b_n is just
+  -- the digit sum of (b_{n-1} - 1)10^N plus the digit sum of (10^N - b_{n-1}).
+  have h8 : (Nat.digits 10 (b (n + 1))).sum =
+            (Nat.digits 10 ((b n - 1) * 10 ^ 2 ^ (n+1))).sum +
+            (Nat.digits 10 (10^2^(n+1) - b n)).sum := by
+   have h9 : 0 < b n := h7
+   have h10 := digits_sum (2^(n+1)) (b n - 1) (10^2^(n+1) - b n)
+             (Nat.sub_lt_self h9 h5.le) -- ..
+   rw [←h10, h4]
+   congr 2
+   omega
+
+  -- Now b_{n-1} is odd
+  have h9 : Odd (b n) := by
+    dsimp [b]
+    suffices H : ∀ i ∈ Finset.range (n + 1), Odd (a i) from
+      Finset.range_prod_odd H
+    intro i hi
+    dsimp [a]
+    rw [Nat.odd_iff]
+    zify
+    have h10 : (((10 ^ 2 ^ i - 1):ℕ):ℤ) = ((10^2^i) : ℤ) - (1:ℤ) := by
+      rw[Int.ofNat_sub (Nat.one_le_pow' (2 ^ i) 9)]
+      norm_cast
+    rw [h10]
+    rw [Int.sub_emod, Int.one_emod_two]
+    have h11 : (10:ℤ) ^ 2 ^ i % 2 = 0 := by
+      have h12 : (10:ℤ) = 2 * 5 := by norm_num
+      rw [h12, mul_pow]
+      have h13 : 0 < 2^i := by positivity
+      obtain ⟨k, hk⟩ := Nat.exists_eq_add_of_le h13
+      rw [hk, pow_add]
+      simp only [Nat.succ_eq_add_one, zero_add, pow_one]
+      rw [mul_assoc]
+      exact Int.mul_emod_right _ _
+    rw [h11]
+    norm_num
+
+  -- and so its last digit is non-zero
+
+  have ten_ne_one : 10 ≠ 1 := by norm_num
+  have h10 : (Nat.digits 10 (b n)).head! ≠ 0 := by
+    rw [Nat.head!_digits ten_ne_one]
+    intro h11
+    have h12 : 10 ∣ b n := Nat.dvd_of_mod_eq_zero h11
+    rw [show 10 = 2 * 5 by norm_num] at h12
+    have h13 : 2 ∣ b n := by omega
+    have h14 : ¬ 2 ∣ b n := Odd.not_two_dvd_nat h9
+    contradiction
+
+  -- consider Nat.digits 10 (b n)
+  -- it's `d :: ds`, where d ≠ 0.
+  -- the sum is d + ds.sum
+  -- on the other hand, Nat.digits 10 (b n - 1) is (d - 1) :: ds.
+  -- so its sum is (d - 1) + ds.sum
+
+  -- Nat.digits 10 (b n) =
+  -- List.head! (Nat.digits 10 (b n)) ::
+  have one_lt_ten : 1 < 10 := by norm_num
+
+  -- so the digit sum of b_{n-1} - 1 is one less than the digit sum of b_{n-1},
+  have h11 : (Nat.digits 10 (b n - 1)).sum + 1 = (Nat.digits 10 (b n)).sum := by
+    rw [Nat.digits_def' one_lt_ten (by omega)]
+    nth_rewrite 2 [Nat.digits_def' one_lt_ten (by omega)]
+    rw [List.sum_cons, List.sum_cons]
+    have h13 : b n % 10 ≠ 0 := by
+      intro h11
+      have h12 : 10 ∣ b n := Nat.dvd_of_mod_eq_zero h11
+      rw [show 10 = 2 * 5 by norm_num] at h12
+      have h13 : 2 ∣ b n := by omega
+      have h14 : ¬ 2 ∣ b n := Odd.not_two_dvd_nat h9
+      contradiction
+
+    have h12 : (b n - 1) / 10 = b n / 10 := by omega
+    rw [h12]
+    suffices H : (b n - 1) % 10 + 1 = b n % 10 by omega
+    omega
+
+  -- and hence is 9·2_{n-1} - 1
+  rw [ih, solution] at h11
 
 /-
-    Now bn-1 is odd and so its last digit is non-zero, so the digit sum of bn-1 - 1 is one less than the digit sum of bn-1, and hence is 9·2n-1 - 1. Multiplying by 10N does not change the digit sum. (10N - 1) - bn-1 has 2n digits, each 9 minus the corresponding digit of bn-1, so its digit sum is 9·2n - 9·2n-1. bn-1 is odd, so its last digit is not 0 and hence the last digit of (10N - 1) - bn-1 is not 9. So the digit sum of 10N - bn-1 is 9·2n - 9·2n-1 + 1. Hence bn has digit sum (9·2n-1 - 1) + (9·2n - 9·2n-1 + 1) = 9·2n.
+    Multiplying by 10N does not change the digit sum. (10N - 1) - bn-1 has 2n digits, each 9 minus the corresponding digit of bn-1, so its digit sum is 9·2n - 9·2n-1. bn-1 is odd, so its last digit is not 0 and hence the last digit of (10N - 1) - bn-1 is not 9. So the digit sum of 10N - bn-1 is 9·2n - 9·2n-1 + 1. Hence bn has digit sum (9·2n-1 - 1) + (9·2n - 9·2n-1 + 1) = 9·2n.
     -/
-    sorry
+  sorry
