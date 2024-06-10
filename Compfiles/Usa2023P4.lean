7118eabf68cf2c2ee409165b7ec4e11a1621bfcb
David Renshaw <dwrenshaw@gmail.com>
2024-06-09 21:09:11 -0400

    [Usa2023P4] remove unnecessary imports

--- Compfiles/Usa2023P4.lean
+++ Compfiles/Usa2023P4.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Renshaw
 -/
 
-import Mathlib.Data.Real.Basic
-import Mathlib.Algebra.Order.Positive.Field
 import Mathlib.Tactic
 
 import ProblemExtraction
