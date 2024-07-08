a4ebb5a0e4ecea6b01c4de1d22eb4060ba032b67
WLZPro <wowo.luo.zhiyi@gmail.com>
2024-07-08 11:41:18 +0800

    [Usa2022P3] Add proof

--- Compfiles.lean
+++ Compfiles.lean
@@ -185,6 +185,7 @@ import Compfiles.Usa2017P5
 import Compfiles.Usa2018P1
 import Compfiles.Usa2019P1
 import Compfiles.Usa2022P1
+import Compfiles.Usa2022P3
 import Compfiles.Usa2022P4
 import Compfiles.Usa2023P2
 import Compfiles.Usa2023P4
