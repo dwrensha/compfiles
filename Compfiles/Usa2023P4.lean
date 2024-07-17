83b68025666c417630e14eb2d3fa2c0db4eb502c
David Renshaw <dwrenshaw@gmail.com>
2024-07-17 08:59:40 -0400

    [Usa2023P4] progress

--- Compfiles/Usa2023P4.lean
+++ Compfiles/Usa2023P4.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Renshaw
 -/
 
+
 import Mathlib.Tactic
 
 import ProblemExtraction
@@ -132,6 +133,72 @@ lemma lemma5 {a x : ℕ} (hx : 0 < x)
   have h6 := Nat.maxPowDiv.le_of_dvd one_lt_two hx h5
   omega
 
+lemma lemma5' {a x : ℕ} (hx : 0 < x)
+    (hax : Nat.maxPowDiv 2 x < Nat.maxPowDiv 2 a) :
+    Nat.maxPowDiv 2 (x + a) = Nat.maxPowDiv 2 x := by
+  have h1 : 2 ^ (Nat.maxPowDiv 2 (x + a)) ∣ x + a := Nat.maxPowDiv.pow_dvd 2 (x + a)
+  have h2 : 2 ^ (Nat.maxPowDiv 2 a) ∣ a := Nat.maxPowDiv.pow_dvd 2 a
+  have h4 : 2 ^ (Nat.maxPowDiv 2 x) ∣ x := Nat.maxPowDiv.pow_dvd 2 x
+
+  obtain ⟨c, hc⟩ : ∃ c, (Nat.maxPowDiv 2 a) = (Nat.maxPowDiv 2 x) + 1 + c :=
+    Nat.exists_eq_add_of_le hax
+
+  obtain ⟨aa, haa⟩ := h2
+  obtain ⟨xx, hxx⟩ := h4
+  obtain ⟨xa, hxa⟩ := h1
+  rw [hc, add_assoc, pow_add] at haa
+
+  have h10 : x + a = 2 ^ Nat.maxPowDiv 2 x * (xx + 2 ^ (1 + c) * aa) := by
+    nth_rw 1 [hxx]
+    nth_rw 1 [haa]
+    rw [mul_assoc, ←mul_add]
+
+  have h12 : 2 ^ Nat.maxPowDiv 2 x ∣ x + a := Dvd.intro _ h10.symm
+  replace h12 := Nat.maxPowDiv.le_of_dvd one_lt_two (Nat.add_pos_left hx a) h12
+  suffices H : Nat.maxPowDiv 2 (x + a) ≤ Nat.maxPowDiv 2 x from Nat.le_antisymm H h12
+  by_contra! H
+
+  rw [h10] at H
+
+  have h16 : ¬ 2 ∣ xx := by
+    rintro ⟨z, rfl⟩
+    rw [←mul_assoc, ←pow_succ] at hxx
+    have h17 : 2 ^ (Nat.maxPowDiv 2 x + 1) ∣ x := Dvd.intro z hxx.symm
+    replace h17 := Nat.maxPowDiv.le_of_dvd one_lt_two hx h17
+    omega
+
+  have h15' : ¬ 2 ∣ xx + 2 ^ (1 + c) * aa := by
+    intro HH
+    have h19 : 2 ∣ 2 ^ (1 + c) * aa := by
+      rw [pow_add, pow_one, mul_assoc]
+      exact Nat.dvd_mul_right 2 _
+    have : 2 ∣ xx := (Nat.dvd_add_iff_left h19).mpr HH
+    contradiction
+
+  have h15 : Nat.maxPowDiv 2 (xx + 2 ^ (1 + c) * aa) = 0 := by
+    by_contra! HH
+    replace HH : 0 < Nat.maxPowDiv 2 (xx + 2 ^ (1 + c) * aa) := Nat.zero_lt_of_ne_zero HH
+    generalize hee : xx + 2 ^ (1 + c) * aa = ee
+    rw [hee] at HH h15'
+    have h18 := Nat.maxPowDiv.pow_dvd 2 ee
+    generalize hee' : Nat.maxPowDiv 2 ee = ee'
+    rw [hee'] at h18 HH
+    cases' ee' with ee'
+    · omega
+    rw [pow_succ] at h18
+    have : 2 ∣ ee := Nat.dvd_of_pow_dvd HH h18
+    contradiction
+
+  have h13 : 0 < xx + 2 ^ (1 + c) * aa := by
+    have h21 : (xx + 2 ^ (1 + c) * aa) % 2 = 1 := Nat.two_dvd_ne_zero.mp h15'
+    obtain ⟨k, hk⟩ : Odd (xx + 2 ^ (1 + c) * aa) := Nat.odd_iff.mpr h21
+    rw [hk]
+    exact Nat.zero_lt_succ (2 * k)
+
+  rw [Nat.maxPowDiv.base_pow_mul one_lt_two h13] at H
+  rw [h15, zero_add] at H
+  omega
+
 lemma alice_move_preserves_nu
      (a : ℕ+) (N : ℕ) (b0 : Blackboard N)
      (hd : ∀ i : Fin N, Nat.maxPowDiv 2 (b0 i) < Nat.maxPowDiv 2 a)
@@ -156,6 +223,29 @@ lemma alice_move_preserves_nu
     rw [h2]
     exact hd i
 
+lemma alice_move_preserves_nu'
+     (a : ℕ+) (N : ℕ) (b0 : Blackboard N)
+     (hd : ∀ i : Fin N, Nat.maxPowDiv 2 (b0 i) < Nat.maxPowDiv 2 a)
+     (s1 : State N) (hs1 : s1 ∈ valid_moves a N ⟨b0, .Alice⟩) :
+     ∀ i, Nat.maxPowDiv 2 (s1.board i) =  Nat.maxPowDiv 2 (b0 i) := by
+  intro i
+  dsimp [valid_moves] at hs1
+  obtain ⟨j, rfl⟩ := hs1
+  dsimp
+  by_cases hji : j = i
+  · rw [hji]
+    have h2 : Function.update b0 i (b0 i + a) i = b0 i + a :=
+      Function.update_same i (b0 i + a) b0
+    rw [h2]
+    have h3 := hd i
+    push_cast
+    exact lemma5' (b0 i).pos h3
+  · have h2 : Function.update b0 j (b0 j + a) i = b0 i := by
+      apply Function.update_noteq
+      symm
+      exact hji
+    rw [h2]
+
 lemma bob_moves_after_alice {a : ℕ+} {N : Nat} (b0 : Blackboard N)
     (s1 : State N)
     (hs : s1 ∈ valid_moves a N ⟨b0, .Alice⟩) :
@@ -164,14 +254,20 @@ lemma bob_moves_after_alice {a : ℕ+} {N : Nat} (b0 : Blackboard N)
   obtain ⟨i, rfl⟩ := hs
   simp
 
+lemma lemma6 {x : ℕ} (hp : 0 < x) (hx : Even x)
+    : Nat.maxPowDiv x / 2 + 1 = Nat.maxPowDiv x := by
+  sorry
+
 -- When N ≥ 2, if ν2(x) < ν2(a) for all x ∈ S, the game must terminate
 -- in ∑_{x∈S} ν2(x) moves, no matter what either player does.
 lemma lemma2' (a : ℕ+) (N : ℕ) (hN : 1 < N) (s0 : State N)
     (hd : ∀ i : Fin N, Nat.maxPowDiv 2 (s0.board i) < Nat.maxPowDiv 2 a) :
     EndInevitableIn a N (∑ i : Fin N, Nat.maxPowDiv 2 (s0.board i)) s0 := by
   generalize hms : ∑ i : Fin N, Nat.maxPowDiv 2 (s0.board i) = ms
-  induction' ms
-  · have h1 := lemma3 N s0.board hms
+  revert s0
+  induction' ms with ms' ih
+  · intro s0 hd hms
+    have h1 := lemma3 N s0.board hms
     obtain ⟨b0, t0⟩ := s0
     match t0 with
     | .Bob =>
@@ -216,7 +312,37 @@ lemma lemma2' (a : ℕ+) (N : ℕ) (hN : 1 < N) (s0 : State N)
         have h3 := h1 i
         exact h3 hie
 
-  sorry
+  intro s0 hd hms
+  obtain ⟨b0, t0⟩ := s0
+  match t0 with
+  | .Bob =>
+      apply EndInevitableIn.BobTurn _ ⟨b0, .Bob⟩
+      intro s hs
+      dsimp [valid_moves] at hs
+      obtain ⟨i, hie, his⟩ := hs
+      sorry
+  | .Alice =>
+    apply EndInevitableIn.AliceTurn _ ⟨b0, .Alice⟩
+    intro s hs
+    have h1 := alice_move_preserves_nu' a N b0 hd s hs
+    dsimp at hms
+    have h2 : ∑ i : Fin N, Nat.maxPowDiv 2 ↑(b0 i) = ∑ i : Fin N, Nat.maxPowDiv 2 ↑(s.board i) := by
+      congr
+      ext i
+      exact (h1 i).symm
+    rw [h2] at hms; clear h1 h2
+    obtain ⟨b1, rfl⟩ := bob_moves_after_alice b0 s hs
+    dsimp at hms
+    dsimp [valid_moves] at hs
+    obtain ⟨i, hi⟩ := hs
+    simp only [State.mk.injEq, and_true] at hi
+    apply EndInevitableIn.BobTurn _ ⟨b1, .Bob⟩
+    intro s2 hs2
+    dsimp [valid_moves] at hs2
+    obtain ⟨j, hje, hj⟩ := hs2
+    specialize ih s2
+    sorry
+
 
 -- When N ≥ 2, if ν2(x) < ν2(a) for all x ∈ S, the game must terminate no
 -- matter what either player does.
