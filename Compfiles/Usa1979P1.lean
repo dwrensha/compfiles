cfa3bb5a56fe0808c136a4594c6583e6dd40ae9d
David Renshaw <dwrenshaw@gmail.com>
2024-07-29 09:38:22 -0400

    [Usa1979P1] a comment explaining MultisetNatOfLen14

--- Compfiles/Usa1979P1.lean
+++ Compfiles/Usa1979P1.lean
@@ -19,6 +19,10 @@ apart from permutations, of the Diophantine Equation $n_1^4+n_2^4+\cdots +n_{14}
 
 namespace Usa1979P1
 
+/--
+A type representing assignments to the variables $n_1$, $n_2$, ..., $n_{14}$,
+quotiented by permutations of indices.
+-/
 structure MultisetNatOfLen14 where
   s : Multiset ℕ
   p : Multiset.card s = 14
