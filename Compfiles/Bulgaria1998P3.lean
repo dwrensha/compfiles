c6e6e1924cb6ea78ad84fa1460839cf4735a63c3
David Renshaw <dwrenshaw@gmail.com>
2024-08-08 07:14:33 -0400

    update mathlib

--- Compfiles/Bulgaria1998P3.lean
+++ Compfiles/Bulgaria1998P3.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Renshaw
 -/
 
-import Mathlib.Algebra.Order.Archimedean
 import Mathlib.Data.Real.Basic
 import Mathlib.Algebra.BigOperators.Group.Finset
 import Mathlib.Analysis.SpecificLimits.Basic
