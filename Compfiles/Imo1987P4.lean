6eca4657a081ad088d765fbec0fcff6bad22d309
David Renshaw <dwrenshaw@gmail.com>
2024-11-17 17:49:41 -0500

    [Imo1987P4] remove unneeded import

--- Compfiles/Imo1987P4.lean
+++ Compfiles/Imo1987P4.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Renshaw
 -/
 
-import Aesop
 import Mathlib.Data.Set.Basic
 import Mathlib.Data.Set.Card
 import Mathlib.Order.Interval.Finset.Nat
