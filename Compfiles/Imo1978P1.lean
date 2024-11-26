c6df2675270f3d477ea1792d8308f934470bc57d
David Renshaw <dwrenshaw@gmail.com>
2024-11-25 21:25:41 -0500

    [Imo1978P1] a bit simpler

--- Compfiles/Imo1978P1.lean
+++ Compfiles/Imo1978P1.lean
@@ -40,32 +40,25 @@ problem imo1978_p1 (m n : ℕ)
   dsimp only [ValidPair] at hmn'
   obtain ⟨h1, h2, h3⟩ := hmn'
   change _ ≡ _ [MOD 1000] at h3
-
-  rw [←ZMod.natCast_eq_natCast_iff] at h3 -- cast into `ZMod 1000`
+  rw [Nat.modEq_iff_dvd] at h3
   push_cast at h3
-  replace h3 : (1978 : ZMod 1000)^m' * (1978^(n' - m') - 1) = 0 := by
-    rw [mul_sub, ←pow_add, add_comm]
-    rw [Nat.sub_add_cancel (Nat.le_of_succ_le h2), h3]
-    ring
-  norm_cast at h3
-  rw [ZMod.intCast_zmod_eq_zero_iff_dvd] at h3
-  push_cast at h3
+  replace h3 : (1000:ℤ) ∣ 1978 ^ m' * (1978 ^ (n' - m') - 1) := by
+    rw [mul_sub, mul_one]
+    rwa [pow_mul_pow_sub 1978 (Nat.le_of_succ_le h2)]
   rw [show (1000 : ℤ) = 8 * 125 by norm_num] at h3
 
   -- So we must have 8 divides 1978^m',
   have h4 : (8 : ℤ) ∣ 1978 ^ m' := by
-    replace h3 : (8:ℤ) ∣ 1978 ^ m' * Int.subNatNat (1978 ^ (n' - m')) 1 :=
+    replace h3 : (8:ℤ) ∣ 1978 ^ m' * (1978 ^ (n' - m') - 1) :=
       dvd_of_mul_right_dvd h3
-    have h5 : IsCoprime (8 : ℤ) (Int.subNatNat (1978 ^ (n' - m')) 1) := by
+    have h5 : IsCoprime (8 : ℤ) (1978 ^ (n' - m') - 1) := by
       rw [show (8 : ℤ) = 2 ^ 3 by norm_num]
-      suffices H : IsCoprime (2 : ℤ) (Int.subNatNat (1978 ^ (n' - m')) 1) from
+      suffices H : IsCoprime (2 : ℤ) (1978 ^ (n' - m')- 1) from
         IsCoprime.pow_left H
-      suffices H : ¬ (2:ℤ) ∣ (Int.subNatNat (1978 ^ (n' - m')) 1) from
+      suffices H : ¬ (2:ℤ) ∣ (1978 ^ (n' - m') - 1) from
         (Prime.coprime_iff_not_dvd Int.prime_two).mpr H
       rw [Int.two_dvd_ne_zero]
       have h6 : 1 ≤ (1978 ^ (n' - m')) := Nat.one_le_pow' (n' - m') 1977
-      rw [Int.subNatNat_eq_coe]
-      simp only [Nat.cast_pow, Nat.cast_ofNat, Nat.cast_one]
       rw [show (1978 : ℤ) = 2 * 989 by norm_num]
       have h7 : (((2:ℤ) * 989) ^ (n' - m')) % 2 = 0 := by
         rw [mul_pow]
@@ -103,6 +96,8 @@ problem imo1978_p1 (m n : ℕ)
     omega
 
   -- and 125 divides 1978^(n'-m') - 1.
+  have h6 : 125 ∣ 1978^(n'-m') - 1 := by
+    sorry
   sorry
 
 end Imo1978P1
