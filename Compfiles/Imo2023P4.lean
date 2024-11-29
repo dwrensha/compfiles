0a54323b746ef9c98645ab162087e433868a378f
David Renshaw <dwrenshaw@gmail.com>
2024-11-29 14:25:16 -0500

    [Imo2023P4] handle base case of induction

--- Compfiles/Imo2023P4.lean
+++ Compfiles/Imo2023P4.lean
@@ -42,13 +42,31 @@ problem imo2023_p4_generalized
     (hxi : x.Injective)
     (hxa : ∀ i : Finset.Icc 1 (2 * m + 1), ∃ k : ℤ, aa m x i = k)
     : 3 * m + 1 ≤ aa m x ⟨2 * m + 1, by simp⟩ := by
-  induction m using Nat.strong_induction_on with | h m ih =>
-  cases m with
+  -- We follow the "induct-by-two" solution from
+  -- https://web.evanchen.cc/exams/IMO-2023-notes.pdf
+  induction m with
   | zero =>
     simp only [CharP.cast_eq_zero, mul_zero, zero_add, aa, Nat.mul_zero, Nat.reduceAdd,
                one_div, Real.one_le_sqrt]
+    have h1 : Finset.filter (fun x ↦ x ≤ ⟨1, by simp⟩)
+                            (Finset.univ (α := {x //x ∈ Finset.Icc 1 1}))
+           = Finset.univ (α := {x // x ∈ Finset.Icc 1 1}) := by
+      refine Finset.filter_true_of_mem ?h
+      intro x hx
+      obtain ⟨x, hx1⟩ := x
+      rw [Finset.mem_Icc] at hx1
+      obtain ⟨hx2, hx3⟩ := hx1
+      simp only [Nat.mul_zero, Nat.reduceAdd, Subtype.mk_le_mk]
+      exact hx3
+    rw [h1]
+    have h2 : Finset.univ (α := { x // x ∈ Finset.Icc 1 1 }) = { ⟨1, by simp⟩ } := by decide
+    simp only [h2, Finset.sum_singleton, ge_iff_le]
+    simp only [Nat.mul_zero, Nat.reduceAdd, Subtype.forall, Finset.Icc_self,
+               Finset.mem_singleton] at hxp
+    specialize hxp 1 (by simp)
+    field_simp
+  | succ m ih =>
     sorry
-  | succ m => sorry
 
 snip end
 
