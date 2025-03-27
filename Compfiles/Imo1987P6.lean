df5cb5a5002a0bce7e34b85bc2489c3956d23e8b
David Renshaw <dwrenshaw@gmail.com>
2025-03-27 13:28:19 -0400

    [Imo1987P6] improvements

--- Compfiles/Imo1987P6.lean
+++ Compfiles/Imo1987P6.lean
@@ -15,7 +15,11 @@ problem_file { tags := [.NumberTheory] }
 
 /-!
 # International Mathematical Olympiad 1987, Problem 6
-Let $n$ be an integer greater than or equal to 2. Prove that if $k^2 + k + n$ is prime for all integers $k$ such that $0 <= k <= \sqrt{n/3}$, then $k^2 + k + n$ is prime for all integers $k$ such that $0 <= k <= n - 2$.
+
+Let $n$ be an integer greater than or equal to 2. Prove that
+if $k^2 + k + n$ is prime for all integers $k$ such that
+$0 <= k <= \sqrt{n/3}$, then $k^2 + k + n$ is prime for all
+integers $k$ such that $0 <= k <= n - 2$.
 -/
 
 namespace Imo1987P6
@@ -65,7 +69,7 @@ lemma dyadic {k b : ℕ} (h1 : 1 ≤ k) (h2 : k ≤ b) : ∃ i, b < 2^i * k ∧ 
     replace h2bk := (Nat.log2_lt hbk).1 h2bk
     replace h2bk := succ_le_of_lt h2bk
     calc
-    _ < b/k * k + k := lt_div_mul_add (by linarith)
+    _ < b/k * k + k := lt_div_mul_add (by omega)
     _ = (b/k+1) *k := by ring
     _ ≤  2 ^((b/k).log2 +1) * k := Nat.mul_le_mul_right k h2bk
   · have h2 : 2 ^((b/k).log2 +1)  = 2 * 2^( (b/k).log2 ):=
@@ -156,11 +160,10 @@ lemma pos_sub_of_lt {a b : ℕ} : a < b → 0 < b-a := by
 snip end
 
 problem imo1987_p6
-  (p : ℕ)
-  (f : ℕ → ℕ)
-  (h₀ : ∀ x, f x = x^2 + x + p)
-  (h₁ : ∀ k : ℕ, k ≤ Nat.floor (Real.sqrt (p / 3)) → Nat.Prime (f k)) :
-  ∀ i ≤ p - 2, Nat.Prime (f i) := by
+    (p : ℕ)
+    (h₁ : ∀ k : ℕ, k ≤ Nat.floor (Real.sqrt ((p:ℝ) / 3)) → Nat.Prime (k^2 + k + p)) :
+    ∀ i ≤ p - 2, Nat.Prime (i^2 + i + p) := by
+  let f x := x^2 + x + p
   let r := Nat.floor (Real.sqrt (p/3))
   intro k
   apply Nat.case_strong_induction_on k
@@ -178,30 +181,20 @@ problem imo1987_p6
     have hs : 1 ≤ s := by
       simp_rw [s,kk]
       exact pos_sub_of_lt h
-    have ieq1 : s+2 < 4*s+1 := by linarith
-    have ieq2 : s^2 ≤ s^2 *4 := by nlinarith
-    have ieq3 : 3*r ≤ 6*r*s  := by nlinarith
+    have ieq3 : 3*r ≤ 6*r*s  := by nlinarith only [hs]
     have ieq4 : p ≤ 3*r^2 + 6*r + 2 := by
       have ieq5: √ (p/3) < r+1 := Nat.lt_floor_add_one _
-      replace ieq5 := Real.lt_sq_of_sqrt_lt ieq5 |> (div_lt_iff₀ (by linarith)).1
+      replace ieq5 := Real.lt_sq_of_sqrt_lt ieq5 |> (div_lt_iff₀ (by norm_num)).1
       replace ieq5: (p) < (3*r^2 + 6*r +3) := Real.coe_lt_coe.1 <| by
         have casteq: ((r:ℝ)+1)^2 * 3 = ((3*r^2+6*r+3:ℕ):ℝ) := by simp;ring_nf
-        rw [<-casteq]
+        rw [←casteq]
         exact ieq5
-      linarith
-    /-
-    have hieqs1 : s ≤ p-r-2 := by
-      calc
-      _ = kk - r := rfl
-      _ ≤ p - 2 - r := Nat.sub_le_sub_right () _
-      _ = _ := Nat.sub_right_comm _ _ _
-    -/
-    have hN0 : N =  kk^2+kk+p := h₀ kk
+      omega
+    have hN0 : N = kk^2+kk+p := rfl
     have hN1 : N < (2 * (s + r) +1)^2  := by
       calc
       _ = _:= hN0
-      _ ≤  3*r^2 + 6*r + 2 + (r+s)*(r+s+1) := by
-        nlinarith
+      _ ≤  3*r^2 + 6*r + 2 + (r+s)*(r+s+1) := by nlinarith only [hN0, ieq4, hs, hksr]
       _ = 4*r^2 + 2* r*s + s^2 + 7*r+s+2 := by ring_nf
       _ < 4*r^2 +4*s^2 +8*r*s+4*r+4*s+1 := by nlinarith
       _ = _ := by ring
@@ -216,7 +209,7 @@ problem imo1987_p6
       have hss0 : j =  ss + (s+r+1) := Nat.eq_add_of_sub_eq hj1' (by rfl)
 
 
-      have hp: 2 ≤ p :=  Nat.lt_of_sub_ne_zero (by linarith: p-2 ≠ 0) |> le_of_lt
+      have hp: 2 ≤ p :=  Nat.lt_of_sub_ne_zero (by omega: p-2 ≠ 0) |> le_of_lt
 
       have hss1 : ss ≤ k := by
         apply Nat.le_of_add_le_add_right (b :=s+r+1)
@@ -224,26 +217,24 @@ problem imo1987_p6
         calc
         _ ≤ _ := hj2
         _ = _ := by omega
-      replace hss1 : Nat.Prime (f ss) := IH ss hss1 (by linarith)
+      replace hss1 : Nat.Prime (f ss) := IH ss hss1 (by omega)
       have hfss: N = f ss + (2*kk - j+1) *j := by
-        rw [hN0,h₀ ss]
+        unfold f
+        rw [hN0]
         zify
         rw [Int.natCast_sub hj2,hss0,<-hksr]
         push_cast
         ring_nf
       rw [hfss,Nat.coprime_add_mul_right_left] at hj3
       have hss2 : f ss ∣  j:= Nat.Prime.dvd_iff_not_coprime hss1 |>.2 hj3
-      have hfss1: p ≤ f ss := by rw [h₀ ss];linarith [sq_nonneg ss]
-      have hp1 : p-2 < p := sub_le_lemma hp (by linarith)
-      have hfss2: j < 2*p := by
-        calc
-        _ ≤ 2*kk := hj2
-        _ ≤ 2*(p-2):= by omega
-        _ < 2*p := by linarith [hp1]
-      have hj : j≠ 0 := by linarith
+      have hfss1: p ≤ f ss := by unfold f; linarith [sq_nonneg ss]
+      have hp1 : p-2 < p := sub_le_lemma hp (by omega)
+      have hfss2: j < 2*p := by omega
+      have hj : j≠ 0 := by omega
       have hfss3:  f ss = j := by
         refine dvd_lemma _ _ _ hj hfss1 hss2  hfss2
-      rw [h₀ ss,hss0,add_comm _ ss,add_assoc] at hfss3
+      unfold f at hfss3
+      rw [hss0,add_comm _ ss,add_assoc] at hfss3
       replace hfss3 := add_left_cancel hfss3
       have hc1 : p ≤ k + 2 := by
         calc
@@ -254,18 +245,19 @@ problem imo1987_p6
         calc
         _ ≤ _ := hc1
         _ = (k+1) + 1 := by ring_nf
-        _ ≤ (p-2)+1 := by linarith
+        _ ≤ (p-2)+1 := by omega
         _ = p-1 := by
           obtain ⟨cc,hcc⟩ := le_iff_exists_add.1 hp
           rw [hcc,add_comm 2 cc];norm_num
       have : p = 0 := zero_of_le_sub_pos (by simp) hc2
-      linarith
+      omega
     have hfk : 1 < f kk := by
-      rw [h₀,hksr]
+      unfold f
+      rw [hksr]
       calc
       _ < 1 + 1 := by decide
-      _ ≤ s^2 + s  := by nlinarith [hs]
-      _ ≤ s^2 + 2 * r *s + r^2 + s + r + p := by nlinarith
+      _ ≤ s^2 + s  := by nlinarith only [hs]
+      _ ≤ s^2 + 2 * r *s + r^2 + s + r + p := by omega
       _ = _ := by ring
     exact key_lemma' (hfk) hP hN1
 
