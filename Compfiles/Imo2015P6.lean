d7915e51e5f6c7e0d276f031fb95092ad278ef9e
David Renshaw <dwrenshaw@gmail.com>
2025-04-09 07:53:56 -0400

    update mathlib

--- Compfiles/Imo2015P6.lean
+++ Compfiles/Imo2015P6.lean
@@ -4,13 +4,14 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Tan
 -/
 import Mathlib.Tactic
+import Mathlib.Algebra.Order.Group.Int.Sum
 
 import ProblemExtraction
 
 problem_file {
   tags := [.Algebra]
   importedFrom :=
-    "https://github.com/leanprover-community/mathlib4/pull/23038",
+    "https://github.com/leanprover-community/mathlib4/blob/master/Archive/Imo/Imo2015Q6.lean",
 }
 
 /-!
@@ -53,85 +54,10 @@ so the absolute difference is upper-bounded by
 $$\sum_{i=0}^{b-2} (2014-i) - (b-1) - \sum_{i=0}^{b-2} (b-2-i) = (b-1)(2015-b) ≤ 1007^2.$$
 -/
 
-namespace Finset
-
-/-- Sharp upper bound for the sum of a finset of integers that is bounded above, `Ioc` version. -/
-lemma sum_le_sum_Ioc {s : Finset ℤ} {c : ℤ} (hs : ∀ x ∈ s, x ≤ c) :
-    ∑ x ∈ s, x ≤ ∑ x ∈ Ioc (c - #s) c, x := by
-  set r := Ioc (c - #s) c
-  calc
-    _ ≤ ∑ x ∈ s ∩ r, x + #(s \ r) • (c - #s) := by
-      rw [← sum_inter_add_sum_diff s r _]
-      refine add_le_add_left (sum_le_card_nsmul _ _ _ fun x mx ↦ ?_) _
-      rw [mem_sdiff, mem_Ioc, not_and'] at mx
-      have := mx.2 (hs _ mx.1); omega
-    _ = ∑ x ∈ r ∩ s, x + #(r \ s) • (c - #s) := by
-      rw [inter_comm, card_sdiff_comm]
-      rw [Int.card_Ioc, sub_sub_cancel, Int.toNat_ofNat]
-    _ ≤ _ := by
-      rw [← sum_inter_add_sum_diff r s _]
-      refine add_le_add_left (card_nsmul_le_sum _ _ _ fun x mx ↦ ?_) _
-      rw [mem_sdiff, mem_Ioc] at mx; exact mx.1.1.le
-
-/-- Sharp upper bound for the sum of a finset of integers that is bounded above, `range` version. -/
-lemma sum_le_sum_range {s : Finset ℤ} {c : ℤ} (hs : ∀ x ∈ s, x ≤ c) :
-    ∑ x ∈ s, x ≤ ∑ n ∈ range #s, (c - n) := by
-  convert sum_le_sum_Ioc hs
-  refine sum_nbij (c - ·) ?_ ?_ ?_ (fun _ _ ↦ rfl)
-  · intro x mx; rw [mem_Ioc]; dsimp only; rw [mem_range] at mx; omega
-  · intro x mx y my (h : c - x = c - y); omega
-  · intro x mx; simp_rw [coe_range, Set.mem_image, Set.mem_Iio]
-    rw [mem_coe, mem_Ioc] at mx
-    use (c - x).toNat; omega
-
-/-- Sharp lower bound for the sum of a finset of integers that is bounded below, `Ico` version. -/
-lemma sum_Ico_le_sum {s : Finset ℤ} {c : ℤ} (hs : ∀ x ∈ s, c ≤ x) :
-    ∑ x ∈ Ico c (c + #s), x ≤ ∑ x ∈ s, x := by
-  set r := Ico c (c + #s)
-  calc
-    _ ≤ ∑ x ∈ r ∩ s, x + #(r \ s) • (c + #s) := by
-      rw [← sum_inter_add_sum_diff r s _]
-      refine add_le_add_left (sum_le_card_nsmul _ _ _ fun x mx ↦ ?_) _
-      rw [mem_sdiff, mem_Ico] at mx; exact mx.1.2.le
-    _ = ∑ x ∈ s ∩ r, x + #(s \ r) • (c + #s) := by
-      rw [inter_comm, card_sdiff_comm]
-      rw [Int.card_Ico, add_sub_cancel_left, Int.toNat_ofNat]
-    _ ≤ _ := by
-      rw [← sum_inter_add_sum_diff s r _]
-      refine add_le_add_left (card_nsmul_le_sum _ _ _ fun x mx ↦ ?_) _
-      rw [mem_sdiff, mem_Ico, not_and] at mx
-      have := mx.2 (hs _ mx.1); omega
-
-/-- Sharp lower bound for the sum of a finset of integers that is bounded below, `range` version. -/
-lemma sum_range_le_sum {s : Finset ℤ} {c : ℤ} (hs : ∀ x ∈ s, c ≤ x) :
-    ∑ n ∈ range #s, (c + n) ≤ ∑ x ∈ s, x := by
-  convert sum_Ico_le_sum hs
-  refine sum_nbij (c + ·) ?_ ?_ ?_ (fun _ _ ↦ rfl)
-  · intro x mx; rw [mem_Ico]; dsimp only; rw [mem_range] at mx; omega
-  · intro x mx y my (h : c + x = c + y); omega
-  · intro x mx; simp_rw [coe_range, Set.mem_image, Set.mem_Iio]
-    rw [mem_coe, mem_Ico] at mx
-    use (x - c).toNat; omega
-
-end Finset
-
 /-- The collection of ball heights in the solution. -/
 def pool (a : ℕ → ℤ) : ℕ → Finset ℤ
   | 0 => ∅
   | t + 1 => (insert (a t) ((pool a t).erase 0)).map (Equiv.subRight (1 : ℤ))
-
-/-- A bounded monotone function `ℕ → ℕ` converges. No topology is needed to prove this. -/
-lemma converges_of_monotone_of_bounded {f : ℕ → ℕ} (mono_f : Monotone f)
-    {c : ℕ} (hc : ∀ n, f n ≤ c) : ∃ b N, ∀ n ≥ N, f n = b := by
-  induction c with
-  | zero => use 0, 0, fun n _ ↦ Nat.eq_zero_of_le_zero (hc n)
-  | succ c ih =>
-    by_cases h : ∀ n, f n ≤ c
-    · exact ih h
-    · push_neg at h; obtain ⟨N, hN⟩ := h
-      replace hN : f N = c + 1 := by specialize hc N; omega
-      use c + 1, N; intro n hn
-      specialize mono_f hn; specialize hc n; omega
 
 variable {a : ℕ → ℤ} (ha : Condition a) {t : ℕ}
 
