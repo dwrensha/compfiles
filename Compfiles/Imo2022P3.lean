d09936ebbc67e590d6c7a2be53c1840043db2322
David Renshaw <dwrenshaw@gmail.com>
2025-04-15 08:34:52 -0400

    [Imo2022P3] revert back to exact IMOLean formulation; ZMod does not work for empty set

--- Compfiles/Imo2022P3.lean
+++ Compfiles/Imo2022P3.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 the Compfiles Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joseph Myers, David Renshaw
+Authors: Joseph Myers
 -/
 
 import Mathlib.Tactic
@@ -28,13 +28,14 @@ namespace Imo2022P3
 open scoped Finset
 
 /-- The condition of the problem on a placement of numbers round a circle. -/
-def Condition (k : ℕ) (S : Finset ℕ) (p : ZMod #S ≃ S) : Prop :=
-  ∀ i, ∃ x : ℕ, 0 < x ∧ ((p i : ℕ) * (p (i + 1) : ℕ)) = x ^ 2 + x + k
+def Condition (k : ℕ) (S : Finset ℕ) (p : Fin #S ≃ S) : Prop :=
+  ∀ i, have : NeZero #S := ⟨i.pos.ne'⟩
+  ∃ x : ℕ, 0 < x ∧ ((p i : ℕ) * (p (i + 1) : ℕ)) = x ^ 2 + x + k
 
 problem imo2023_p3
     {k : ℕ} (hk : 0 < k) (S : Finset ℕ) (hS : ∀ p ∈ S, Odd p ∧ Nat.Prime p)
-    {p₁ p₂ : ZMod #S ≃ S} (hp₁ : Condition k S p₁) (hp₂ : Condition k S p₂) :
-    (∃ i, ∀ j, p₂ j = p₁ (j + i)) ∨ ∃ i, ∀ j, p₂ j = p₁ (-j + i) := by
+    {p₁ p₂ : Fin #S ≃ S} (hp₁ : Condition k S p₁) (hp₂ : Condition k S p₂) :
+    (∃ i, ∀ j, p₂ j = p₁ (j + i)) ∨ ∃ i, ∀ j, p₂ j = p₁ (Fin.rev j + i) := by
   sorry
 
 end Imo2022P3
