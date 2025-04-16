27fd40b12002525341747c88f77d7d8f5048bf64
David Renshaw <dwrenshaw@gmail.com>
2025-04-16 09:46:18 -0400

    use simpler John Scholes solution

--- Compfiles/Imo1983P6.lean
+++ Compfiles/Imo1983P6.lean
@@ -26,137 +26,6 @@ namespace Imo1983P6
 
 snip begin
 
-lemma mylemma_1
-  (a b c : ℝ)
-  (x y z : ℝ)
-  (h₀ : 0 < a ∧ 0 < b ∧ 0 < c)
-  (h₂: c ≤ b ∧ b ≤ a)
-  (h₃: z ≤ y ∧ y ≤ x) :
-  a * z + c * y + b * x ≤ c * z + b * y + a * x := by
-  suffices h₄: c * (y - z) + b * (x - y) ≤ a * (x - z)
-  . linarith
-  . have h₅: c * (y - z) + b * (x - y) ≤ b * (y - z) + b * (x - y) := by
-      simp
-      refine mul_le_mul h₂.1 ?_ ?_ ?_
-      . exact le_rfl
-      . exact sub_nonneg_of_le h₃.1
-      . exact le_of_lt h₀.2.1
-    refine le_trans h₅ ?_
-    rw [mul_sub, mul_sub, add_comm]
-    rw [← add_sub_assoc, sub_add_cancel]
-    rw [← mul_sub]
-    refine mul_le_mul h₂.2 ?_ ?_ ?_
-    . exact le_rfl
-    . refine sub_nonneg_of_le ?_
-      exact le_trans h₃.1 h₃.2
-    . exact le_of_lt h₀.1
-
-
-lemma mylemma_2
-  (a b c : ℝ)
-  (x y z : ℝ)
-  (h₀ : 0 < a ∧ 0 < b ∧ 0 < c)
-  (h₂: c ≤ b ∧ b ≤ a)
-  (h₃: z ≤ y ∧ y ≤ x) :
-  b * z + a * y + c * x ≤ c * z + b * y + a * x := by
-  suffices h₄: c * (x - z) + b * (z - y) ≤ a * (x - y)
-  . linarith
-  . have h₅: c * (x - z) + b * (z - y) ≤ b * (x - z) + b * (z - y) := by
-      simp
-      refine mul_le_mul h₂.1 ?_ ?_ ?_
-      . exact le_rfl
-      . refine sub_nonneg_of_le ?_
-        exact le_trans h₃.1 h₃.2
-      . exact le_of_lt h₀.2.1
-    refine le_trans h₅ ?_
-    rw [mul_sub, mul_sub]
-    rw [← add_sub_assoc, sub_add_cancel]
-    rw [← mul_sub]
-    refine mul_le_mul h₂.2 ?_ ?_ ?_
-    . exact le_rfl
-    . exact sub_nonneg_of_le h₃.2
-    . exact le_of_lt h₀.1
-
-
--- case #1
-lemma mylemma_cba
-  (a b c : ℝ)
-  (hap : 0 < a )
-  (hbp : 0 < b )
-  (hcp : 0 < c )
-  (h₁ : c < a + b)
-  -- (h₂ : b < a + c)
-  (h₃ : a < b + c)
-  (hba: b ≤ a)
-  (hcb: c ≤ b) :
-  0 ≤ a^2 * b * (a - b) + b^2 * c * (b - c) + c^2 * a * (c - a) := by
-  have g₀: b * c ≤ a * c := by exact (mul_le_mul_iff_of_pos_right hcp).mpr hba
-  have g₁: a * c ≤ a * b := by exact (mul_le_mul_iff_of_pos_left hap).mpr hcb
-  have g₂: a * (b + c - a) ≤ b * (a + c - b) := by
-    have g₂₁: 0 ≤ (a-b) * (a+b-c) := by
-      refine mul_nonneg ?_ ?_
-      . exact sub_nonneg_of_le hba
-      . refine le_of_lt ?_
-        exact sub_pos.mpr h₁
-    linarith
-  have g₃: b * (a + c - b) ≤ c * (a + b - c) := by
-    have g₃₁: 0 ≤ (b - c) * (b + c - a) := by
-      refine mul_nonneg ?_ ?_
-      . exact sub_nonneg_of_le hcb
-      . refine le_of_lt ?_
-        exact sub_pos.mpr h₃
-    linarith
-  have g₄: (a * b) * (a * (b + c - a)) + (b * c) * (b * (a + c - b)) + (a * c) * (c * (a + b - c))
-      ≤ (b * c) * (a * (b + c - a)) + (a * c) * (b * (a + c - b)) + (a * b) * (c * (a + b - c)) := by
-    refine mylemma_1 (a * b) (a * c) (b * c) (c * (a + b - c)) (b * (a + c - b)) (a * (b + c - a)) ?_ ?_ ?_
-    . constructor
-      . exact mul_pos hap hbp
-      . constructor
-        . exact mul_pos hap hcp
-        . exact mul_pos hbp hcp
-    . exact { left := g₀, right := g₁ }
-    . exact { left := g₂, right := g₃ }
-  linarith
-
--- tight version
-lemma mylemma_cba_tight
-    (a b c : ℝ)
-    (hap : 0 < a)
-    (hbp : 0 < b)
-    (hcp : 0 < c)
-    (h₁ : c < a + b)
-    (h₃ : a < b + c)
-    (hba: b ≤ a)
-    (hcb: c ≤ b) :
-    0 ≤ a^2 * c * (a - c) + c^2 * b * (c - b) + b^2 * a * (b - a) := by
-  have g₀: b * c ≤ a * c := by exact (mul_le_mul_iff_of_pos_right hcp).mpr hba
-  have g₁: a * c ≤ a * b := by exact (mul_le_mul_iff_of_pos_left hap).mpr hcb
-  have g₂: a * (b + c - a) ≤ b * (a + c - b) := by
-    have g₂₁: 0 ≤ (a-b) * (a+b-c) := by
-      refine mul_nonneg ?_ ?_
-      . exact sub_nonneg_of_le hba
-      . refine le_of_lt ?_
-        exact sub_pos.mpr h₁
-    linarith
-  have g₃: b * (a + c - b) ≤ c * (a + b - c) := by
-    have g₃₁: 0 ≤ (b - c) * (b + c - a) := by
-      refine mul_nonneg ?_ ?_
-      . exact sub_nonneg_of_le hcb
-      . refine le_of_lt ?_
-        exact sub_pos.mpr h₃
-    linarith
-  have g₄: (a * c) * (a * (b + c - a)) + (a * b) * (b * (a + c - b)) + (b * c) * (c * (a + b - c))
-      ≤ (b * c) * (a * (b + c - a)) + (a * c) * (b * (a + c - b)) + (a * b) * (c * (a + b - c)) := by
-    refine mylemma_2 (a * b) (a * c) (b * c) (c * (a + b - c)) (b * (a + c - b)) (a * (b + c - a)) ?_ ?_ ?_
-    . constructor
-      . exact mul_pos hap hbp
-      . constructor
-        . exact mul_pos hap hcp
-        . exact mul_pos hbp hcp
-    . exact { left := g₀, right := g₁ }
-    . exact { left := g₂, right := g₃ }
-  linarith
-
 section triangle_inequality
 
 variable {V P : Type*} [NormedAddCommGroup V] [NormedSpace ℝ V]
@@ -237,44 +106,43 @@ problem imo1983_p6
     linarith
 
   clear T ha hb hc
+
+  -- https://prase.cz/kalva/imo/isoln/isoln836.html
+  set x := (-a + b + c) / 2
+  set y := (a - b + c) / 2
+  set z := (a + b - c) / 2
+  have hx : 0 < x := by unfold x; linarith
+  have hy : 0 < y := by unfold y; linarith
+  have hz : 0 < z := by unfold z; linarith
+
   constructor
-  · wlog ho₀: b ≤ a generalizing a b c
-    . clear this
-      push_neg at ho₀
-      wlog ho₁: c ≤ b generalizing a b c
-      . clear this
-        push_neg at ho₁ -- a < b < c
-        rw [add_comm] at h₁ h₂ h₃
-        have g₀: 0 ≤ c ^ 2 * a * (c - a) + a ^ 2 * b * (a - b) + b ^ 2 * c * (b - c) := by
-          exact mylemma_cba_tight c b a hc' hb' ha' h₃ h₁ (le_of_lt ho₁) (le_of_lt ho₀)
-        linarith
-      . wlog ho₂: c ≤ a generalizing a b c
-        . clear this -- a < c ≤ b
-          push_neg at ho₂
-          rw [add_comm] at h₁ h₂
-          have g₀: 0 ≤ b ^ 2 * c * (b - c) + c ^ 2 * a * (c - a) + a ^ 2 * b * (a - b) := by
-            exact mylemma_cba b c a hb' hc' ha' h₃ h₂ ho₁ (le_of_lt ho₂)
-          linarith
-        . -- c ≤ a < b
-          rw [add_comm] at h₁
-          have g₀: 0 ≤ b ^ 2 * c * (b - c) + c ^ 2 * a * (c - a) + a ^ 2 * b * (a - b) := by
-            exact mylemma_cba_tight b a c hb' ha' hc' h₁ h₂ (le_of_lt ho₀) ho₂
-          linarith
-    . wlog ho₁: c ≤ b generalizing a b c
-      . clear this
-        push_neg at ho₁
-        wlog ho₂: c ≤ a generalizing a b c
-        . clear this
-          push_neg at ho₂ -- b < a < c
-          rw [add_comm] at h₂ h₃
-          have g₀: 0 ≤ c ^ 2 * a * (c - a) + a ^ 2 * b * (a - b) + b ^ 2 * c * (b - c) := by
-            exact mylemma_cba c a b hc' ha' hb' h₂ h₁ (le_of_lt ho₂) ho₀
-          linarith
-        . rw [add_comm] at h₃
-          exact mylemma_cba_tight a c b ha' hc' hb' h₂ h₃ ho₂ (le_of_lt ho₁)
-      . exact mylemma_cba a b c ha' hb' hc' h₁ h₃ ho₀ ho₁
+  · suffices H : x * y * z * (z + x + y) ≤ x * y ^3 + y * z^3 + z * x ^3 by
+      unfold x y z at H
+      nlinarith
+    let f : Fin 3 → ℝ := ![√x * √(y^3), √y * √(z^3), √z * √(x^3)]
+    let g : Fin 3 → ℝ := ![√z, √x, √y]
+    have hsum := Finset.sum_mul_sq_le_sq_mul_sq (Finset.univ) f g
+    simp only [Fin.sum_univ_three, f, g] at hsum
+    dsimp at hsum
+    rw [show x^3 = x^2 * x from rfl, show y^3 = y^2 * y from rfl,
+        show z^3 = z^2 * z from rfl] at hsum
+    rw [Real.sqrt_mul (by positivity), Real.sqrt_mul (by positivity),
+        Real.sqrt_mul (by positivity)] at hsum
+    rw [Real.sqrt_sq hx.le, Real.sqrt_sq hy.le, Real.sqrt_sq hz.le] at hsum
+    have h1 : √x * (y * √y) * √z + √y * (z * √z) * √x + √z * (x * √x) * √y =
+              (√x * √y * √z) * (z + x + y) := by ring
+    rw [h1] at hsum; clear h1
+    simp only [mul_pow] at hsum
+    simp only [Real.sq_sqrt hx.le, Real.sq_sqrt hy.le, Real.sq_sqrt hz.le] at hsum
+    rw [pow_two (z + x + y), ←mul_assoc] at hsum
+    have h2 : 0 < (z + x + y) := by positivity
+    rw [show x^2 * x = x^3 from rfl, show y^2 * y = y^3 from rfl,
+        show z^2 * z = z^3 from rfl] at hsum
+    exact le_of_mul_le_mul_right hsum h2
   constructor
   · intro h
+    have hxyz : x * y * z * (z + x + y) = x * y ^3 + y * z^3 + z * x ^3 := by
+      sorry
     sorry
   · rintro ⟨rfl, rfl⟩
     simp
