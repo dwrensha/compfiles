78a197bbfa08554ea5c1cd7d0c4b66b4941cc6ea
David Renshaw <dwrenshaw@gmail.com>
2025-05-04 09:15:37 -0400

    update mathlib

--- Compfiles/Imo2024P1.lean
+++ Compfiles/Imo2024P1.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Myers
 -/
 
+import Mathlib.Algebra.Order.BigOperators.Group.LocallyFinite
 import Mathlib.Tactic
 
 import ProblemExtraction
