b0c290ffc02fa744455cda8754dea68ef1e75759
ClaytonKnittel <cknit1999@gmail.com>
2025-05-15 09:58:05 -0700

    Author

--- Compfiles/Imo2023P1.lean
+++ Compfiles/Imo2023P1.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 The Compfiles Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors:
+Authors: Clayton Knittel
 -/
 
 import Mathlib.Tactic
