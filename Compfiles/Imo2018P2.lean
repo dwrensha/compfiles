18de7b3694d303690bcd6aa86c7e162ee1beaf79
Clayton Knittel <cknit1999@gmail.com>
2025-05-20 10:25:09 -0700

    Progress...

--- Compfiles/Imo2018P2.lean
+++ Compfiles/Imo2018P2.lean
@@ -49,20 +49,93 @@ theorem mod_dvd_not_dvd_add {n d : ℕ} {a : ZMod n} (hn : n ≠ 0)
   apply ha
   exact Nat.dvd_of_mod_eq_zero y
 
-theorem oneof_3_dvd_3 {n : ℕ} (i : ZMod n) (hn : 3 ≤ n) (h : 3 ∣ n) :
+lemma ZMod.val_mod_eq_zero_iff_eq_zero {n : ℕ} (x : ZMod n) :
+    x.val = 0 ↔ x = 0 := by
+  constructor
+  . exact (ZMod.val_eq_zero x).mp
+  . intro h
+    rw [h, ZMod.val_zero]
+
+lemma ZMod.val_eq_mod_n {n : ℕ} (x : ZMod n) : x.val % n = x.val := by
+  if h : n = 0 then
+    simp_rw [h, Nat.mod_zero]
+  else
+    have : NeZero n := ⟨h⟩
+    exact Nat.mod_eq_of_lt (ZMod.val_lt x)
+
+theorem oneof_3_dvd_3' {n : ℕ} (i : ZMod n) (hn : 3 ≤ n) (h : 3 ∣ n) :
+    (3 ∣ i.val ∧ ¬3 ∣ (i + 1).val ∧ ¬3 ∣ (i + 2).val) ∨
+    (¬3 ∣ i.val ∧ 3 ∣ (i + 1).val ∧ ¬3 ∣ (i + 2).val) ∨
+    (¬3 ∣ i.val ∧ ¬3 ∣ (i + 1).val ∧ 3 ∣ (i + 2).val) := by
+
+  -- First, map the `.val` values mod 3
+  let a := (i.val : ZMod 3)
+  let b := ((i + 1).val : ZMod 3)
+  let c := ((i + 2).val : ZMod 3)
+
+  -- In ZMod 3, addition and coercion are well-behaved
+  have h1 : b = a + 1 := by
+    apply_fun ZMod.val
+    . dsimp [a, b]
+      sorry
+    · trivial
+
+  have h2 : c = a + 2 := by
+    sorry
+
+  -- Since ZMod 3 only has 3 elements, exactly one of a, b, or c is zero
+  let x : Finset (ZMod 3) := {a, b, c}
+  have : x.card = 3 := by
+    dsimp [x]
+    rw [h1, h2]
+    repeat rw [Finset.card_insert_of_not_mem]
+    . simp
+    all_goals {
+      simp
+      trivial
+    }
+
+  have : i.val = a.val := by sorry
+  rw [this]
+  have : (i + 1).val = b.val := by sorry
+  rw [this]
+  have : (i + 2).val = c.val := by sorry
+  rw [this]
+  repeat rw [Nat.dvd_iff_mod_eq_zero, ZMod.val_eq_mod_n,
+             ZMod.val_mod_eq_zero_iff_eq_zero _]
+
+  decide
+
+theorem oneof_3_dvd_3 {n : ℕ} (i : ZMod n) (hn : 3 ≤ n) (hd : 3 ∣ n) :
     (3 ∣ i.val ∧ ¬3 ∣ (i + 1).val ∧ ¬3 ∣ (i + 2).val) ∨
     (¬3 ∣ i.val ∧ 3 ∣ (i + 1).val ∧ ¬3 ∣ (i + 2).val) ∨
     (¬3 ∣ i.val ∧ ¬3 ∣ (i + 1).val ∧ 3 ∣ (i + 2).val) := by
   have n_ne_0 : n ≠ 0 := Nat.ne_zero_of_lt hn
   have : Fact (1 < n) := ⟨Nat.lt_of_add_left_lt hn⟩
 
+  have : 3 ∣ i.val % n ↔ 3 ∣ i.val := by exact Nat.dvd_mod_iff hd
+
+  mod_cases h : i.val % 3
+  . left
+    have : 3 ∣ i.val := Nat.dvd_of_mod_eq_zero h
+    refine ⟨this, ?_⟩
+    repeat rw [Nat.dvd_iff_mod_eq_zero, ZMod.val_add,
+               Nat.mod_mod_of_dvd _ hd,
+               Nat.add_mod i.val]
+    rw [h, ZMod.val_one, ZMod.val_ofNat_of_lt hn]
+    norm_num
+  . right
+    left
+    have : 3 ∣ (i.val + 1) := sorry
+    sorry
+  . sorry
+
   if h₁ : 3 ∣ i.val then
     refine Or.inl ⟨h₁, ?_, ?_⟩
-    . refine mod_dvd_not_dvd_add n_ne_0 h ?_ i h₁
-      rw [ZMod.val_one]
+    <;> refine mod_dvd_not_dvd_add n_ne_0 h ?_ i h₁
+    . rw [ZMod.val_one]
       norm_num
-    . refine mod_dvd_not_dvd_add n_ne_0 h ?_ i h₁
-      rw [ZMod.val_ofNat_of_lt hn]
+    . rw [ZMod.val_ofNat_of_lt hn]
       norm_num
   else if h₂ : 3 ∣ (i + 1).val then
     refine Or.inr (Or.inl ⟨h₁, h₂, ?_⟩)
@@ -108,9 +181,8 @@ lemma mod_3_satisfies {n : ℕ} (hn : 3 ≤ n) (h : 3 ∣ n) :
 
   refine (oneof_3_dvd_3 i hn h).by_cases ?_ (Or.by_cases · ?_ ?_)
   all_goals {
-    intro ⟨h1, h2, h3⟩
-    dsimp [fn]
-    simp [h1, h2, h3]
+    intro h
+    simp [fn, h]
     linarith
   }
 
