0abc23b9381badae1c77a5a24730d6a5548b7b94
Clayton Knittel <cknit1999@gmail.com>
2025-05-20 12:57:35 -0700

    Progress on other direction

--- Compfiles/Imo2018P2.lean
+++ Compfiles/Imo2018P2.lean
@@ -30,35 +30,65 @@ abbrev P {n : ℕ} (a : ZMod n → ℝ) :=
 
 snip begin
 
-theorem oneof_3_dvd_3 {n : ℕ} (i : ZMod n) (hn : 3 ≤ n) (hd : 3 ∣ n) :
-    (3 ∣ i.val ∧ ¬3 ∣ (i + 1).val ∧ ¬3 ∣ (i + 2).val) ∨
-    (¬3 ∣ i.val ∧ 3 ∣ (i + 1).val ∧ ¬3 ∣ (i + 2).val) ∨
-    (¬3 ∣ i.val ∧ ¬3 ∣ (i + 1).val ∧ 3 ∣ (i + 2).val) := by
-  have : Fact (1 < n) := ⟨Nat.lt_of_add_left_lt hn⟩
-  mod_cases h : i.val % 3
-  all_goals {
-    repeat rw [Nat.dvd_iff_mod_eq_zero]
-    repeat rw [ZMod.val_add, Nat.mod_mod_of_dvd _ hd,
-               Nat.add_mod i.val]
-    rw [h, ZMod.val_one, ZMod.val_ofNat_of_lt hn]
-    norm_num
-  }
-
-lemma mod_3_satisfies {n : ℕ} (hn : 3 ≤ n) (h : 3 ∣ n) :
+lemma mod_3_satisfies {n : ℕ} (hn : 3 ≤ n) (hd : 3 ∣ n) :
     ∃ a : ZMod n → ℝ, P a := by
+  have : Fact (1 < n) := ⟨Nat.lt_of_add_left_lt hn⟩
+
   let fn : ZMod n → ℝ := fun i => if 3 ∣ i.val then 2 else -1
   exists fn
   intro i
 
-  refine (oneof_3_dvd_3 i hn h).by_cases ?_ (Or.by_cases · ?_ ?_)
+  have :
+      (3 ∣ i.val ∧ ¬3 ∣ (i + 1).val ∧ ¬3 ∣ (i + 2).val) ∨
+      (¬3 ∣ i.val ∧ 3 ∣ (i + 1).val ∧ ¬3 ∣ (i + 2).val) ∨
+      (¬3 ∣ i.val ∧ ¬3 ∣ (i + 1).val ∧ 3 ∣ (i + 2).val) := by
+    mod_cases h : i.val % 3
+    all_goals {
+      repeat rw [Nat.dvd_iff_mod_eq_zero]
+      repeat rw [ZMod.val_add, Nat.mod_mod_of_dvd _ hd,
+                 Nat.add_mod i.val]
+      rw [h, ZMod.val_one, ZMod.val_ofNat_of_lt hn]
+      norm_num
+    }
+
+  refine this.by_cases ?_ (Or.by_cases · ?_ ?_)
   all_goals {
     intro h
     simp [fn, h]
     linarith
   }
 
-lemma satisfies_is_mod_3 {n : ℕ} (h : ∃ a : ZMod n → ℝ, P a) :
-    3 ∣ n := sorry
+example : ¬ ∃ x : ℝ, x * x - x + 1 = 0 := by
+  intro ⟨x, hx⟩
+  suffices x * x - x + 1 ≠ 0 from this hx
+  have : x * x - x + 1 = (x - 1/2)^2 + 3/4 := by ring_nf
+  have : (x - 1/2)^2 ≥ 0 := sq_nonneg _
+  linarith
+
+lemma satisfies_is_mod_3 {n : ℕ} (hn : 3 ≤ n) (h : ∃ a : ZMod n → ℝ, P a) :
+    3 ∣ n := by
+  have : Fact (1 < n) := ⟨Nat.lt_of_add_left_lt hn⟩
+
+  obtain ⟨a, ha⟩ := h
+  have {i : ZMod n} : a i = a (i + 3) := by sorry
+
+  by_contra h
+  let x := ha 0
+  rw [← one_add_one_eq_two, ← add_assoc] at x
+  have {i : ZMod n} : a i = a (i + 1) := by sorry
+  repeat rw [← this] at x
+  apply_fun (· - a 0) at x
+  rw [add_sub_right_comm] at x
+  field_simp at x
+
+  have : ¬ ∃ x : ℝ, x * x - x + 1 = 0 := by
+    intro ⟨x, hx⟩
+    suffices x * x - x + 1 ≠ 0 from this hx
+    have : x * x - x + 1 = (x - 1/2)^2 + 3/4 := by ring_nf
+    have : (x - 1/2)^2 ≥ 0 := sq_nonneg _
+    linarith
+  apply this
+  exists a 0
 
 snip end
 
@@ -68,6 +98,6 @@ problem imo2018_p2 (n : ℕ) :
   · rintro ⟨hn1, hn2⟩
     exact ⟨hn1, mod_3_satisfies hn1 hn2⟩
   · rintro ⟨hn1, hn2⟩
-    exact ⟨hn1, satisfies_is_mod_3 hn2⟩
+    exact ⟨hn1, satisfies_is_mod_3 hn1 hn2⟩
 
 end Imo2018P2
