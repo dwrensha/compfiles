b5a327b2121d159cb155be165b5a7a8c9efba84f
Clayton Knittel <cknit1999@gmail.com>
2025-05-20 17:03:19 -0700

    Progress

--- Compfiles/Imo2018P2.lean
+++ Compfiles/Imo2018P2.lean
@@ -4,11 +4,26 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors:
 -/
 
+import Mathlib.Algebra.ModEq
 import Mathlib.Tactic
 
 import ProblemExtraction
 
 problem_file { tags := [.Algebra] }
+
+lemma three_periodic {n : ℕ} (i c : ZMod n) (a : ZMod n → ℝ)
+    [NeZero n] (test : ∀ i : ZMod n, a i = a (i + 3)) :
+    a i = a (i + 3 * c) := by
+  let k := c.val
+  have h_cast : c = ↑k := Eq.symm (ZMod.natCast_zmod_val c)
+  rw [h_cast] -- , ← Nat.cast_mul]
+  induction k with
+  | zero =>
+      simp
+  | succ d ih =>
+      rw [Nat.cast_add, mul_add, Nat.cast_one, ← add_assoc, mul_one]
+      rw [← test _]
+      exact ih
 
 /-!
 # International Mathematical Olympiad 2018, Problem 2
@@ -58,24 +73,53 @@ lemma mod_3_satisfies {n : ℕ} (hn : 3 ≤ n) (hd : 3 ∣ n) :
     linarith
   }
 
-example : ¬ ∃ x : ℝ, x * x - x + 1 = 0 := by
-  intro ⟨x, hx⟩
-  suffices x * x - x + 1 ≠ 0 from this hx
-  have : x * x - x + 1 = (x - 1/2)^2 + 3/4 := by ring_nf
-  have : (x - 1/2)^2 ≥ 0 := sq_nonneg _
-  linarith
+lemma not_dvd_prime_exists_mod_inverse {n p : ℕ}
+    (pp : p.Prime) (hn : 1 < n) (h : ¬p ∣ n) :
+    ∃ c, p * c ≡ 1 [MOD n] := by
+  change ∃ c, _ % n = _ % n
+  rw [Nat.mod_eq_of_lt hn]
+  refine Nat.exists_mul_emod_eq_one_of_coprime ?_ hn
+  exact (pp.coprime_iff_not_dvd).mpr h
+
+lemma not_dvd_prime_exists_mod_inverse' {n p : ℕ}
+    (pp : p.Prime) (hn : 1 < n) (h : ¬p ∣ n) :
+    ∃ c : ZMod n, ↑p * c = 1 := by
+  let ⟨c, hc⟩ := not_dvd_prime_exists_mod_inverse pp hn h
+  exists c
+  rw [← Nat.cast_mul, ← Nat.cast_one]
+  exact (ZMod.natCast_eq_natCast_iff (p * c) 1 n).mpr hc
 
 lemma satisfies_is_mod_3 {n : ℕ} (hn : 3 ≤ n) (h : ∃ a : ZMod n → ℝ, P a) :
     3 ∣ n := by
-  have : Fact (1 < n) := ⟨Nat.lt_of_add_left_lt hn⟩
+  have n_gt_1 : 1 < n := Nat.lt_of_add_left_lt hn
+  have : Fact (1 < n) := ⟨n_gt_1⟩
 
   obtain ⟨a, ha⟩ := h
-  have {i : ZMod n} : a i = a (i + 3) := by sorry
+  have {i : ZMod n} : a i = a (i + (Nat.cast 3)) := by sorry
+  have three_periodic {i c : ZMod n} : a i = a ((Nat.cast 3) * c + i) := by
+    let k := c.val
+    have h_cast : c = ↑k := Eq.symm (ZMod.natCast_zmod_val c)
+    rw [h_cast]
+    induction k with
+    | zero =>
+        simp
+    | succ d ih =>
+        rw [Nat.cast_add, mul_add, Nat.cast_one, add_right_comm, mul_one]
+        rw [← this]
+        exact ih
 
   by_contra h
   let x := ha 0
   rw [← one_add_one_eq_two, ← add_assoc] at x
-  have {i : ZMod n} : a i = a (i + 1) := by sorry
+
+  have {i : ZMod n} : a i = a (i + 1) := by
+    mod_cases h_dvd : n % 3
+    . absurd h
+      exact Nat.dvd_of_mod_eq_zero h_dvd
+    all_goals {
+      let ⟨x, y⟩ := not_dvd_prime_exists_mod_inverse' Nat.prime_three n_gt_1 h
+      rw [← y, add_comm, ← three_periodic]
+    }
   repeat rw [← this] at x
   apply_fun (· - a 0) at x
   rw [add_sub_right_comm] at x
