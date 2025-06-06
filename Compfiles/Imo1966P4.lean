c6784bbb5936448c78c023bbb64ee735a3a627f2
David Renshaw <dwrenshaw@gmail.com>
2025-06-05 22:19:52 -0400

    [Imo1966P4] don't open Real

--- Compfiles/Imo1966P4.lean
+++ Compfiles/Imo1966P4.lean
@@ -23,8 +23,6 @@ integer and k any integer,
 
 namespace Imo1966P4
 
-open Real
-
 problem imo1966_p4 (n : ℕ) (x : ℝ)
     (hx : ∀ t : ℕ, ∀ k : ℤ, x ≠ k * Real.pi / 2^t) :
     ∑ i ∈ Finset.range n, 1 / Real.sin (2^(i + 1) * x) =
@@ -36,22 +34,22 @@ problem imo1966_p4 (n : ℕ) (x : ℝ)
       rw [Finset.sum_range_succ, ih]
       let θ := 2 ^ n * x
       have hθ : θ = 2 ^ n * x := by rfl
-      have h₀ : sin (2^(n+1) * x) ≠ 0 := by
+      have h₀ : Real.sin (2^(n+1) * x) ≠ 0 := by
         intro h
-        rw [sin_eq_zero_iff] at h
+        rw [Real.sin_eq_zero_iff] at h
         obtain ⟨k, hk⟩ := h
         apply hx (n+1) k
         rw [eq_div_iff (pow_ne_zero (n+1) (by norm_num)), mul_comm, ← hk]
-      have h₁ : sin θ ≠ 0 ∧ cos θ ≠ 0 := by
+      have h₁ : Real.sin θ ≠ 0 ∧ Real.cos θ ≠ 0 := by
         rw [pow_succ, mul_comm (2^n) 2, mul_assoc, ← hθ] at h₀
-        rw [sin_two_mul] at h₀
+        rw [Real.sin_two_mul] at h₀
         constructor
         · rw [mul_comm 2, mul_assoc] at h₀
           apply left_ne_zero_of_mul h₀
         · exact right_ne_zero_of_mul h₀
-      have h : 1 / sin (2 * θ) = cot θ - cot (2 * θ) := by
-        rw [cot_eq_cos_div_sin, cot_eq_cos_div_sin]
-        rw [sin_two_mul, cos_two_mul]
+      have h : 1 / Real.sin (2 * θ) = Real.cot θ - Real.cot (2 * θ) := by
+        simp only [Real.cot_eq_cos_div_sin]
+        rw [Real.sin_two_mul, Real.cos_two_mul]
         field_simp [h₁]
         ring
       rw [pow_succ, mul_comm (2^n) 2, mul_assoc, ← hθ, h]
