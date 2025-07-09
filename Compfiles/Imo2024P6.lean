925e7ca67affc40eaf7f3180e2278dc4c24f9054
David Renshaw <dwrenshaw@gmail.com>
2025-07-09 10:16:40 -0400

    [Imo2024P6] remove unneeded simp attribute

--- Compfiles/Imo2024P6.lean
+++ Compfiles/Imo2024P6.lean
@@ -75,7 +75,6 @@ lemma Aquaesulian.apply_zero : f 0 = 0 := by
   refine h.injective ?_
   convert h.apply_apply_add 0 using 1 <;> simp
 
-@[simp]
 lemma Aquaesulian.apply_neg_apply_add (x : G) : f (-(f x)) + x = 0 := by
   rcases h x (-(f x)) with hc | hc
   · rw [add_neg_cancel, ← h.apply_zero] at hc
