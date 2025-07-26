6799c6048d3a8755a9f82c81aebf429692e50ff7
m-ow <maxonomar@ciencias.unam.mx>
2025-07-26 10:09:14 -0600

    prove first part

--- Compfiles/Imo1972P4.lean
+++ Compfiles/Imo1972P4.lean
@@ -1,10 +1,11 @@
 /-
 Copyright (c) 2024 The Compfiles Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: InternLM-MATH LEAN Formalizer v0.1
+Authors: InternLM-MATH LEAN Formalizer v0.1, Maximiliano Onofre-Martínez
 -/
 
 import Mathlib.Tactic
+import Mathlib.Algebra.GroupWithZero.Basic
 
 import ProblemExtraction
 
@@ -34,7 +35,50 @@ problem imo1972_p4 (a b c d e : ℝ)
     (c^2 - e * b) * (d^2 - e * b) ≤ 0 ∧
     (d^2 - a * c) * (e^2 - a * c) ≤ 0 ∧
     (e^2 - b * d) * (a^2 - b * d) ≤ 0 ↔
-      (a, b, c, d, e) ∈ solution_set := by sorry
+      (a, b, c, d, e) ∈ solution_set := by
+  constructor
+  · intro _
+    have : (a * b - a * d)^2 + (b * c - b * e)^2 +
+           (c * d - c * a)^2 + (d * e - d * b)^2 +
+           (e * a - e * c)^2 + (a * c - a * e)^2 +
+           (b * d - b * a)^2 + (c * e - c * b)^2 +
+           (d * a - d * c)^2 + (e * b - e * d)^2 ≤ 0 := by linarith
 
+    have : 0 ≤ (a * b - a * d)^2 ∧ 0 ≤ (b * c - b * e)^2 ∧
+           0 ≤ (c * d - c * a)^2 ∧ 0 ≤ (d * e - d * b)^2 ∧
+           0 ≤ (e * a - e * c)^2 ∧ 0 ≤ (a * c - a * e)^2 ∧
+           0 ≤ (b * d - b * a)^2 ∧ 0 ≤ (c * e - c * b)^2 ∧
+           0 ≤ (d * a - d * c)^2 ∧ 0 ≤ (e * b - e * d)^2 := by simp [sq_nonneg]
+
+    have bd : b = d := by
+      have h₁ : (a * b - a * d)^2 = 0 := by linarith
+      have h₂ : a * b - a * d = 0 := by rwa [sq_eq_zero_iff] at h₁
+      have h₃ : a * b = a * d := by rwa [sub_eq_zero] at h₂
+      exact mul_left_cancel₀ (ne_of_gt h₀.1) h₃
+
+    have ce : c = e := by
+      have h₁ : (b * c - b * e)^2 = 0 := by linarith
+      have h₂ : b * c - b * e = 0 := by rwa [sq_eq_zero_iff] at h₁
+      have h₃ : b * c = b * e := by rwa [sub_eq_zero] at h₂
+      exact mul_left_cancel₀ (ne_of_gt h₀.2.1) h₃
+
+    have da : d = a := by
+      have h₁ : (c * d - c * a)^2 = 0 := by linarith
+      have h₂ : c * d - c * a = 0 := by rwa [sq_eq_zero_iff] at h₁
+      have h₃ : c * d = c * a := by rwa [sub_eq_zero] at h₂
+      exact mul_left_cancel₀ (ne_of_gt h₀.2.2.1) h₃
+
+    have eb : e = b := by
+      have h₁ : (d * e - d * b)^2 = 0 := by linarith
+      have h₂ : d * e - d * b = 0 := by rwa [sq_eq_zero_iff] at h₁
+      have h₃ : d * e = d * b := by rwa [sub_eq_zero] at h₂
+      exact mul_left_cancel₀ (ne_of_gt h₀.2.2.2.1) h₃
+
+    have ab : a = b := by rw [<- da]; exact bd.symm
+    have bc : b = c := by rw [<- eb]; exact ce.symm
+    have cd : c = d := by rwa [<- bc]
+    have de : d = e := by rwa [cd] at ce
+    exact ⟨ab, bc, cd, de⟩
+  · sorry
 
 end Imo1972P4
