b3a010d344c02742512185c51ef8ea536e9d6926
David Renshaw <dwrenshaw@gmail.com>
2025-07-27 12:08:13 -0400

    [Imo1975] shorten

--- Compfiles/Imo1975P2.lean
+++ Compfiles/Imo1975P2.lean
@@ -27,26 +27,9 @@ namespace Imo1975P2
 
 snip begin
 
-lemma monotone (a : ℕ → ℤ) (ha : ∀ i : ℕ, a i < a (i + 1)) : (∀ m k : ℕ , (m>k) → (a m>a k)) := by
-  intro m
-  intro k
-  intro h
-  induction m with
-  | zero =>
-    exfalso
-    exact Nat.not_lt_zero k h
-  | succ d hd =>
-    by_cases h2 : d=k
-    · rw [h2]
-      exact ha k
-    have h3 : d>k := by
-      have g1 : d+1>=k +1 := Nat.lt_iff_add_one_le.mp h
-      have g2 : d>=k := Nat.le_of_add_le_add_right g1
-      push_neg at h2
-      exact lt_of_le_of_ne g2 h2.symm
-    have h4 : a (d+1) > a d := ha d
-    have h5 := hd h3
-    exact lt_trans h5 h4
+lemma monotone (a : ℕ → ℤ) (ha : ∀ i : ℕ, a i < a (i + 1)) : ∀ m k : ℕ , m > k → a k < a m := by
+  intro m k h
+  induction m <;> grind
 
 snip end
 
@@ -63,10 +46,7 @@ problem imo1975_p2 (a : ℕ → ℤ)
         0 < s ):= by
   let b : ℕ → ℕ := fun n => (a n).natAbs
   intro i
-  have hn0 : ∀j:ℕ , a j ≠ (0 : ℤ) := by
-    intro j
-    have hn0' := apos j
-    exact Ne.symm (Int.ne_of_lt hn0')
+  have hn0 : ∀ j, a j ≠ (0 : ℤ) := fun j ↦ (Int.ne_of_lt (apos j)).symm
   intro n0
   have h1 : ∃t:ℕ , ∀ n1:ℕ , ∃ n:ℕ , n>n1 ∧ a i ∣ (a n-t) := by
     by_contra h2
@@ -84,9 +64,8 @@ problem imo1975_p2 (a : ℕ → ℤ)
           apply Finset.le_sup
           rw[List.mem_toFinset]
           exact List.mem_range.2 r
-        unfold m
-        apply Nat.lt_of_le_of_lt h4' (Nat.lt_succ_self ((List.range (b i)).toFinset.sup f))
-      have h6 : n1 > f t1 := by exact Nat.lt_trans h4 g
+        exact Nat.lt_add_one_of_le h4'
+      have h6 : n1 > f t1 := Nat.lt_trans h4 g
       apply hf t1 n1 h6
     have h4 := h3 (m+1) (Nat.lt_add_one m)
     let t_int := a (m+1) - (a i)*((a (m+1))/(a i))
@@ -102,22 +81,16 @@ problem imo1975_p2 (a : ℕ → ℤ)
     dsimp [t] at h6
     dsimp [t_int] at h6
     have g : (a (m + 1) - a i * (a (m + 1) / a i))≥0 := by
-        have r : (a i)*(a (m + 1) / a i) ≤ a (m+1) := by
-          nth_rewrite 2 [← Int.ediv_add_emod (a (m+1)) (a i)]
-          simp
-          exact Int.emod_nonneg (a (m+1)) (hn0 (i))
-        linarith
+        have r : (a i)*(a (m + 1) / a i) ≤ a (m+1) := Int.mul_ediv_self_le (hn0 i)
+        omega
     have h7 : ↑(a (m + 1) - a i * (a (m + 1) / a i)).natAbs = (a (m + 1) - a i * (a (m + 1) / a i)) := Int.natAbs_of_nonneg g
     rw [h7] at h6
-    simp at h6
+    simp only [sub_sub_cancel, dvd_mul_right, ge_iff_le, forall_const] at h6
     unfold b at h6
     have h8 : a (m + 1) - a i * (a (m + 1) / a i) < (a i):= by
       have h8' : a i * (a (m + 1) / a i) + (a i) > a (m + 1) :=  Int.lt_mul_ediv_self_add (apos i)
-      linarith
-    rw [← Int.natAbs_of_nonneg g] at h8
-    nth_rewrite 3 [← Int.natAbs_of_nonneg (le_of_lt (apos i))] at h8
-    simp at h8
-    linarith
+      omega
+    omega
   obtain ⟨t,ht⟩ := h1
   have g := ht (n0+i)
   obtain ⟨k,hk⟩ := g
@@ -131,11 +104,7 @@ problem imo1975_p2 (a : ℕ → ℤ)
           _   = a i * y - a i * x +(a k)     := by rw [hy, hx]
           _   = (y - x) * a i +(a k)        := by ring
   have rm : n0 ≤ m ∧ ∃ r s j : ℕ, a m = ↑r * a i + ↑s * a j ∧ i < j ∧ 0 < r ∧ 0 < s := by
-    have r1 : m ≥ n0 := by
-      have r1' : m > n0+i := Nat.lt_trans hk.left hm.left
-      have r1'' : m≥ n0+i := le_of_lt r1'
-      have r2 : n0+i>=n0 := Nat.le_add_right n0 i
-      exact le_trans r2 r1''
+    have r1 : m ≥ n0 := by omega
     have r2 : ∃ r s j : ℕ, a m = r * a i + s * a j ∧ i < j ∧ 0 < r ∧ 0 < s := by
       have s3 : 0<y-x := by
         have t1 : a m > a k := monotone a ha m k hm.left
@@ -145,19 +114,7 @@ problem imo1975_p2 (a : ℕ → ℤ)
         have t3 := (mul_lt_mul_left (apos i)).mp t2
         exact sub_pos.mpr t3
       have s : a m = (y-x) * a i + 1 * a k ∧ i < k ∧ 0 < (y - x).natAbs ∧ (0 : ℕ)<(1 : ℕ) := by
-        have s1 : a m = (y-x) * a i + 1 * a k := by
-          rw [g]
-          ring
-        have s2 : i<k := by
-          have s2' : n0+i≥i := by exact Nat.le_add_left i n0
-          exact lt_of_le_of_lt s2' hk.left
-
-        have s4 : (0 : ℕ)<(1 : ℕ) := Nat.zero_lt_one
-        have s3' : (y-x)≠0 := by
-          symm
-          exact ne_of_lt s3
-        have s3'' := Int.natAbs_pos.mpr s3'
-        exact And.intro s1 (And.intro s2 (And.intro s3'' s4))
+        omega
       use (y-x).natAbs
       use 1
       use k
@@ -167,4 +124,5 @@ problem imo1975_p2 (a : ℕ → ℤ)
       exact s
     exact And.intro r1 r2
   use m
+
 end Imo1975P2
