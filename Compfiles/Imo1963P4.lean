a7021aeb6e011e8d4d7f7e890c982895242442c4
m-ow <maxonomar@ciencias.unam.mx>
2025-07-29 14:34:07 -0600

    [IMO1963P4] add author

--- Compfiles/Imo1963P4.lean
+++ Compfiles/Imo1963P4.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 The Compfiles Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors:
+Authors: Maximiliano Onofre-Martínez
 -/
 
 import Mathlib.Tactic
