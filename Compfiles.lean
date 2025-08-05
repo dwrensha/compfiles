4ca84cd18b10baf9382593c14dafd9eb66690cef
Bolton Bailey <bolton.bailey@gmail.com>
2025-08-04 22:43:11 -0400

    ./scripts/mk_toplevel_file.bash

--- Compfiles.lean
+++ Compfiles.lean
@@ -113,6 +113,7 @@ import Compfiles.Imo2004P6
 import Compfiles.Imo2005P3
 import Compfiles.Imo2005P4
 import Compfiles.Imo2006P3
+import Compfiles.Imo2006P4
 import Compfiles.Imo2006P5
 import Compfiles.Imo2007P1
 import Compfiles.Imo2007P5
