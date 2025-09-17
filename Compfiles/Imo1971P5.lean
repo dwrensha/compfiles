0a6ecdd97d17d1e30a357c9a6f786d0ada66e7ea
David Renshaw <dwrenshaw@gmail.com>
2025-09-17 09:25:35 -0400

    [Imo1971P5] update informal statement to included needed Nonempty condition
    https://www.imo-register.org.uk/papers/1971-English-day2.jpeg
    https://leanprover.zulipchat.com/#narrow/channel/208328-IMO-grand-challenge/topic/Suggested.20formalization.20conventions/near/540006867

--- Compfiles/Imo1971P5.lean
+++ Compfiles/Imo1971P5.lean
@@ -14,8 +14,8 @@ problem_file { tags := [.Combinatorics] }
 /-!
 # International Mathematical Olympiad 1971, Problem 5
 
-Prove that for every natural number m there exists a finite set S of
-points in the plane with the following property:
+Prove that for every natural number m there exists a nonempty finite
+set S of points in the plane with the following property:
 For every point s in S, there are exactly m points which are at a unit
 distance from s.
 -/
