869bbdbbe29a99d51f1e11940f02a955d7648eef
David Renshaw <dwrenshaw@gmail.com>
2025-09-18 06:55:45 -0400

    [Imo1982P4] golf

--- Compfiles/Imo1982P4.lean
+++ Compfiles/Imo1982P4.lean
@@ -23,7 +23,7 @@ Show that the equation has no solutions in integers for n = 2891.
 
 namespace Imo1982P4
 
-set_option maxHeartbeats 0
+set_option maxHeartbeats 300000
 
 problem imo1982_p4 (n : ℕ)
   (hn : 0 < n)
@@ -34,7 +34,7 @@ problem imo1982_p4 (n : ℕ)
       x3^3 - 3 * x3 * y3^2 + y3^3 = n ∧
       (x1 ≠ x2 ∨ y1 ≠ y2) ∧
       (x1 ≠ x3 ∨ y1 ≠ y3) ∧
-      (x2 ≠ x3 ∨ y2 ≠ y3)) := by 
+      (x2 ≠ x3 ∨ y2 ≠ y3)) := by
   have h_part1 : n ≠ 2891 := by
     rintro rfl
     rcases hxy with ⟨x, y, hxy⟩
@@ -43,15 +43,11 @@ problem imo1982_p4 (n : ℕ)
       have h₃ := h₁
       have h₄ : (x : ℤ) ^ 3 - 3 * x * y ^ 2 + y ^ 3 ≡ 0 [ZMOD 7] := by
         norm_num [Int.ModEq] at h₃ ⊢
-        (try omega)
+        omega
       have h₅ : (x : ℤ) % 7 = 0 := by
-        have : (x : ℤ) % 7 = 0 ∨ (x : ℤ) % 7 = 1 ∨ (x : ℤ) % 7 = 2 ∨ (x : ℤ) % 7 = 3 ∨ (x : ℤ) % 7 = 4 ∨ (x : ℤ) % 7 = 5 ∨ (x : ℤ) % 7 = 6 := by omega
-        have : (y : ℤ) % 7 = 0 ∨ (y : ℤ) % 7 = 1 ∨ (y : ℤ) % 7 = 2 ∨ (y : ℤ) % 7 = 3 ∨ (y : ℤ) % 7 = 4 ∨ (y : ℤ) % 7 = 5 ∨ (y : ℤ) % 7 = 6 := by omega
-        rcases this with (h | h | h | h | h | h | h) <;>
-        rcases this with (h' | h' | h' | h' | h' | h' | h') <;>
-        (try {
-          simp [h, h', pow_three, pow_two, Int.ModEq, Int.mul_emod, Int.add_emod, Int.sub_emod] at h₄ ⊢
-        })
+        mod_cases h : x % 7 <;> change _ = _ at h <;>
+        mod_cases h' : y % 7 <;> change _ = _ at h' <;>
+        simp [h, h', pow_three, pow_two, Int.ModEq, Int.mul_emod, Int.add_emod, Int.sub_emod] at h₄ ⊢
       exact h₅
     have h₃ : (y : ℤ) % 7 = 0 := by
       have h₄ := h₁
@@ -59,13 +55,9 @@ problem imo1982_p4 (n : ℕ)
         norm_num [Int.ModEq] at h₄ ⊢
         (try omega)
       have h₆ : (y : ℤ) % 7 = 0 := by
-        have : (x : ℤ) % 7 = 0 ∨ (x : ℤ) % 7 = 1 ∨ (x : ℤ) % 7 = 2 ∨ (x : ℤ) % 7 = 3 ∨ (x : ℤ) % 7 = 4 ∨ (x : ℤ) % 7 = 5 ∨ (x : ℤ) % 7 = 6 := by omega
-        have : (y : ℤ) % 7 = 0 ∨ (y : ℤ) % 7 = 1 ∨ (y : ℤ) % 7 = 2 ∨ (y : ℤ) % 7 = 3 ∨ (y : ℤ) % 7 = 4 ∨ (y : ℤ) % 7 = 5 ∨ (y : ℤ) % 7 = 6 := by omega
-        rcases this with (h | h | h | h | h | h | h) <;>
-        rcases this with (h' | h' | h' | h' | h' | h' | h') <;>
-        (try {
-          simp [h, h', pow_three, pow_two, Int.ModEq, Int.mul_emod, Int.add_emod, Int.sub_emod] at h₅ ⊢
-        })
+        mod_cases h : x % 7 <;> change _ = _ at h <;>
+        mod_cases h' : y % 7 <;> change _ = _ at h' <;>
+        simp [h, h', pow_three, pow_two, Int.ModEq, Int.mul_emod, Int.add_emod, Int.sub_emod] at h₅ ⊢
       exact h₆
     have h₄ : (x : ℤ) % 7 = 0 := h₂
     have h₅ : (y : ℤ) % 7 = 0 := h₃
@@ -81,11 +73,9 @@ problem imo1982_p4 (n : ℕ)
     obtain ⟨b, hb⟩ := h₇
     rw [ha, hb] at h₁
     ring_nf at h₁
-    have h₈ : (7 : ℤ) * (a ^ 3 + b ^ 3 - 3 * a * b ^ 2) = 59 := by
-      omega
     have h₉ : (7 : ℤ) ∣ 59 := by
       use (a ^ 3 + b ^ 3 - 3 * a * b ^ 2)
-      linarith
+      omega
     norm_num at h₉
   have h_part2 : ∃ x1 x2 x3 y1 y2 y3 : ℤ, (x1^3 - 3 * x1 * y1^2 + y1^3 = n ∧ x2^3 - 3 * x2 * y2^2 + y2^3 = n ∧ x3^3 - 3 * x3 * y3^2 + y3^3 = n ∧ (x1 ≠ x2 ∨ y1 ≠ y2) ∧ (x1 ≠ x3 ∨ y1 ≠ y3) ∧ (x2 ≠ x3 ∨ y2 ≠ y3)) := by
     obtain ⟨x, y, hxy⟩ := hxy
@@ -98,10 +88,8 @@ problem imo1982_p4 (n : ℕ)
     (try ring_nf at * ; simp_all) <;>
     (try {
       by_contra! h
-      have h₃ : x = 0 := by
-        nlinarith [sq_nonneg (x + y), sq_nonneg (x - y)]
-      have h₄ : y = 0 := by
-        nlinarith [sq_nonneg (x + y), sq_nonneg (x - y)]
+      have h₃ : x = 0 := by grind
+      have h₄ : y = 0 := by grind
       simp [h₃, h₄] at hxy
       norm_cast at hxy
       omega
