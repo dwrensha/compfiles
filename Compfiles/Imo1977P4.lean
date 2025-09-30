522fc294836b08b68bf2a0752a472955347a9698
David Renshaw <dwrenshaw@gmail.com>
2025-09-30 13:54:50 -0400

    [Imo1977P4] formatting

--- Compfiles/Imo1977P4.lean
+++ Compfiles/Imo1977P4.lean
@@ -22,9 +22,9 @@ Prove that a^2 + b^2 ≤ 2 and A^2 + B^2 ≤ 1.
 namespace Imo1977P4
 
 problem imo1977_p4 (f : ℝ → ℝ) (a b A B : ℝ)
-  (h₀ : ∀ x, f x =
-             1 - a * Real.cos x - b * Real.sin x - A * Real.cos (2 * x) - B * Real.sin (2 * x))
-  (h₁ : ∀ x, f x ≥ 0) :
+    (h₀ : ∀ x, f x = 1 - a * Real.cos x - b * Real.sin x -
+                     A * Real.cos (2 * x) - B * Real.sin (2 * x))
+    (h₁ : ∀ x, f x ≥ 0) :
     a ^ 2 + b ^ 2 ≤ 2 ∧ A ^ 2 + B ^ 2 ≤ 1 := by
   have h₂ : A ^ 2 + B ^ 2 ≤ 1 := by
     by_contra! h
