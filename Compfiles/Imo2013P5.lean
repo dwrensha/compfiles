b202c89332bce7bb8bade78ede1f2e71ac41ef15
David Renshaw <dwrenshaw@gmail.com>
2025-10-11 10:54:07 -0400

    [Imo2013P5] pow_le_pow_right₀ -> one_le_pow₀

--- Compfiles/Imo2013P5.lean
+++ Compfiles/Imo2013P5.lean
@@ -46,7 +46,7 @@ lemma le_of_all_pow_lt_succ {x y : ℝ} (hy : 1 < y)
     exact pow_unbounded_of_one_lt _ h_base;
   have h_mul : x^n > 2 * y^n := by
     rwa [div_pow, gt_iff_lt, lt_div_iff₀ (by positivity)] at hn;
-  linarith only [h_mul, pow_le_pow_right₀ hy.le n.zero_le,
+  linarith only [h_mul, one_le_pow₀ (n := n) hy.le,
                  h n (Nat.pos_of_ne_zero (by rintro rfl; norm_num at hn))]
 
 /--
