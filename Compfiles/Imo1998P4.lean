2daa4c748b159b680ae9beb88b7d66539ff7d95a
Ben Pig Chu <benpichu@gmail.com>
2025-10-24 23:12:00 +0800

    Fix author

--- Compfiles/Imo1998P4.lean
+++ Compfiles/Imo1998P4.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 The Compfiles Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: InternLM-MATH LEAN Formalizer v0.1， Benpigchu
+Authors: InternLM-MATH LEAN Formalizer v0.1, Benpigchu
 -/
 
 import Mathlib.Tactic
