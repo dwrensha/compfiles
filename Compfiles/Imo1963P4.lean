a468e0349cea248c719ad62eaa67982b4b11ff84
David Renshaw <dwrenshaw@gmail.com>
2025-11-18 09:07:48 -0500

    [Imo1963P4] grind suffices

--- Compfiles/Imo1963P4.lean
+++ Compfiles/Imo1963P4.lean
@@ -44,63 +44,6 @@ problem imo1963_p4 (x₁ x₂ x₃ x₄ x₅ y : ℝ) :
      x₂ + x₄ = y * x₃ ∧
      x₃ + x₅ = y * x₄ ∧
      x₄ + x₁ = y * x₅) := by
-  constructor
-  · intro h
-    rcases h with h₀ | h₁
-    · obtain ⟨rfl, rfl, rfl, rfl, rfl⟩ := h₀
-      ring_nf; trivial
-    rcases h₁ with heq | h₂
-    · obtain ⟨rfl, rfl, rfl, rfl, rfl⟩ := heq
-      ring_nf; trivial
-    rcases h₂ with ⟨hy, hst⟩
-    rcases hst with ⟨s, ⟨t, h⟩⟩
-    obtain ⟨rfl, rfl, rfl, rfl, rfl⟩ := h
-    have h' : y^2 = 1 - y := by linarith
-    ring_nf
-    refine ⟨trivial, trivial, ?_, ?_, ?_⟩ <;> rw [h'] <;> ring
-
-  intro h
-  obtain ⟨h₁, h₂, h₃, h₄, h₅⟩ := h
-  by_cases hy : y ≠ 2
-  · have hx₃ : x₃ = y * x₂ - x₁ := by linarith
-    have hx₄ : x₄ = y * x₃ - x₂ := by linarith
-    have hx₅ : x₅ = y * x₁ - x₂ := by linarith
-    have hxy₁ : (y^2 + y - 1) * (x₂ - x₁) = 0 := by
-      rw [hx₃] at hx₄
-      rw [hx₄, hx₅] at h₅
-      linarith
-    have hx₄' : x₄ = (y^2 - 1) * x₂ - y * x₁ := by
-      have h' : y * (y * x₂ - x₁) - x₂ = (y^2 - 1) * x₂ - y * x₁ := by ring
-      rwa [hx₃, h'] at hx₄
-    have hxy₂ : (y^2 + y - 1) * ((y-1) * x₂ - x₁) = 0 := by
-      have : (y * x₂ - x₁) + (y * x₁ - x₂) = y * ((y^2 - 1) * x₂ - y * x₁) := by
-        rwa [<- hx₃, <- hx₅, <- hx₄']
-      linarith
-
-    by_cases hy' : y^2 + y - 1 ≠ 0
-    · have h0₁ : (x₂ - x₁ = 0) ∧ ((y - 1) * x₂ - x₁ = 0) := by
-        exact ⟨mul_eq_zero.mp hxy₁ |>.resolve_left hy',
-        mul_eq_zero.mp hxy₂ |>.resolve_left hy'⟩
-      have h0₂ : 0 = (y + -2) * x₂ := by linarith
-      rw [zero_eq_mul] at h0₂
-      rcases h0₂ with hy | hx₂
-      · rw [add_neg_eq_zero] at hy; contradiction
-      have hx₁ : x₁ = 0 := by linarith
-      simp [hx₁, hx₂] at hx₃
-      simp [hx₃, hx₂] at hx₄
-      simp [hx₁, hx₂] at hx₅
-      left; exact ⟨hx₁, hx₂, hx₃, hx₄, hx₅⟩
-
-    simp at hy'
-    have h' : y^2 = 1 - y := by linarith
-    simp [h'] at hx₄'
-    right; right
-    exact ⟨by assumption, by use x₁, x₂⟩
-
-  simp at hy
-  rw [hy] at h₁ h₂ h₃ h₄ h₅
-  have h₆ : x₅ + x₂ = x₃ + x₄ := by linarith
-  have h₇ : x₃ + x₄ + (x₅ + x₂) + x₁ = 5 * x₁ := by rw [<- h₆, h₁]; ring
-  right; left; refine ⟨?_, ?_, ?_, ?_, hy⟩ <;> linarith
+  grind
 
 end Imo1963P4
