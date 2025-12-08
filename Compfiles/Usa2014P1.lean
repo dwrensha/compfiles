66edda7bc92be33e766521f855ef3c4c5ec9df00
Evan Chen <evan@evanchen.cc>
2025-12-08 15:57:18 -0500

    feat(Usa2014P1): put in statement

---
+++ Compfiles/Usa2014P1.lean
@@ -0,0 +1,43 @@
+/-
+Copyright (c) 2024 The Compfiles Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Evan Chen
+-/
+
+import Mathlib.Tactic
+
+import ProblemExtraction
+
+problem_file { tags := [.Algebra] }
+
+/-!
+# USA Mathematical Olympiad 2014, Problem 1
+
+Let $a$, $b$, $c$, $d$ be real numbers such that $b-d \ge 5$ and all zeros
+$x_1$, $x_2$, $x_3$, and $x_4$ of the polynomial $P(x)=x^4+ax^3+bx^2+cx+d$ are real.
+Find the smallest value the product $(x_1^2+1)(x_2^2+1)(x_3^2+1)(x_4^2+1)$ can take.
+-/
+
+namespace Usa2014P1
+
+open Polynomial
+
+noncomputable def Objective (x : Fin 4 → ℝ) : ℝ := ∏ i, ((x i)^2 + 1)
+
+def Condition (x : Fin 4 → ℝ) : Prop :=
+  ∃ a b c d : ℝ, (b - d ≥ 5) ∧ (
+    (X - C (x 0)) * (X - C (x 1)) * (X - C (x 2)) * (X - C (x 3))
+    = X^4 + C a * X^3 + C b * X^2 + C c * X + C d)
+
+snip begin
+
+-- This follows the solution in
+-- https://web.evanchen.cc/exams/USAMO-2014-notes.pdf
+
+snip end
+
+noncomputable determine solution : ℝ := 16
+
+problem usa2014_p1 : IsLeast (Objective '' { x | Condition x } ) solution := by
+  sorry
+end Usa2014P1
