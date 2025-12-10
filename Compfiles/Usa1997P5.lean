952becd7744d581d26d620386ea099500c79c58d
Evan Chen <evan@evanchen.cc>
2025-12-10 13:23:48 -0800

    feat(Usa1997P5): solve the problem

--- Compfiles/Usa1997P5.lean
+++ Compfiles/Usa1997P5.lean
@@ -27,10 +27,25 @@ variable {a b c : ℝ}
 snip begin
 
 -- Follows the solution at https://web.evanchen.cc/exams/USAMO-1997-notes.pdf
+-- One just cyclically sums 1 / (a³ + b³ + abc) ≤ 1 / (ab(a+b) + abc)
+
+-- Bounding the individual fraction
+lemma bound (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) :
+    1 / (a^3 + b^3 + a*b*c) ≤ 1 / (a^2*b + a*b^2 + a*b*c) := by
+  field_simp -- clear denominators
+  have h : 0 ≤ (a-b)^2*(a+b) := by positivity
+  linarith
 
 snip end
 
 problem usa1997_p5 (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) :
     1 / (a^3 + b^3 + a*b*c) + 1 / (b^3 + c^3 + a*b*c) + 1 / (c^3 + a^3 + a*b*c) ≤ 1 / (a*b*c) := by
-  sorry
+  have h1 := bound ha hb hc
+  have h2 := bound hb hc ha
+  have h3 := bound hc ha hb
+  have hsum := add_le_add h1 (add_le_add h2 h3) -- sum the three bounds
+  convert hsum using 1
+  · ring -- LHS of hsum = LHS of desired
+  · field -- RHS of hsum = RHS of desired
+
 end Usa1997P5
