79b9cf966e9ad987f0f618727b42c59be1745440
John Rathgeber <john_rathgeber@brown.edu>
2025-12-17 18:04:59 -0500

    line length cleanup

--- Compfiles/Imo1998P3.lean
+++ Compfiles/Imo1998P3.lean
@@ -26,14 +26,16 @@ Determine all positive integers $k$ such that $d(n^2)/d(n) = k$ for some $n$.
 ## Solution:
 The answer to this problem is $k$ can be any positive odd integer. To formalize this,
 we must prove that if a $k$ satisfies $d(n^2)/d(n) = k$ for some $n$, then
-$k$ is odd, and also that if $k$ is positive odd integer, it satisfies $d(n^2)/d(n) = k$ for some $n$.
+$k$ is odd, and also that if $k$ is positive odd integer,
+it satisfies $d(n^2)/d(n) = k$ for some $n$.
 
 ## Proof:
 We follow Evan Chen's proof: <https://web.evanchen.cc/exams/IMO-1998-notes.pdf>.
 
 Throughout this proof, we will use the following facts from number theory:
 * $d(ab) = d(a)d(b)$ when $a$ and $b$ are coprime
-* $p_1^{k_1}$ is coprime to $p_2^{k_2}$ when both $p_1$ and $p_2$ are prime, $p_1 \neq p_2$, and $k_1, k_2 \geq 0$.
+* $p_1^{k_1}$ is coprime to $p_2^{k_2}$ when both $p_1$ and $p_2$ are prime,
+  $p_1 \neq p_2$, and $k_1, k_2 \geq 0$.
 * $d(p^k) = k + 1$ for prime $p$ and $k \geq 1$
 
 ### Necessity: $(\exists n > 0, d(n^2)/d(n) = k) → k \equiv 1 \pmod{2}$.
@@ -44,9 +46,11 @@ $$n = \prod_{i=1}^tp_i^{k_i}$$
 
 Then, using the above number theory facts, we can write
 
-$$d(n) = d\left(\prod_{i=1}^tp_i^{k_i}\right) = \prod_{i=1}^td\left(p_i^{k_i}\right) = \prod_{i=1}^t(k_i + 1)$$
+$$d(n) = d\left(\prod_{i=1}^tp_i^{k_i}\right) =
+\prod_{i=1}^td\left(p_i^{k_i}\right) = \prod_{i=1}^t(k_i + 1)$$
 
-$$d(n^2) = d\left(\prod_{i=1}^tp_i^{2k_i}\right) = \prod_{i=1}^td\left(p_i^{2k_i}\right) = \prod_{i=1}^t(2k_i + 1)$$
+$$d(n^2) = d\left(\prod_{i=1}^tp_i^{2k_i}\right) =
+\prod_{i=1}^td\left(p_i^{2k_i}\right) = \prod_{i=1}^t(2k_i + 1)$$
 
 so
 
@@ -54,13 +58,17 @@ $$k = \frac{d(n^2)}{d(n)} = \prod_{i=1}^t\frac{2k_i + 1}{k_i + 1}$$
 
 Since the numerator is always odd, $k$ is always odd. This concludes the proof of necessity.
 
-### Sufficiency: For positive $k$, $k \equiv 1 \pmod{2} \rightarrow (\exists n > 0, d(n^2)/d(n) = k)$.
+### Sufficiency: For positive $k$, $k \equiv 1 \pmod{2} \to (\exists n > 0, d(n^2)/d(n) = k)$.
 
 We prove this by strong induction.
 
 Base Case: $k = 1$. $n = 1$ works.
 
-Inductive Step: Assume $k > 1$, and $\forall j < k, j \equiv 1 \pmod{2} \to \exists n > 0, d(n^2)/d(n) = j$. Let $k = 2^tj - 1$ be odd, where $j$ is also odd. We can write $k$ in this way because every even number has both an even and an odd part, and since $k + 1$ is even, we can let $2^t$ be its even part and $j$ be its odd part. In particular, $t$ will be the power $2$ is raised to in $k+1$'s prime factorization.
+Inductive Step: Assume $k > 1$, and $\forall j < k, j
+\equiv 1 \pmod{2} \to \exists n > 0, d(n^2)/d(n) = j$. Let $k = 2^tj - 1$ be odd, where $j$ is
+also odd. We can write $k$ in this way because every even number has both an even and an odd part,
+and since $k + 1$ is even, we can let $2^t$ be its even part and $j$ be its odd part.
+In particular, $t$ will be the power $2$ is raised to in $k+1$'s prime factorization.
 
 We get $j < k$ immediately, so by our inductive hypothesis,
 
@@ -70,11 +78,18 @@ We now aim to construct a number $x$ such that $d((n_jx)^2)/d(n_jx) = k$. Let
 
 $$x = \prod_{i=0}^{t-1} p_i^{2^{t + i}j - 2^i(j + 1)}$$
 
-where each $p_i$ is prime, distinct, and does not appear as a prime in $n_j$'s prime factorization. This is justified by the infinitude of primes. We also have that $d(p_i^{2^{t + i}j - 2^i(j + 1)}) = (2^{t + i}j - 2^i(j + 1)) + 1$ for all $i$, as $2^{t + i}j - 2^i(j + 1) \geq 1$ for all $i$. We can now compute
+where each $p_i$ is prime, distinct, and does not appear as a prime in $n_j$'s prime factorization.
+This is justified by the infinitude of primes. We also have that $d(p_i^{2^{t + i}j - 2^i(j + 1)})
+= (2^{t + i}j - 2^i(j + 1)) + 1$ for all $i$, as $2^{t + i}j - 2^i(j + 1) \geq 1$ for all $i$.
+We can now compute
 
-$$\frac{d(x^2)}{d(x)} = \prod_{i=0}^{t-1}\frac{2(2^{t + i}j - 2^i(j + 1)) + 1}{(2^{t + i}j - 2^i(j + 1)) + 1} = \prod_{i=0}^{t-1}\frac{2^{t + i + 1}j - 2^{i + 1}(j + 1) + 1}{2^{t+i}j - 2^i(j + 1) + 1}$$
+$$\frac{d(x^2)}{d(x)} = \prod_{i=0}^{t-1}
+\frac{2(2^{t + i}j - 2^i(j + 1)) + 1}{(2^{t + i}j - 2^i(j + 1)) + 1} =
+\prod_{i=0}^{t-1}\frac{2^{t + i + 1}j - 2^{i + 1}(j + 1) + 1}{2^{t+i}j - 2^i(j + 1) + 1}$$
 
-$$ = \frac{2^{2t}j-2^t(j+1)+1}{2^{2t-1}j-2^{t-1}(j+1)+1}\cdot \frac{2^{2t-1}j-2^{t-1}(j+1)+1}{2^{2t-2}j-2^{t-2}(j+1)+1}\cdot\cdots\cdot\frac{2^{t+1}j-2(j+1)+1}{2^tj-(j+1)+1}$$
+$$ = \frac{2^{2t}j-2^t(j+1)+1}{2^{2t-1}j-2^{t-1}(j+1)+1}\cdot
+\frac{2^{2t-1}j-2^{t-1}(j+1)+1}{2^{2t-2}j-2^{t-2}(j+1)+1}\cdot\cdots\cdot
+\frac{2^{t+1}j-2(j+1)+1}{2^tj-(j+1)+1}$$
 
 Which telescopes cleanly to
 
@@ -82,9 +97,12 @@ $$= \frac{2^{2t}j-2^t(j+1)+1}{2^tj-(j+1)+1} = \frac{(2^tj-1)(2^t-1)}{j(2^t-1)} =
 
 We can multiply both sides by $j$ to achieve
 
-$$k = 2^tj - 1 = j \cdot \frac{d(x^2)}{d(x)} = \frac{d(n_j^2)d(x^2)}{d(n_j)d(x)} = \frac{d((n_jx)^2)}{d(n_jx)}$$
+$$k = 2^tj - 1 = j \cdot \frac{d(x^2)}{d(x)} = \frac{d(n_j^2)d(x^2)}{d(n_j)d(x)} =
+ \frac{d((n_jx)^2)}{d(n_jx)}$$
 
-where the last equality holds because each prime in $x$'s factorization does not appear in $n_j$'s factorization, so $x$ and $n_j$ are coprime, and similarly for $x^2$ and $n_j^2$. This concludes the proof of sufficiency.
+where the last equality holds because each prime in $x$'s factorization does not appear in $n_j$'s
+factorization, so $x$ and $n_j$ are coprime, and similarly for $x^2$ and $n_j^2$.
+This concludes the proof of sufficiency.
 
 Since we have proven both necessity and sufficiency, we know for any positive integer $k$,
 
