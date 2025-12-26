1f313ded8f2246c81c29da511fb81de50a670ae1
Hongyu Ouyang <96765450+casavaca@users.noreply.github.com>
2025-12-26 15:28:04 -0800

    Add proof for Usa2001P1 (#138)
    * first attempt: Compfiles/Usa2001P1.lean
    
    * style(Usa2001P1): fix multi-goal warnings
    
    * manually clean up
    
    * manually clean up
    
    * manually clean up
    
    * manually clean up
    
    * simp -> simp only
    
    * simp
    
    * simp
    
    * m
    
    * Fintype.elems --> Finset.univ
    
    * simplify
    
    * simp
    
    * simp
    
    * simp
    
    * simp
    
    * simp
    
    * simp
    
    * manually clean up
    
    * Update Compfiles/Usa2001P1.lean
    
    * Update Compfiles/Usa2001P1.lean
    
    ---------
    
    Co-authored-by: David Renshaw <dwrenshaw@gmail.com>

--- Compfiles/Usa2001P1.lean
+++ Compfiles/Usa2001P1.lean
@@ -28,11 +28,43 @@ def possible_num_colors : Set ℕ :=
 { n : ℕ | ∃ f : Fin 8 → Finset (Fin n),
     (∀ i, (f i).card = 6) ∧
     (∀ x y : Fin n, ∀ i j : Fin 8,
-      i ≠ j → x ∈ f i → y ∈ f i →
+      i ≠ j → x ∈ f i → y ∈ f i → x ≠ y →
         (¬ (x ∈ f j ∧ y ∈ f j))) }
 
 determine min_colors : ℕ := 23
 
+snip begin
+
+/--
+  if
+    a₁, ... a₆ is positive, ∑ (i=1 to 6), aᵢ ≤ 13
+  then
+    ∑ (i=1 to 6) (1 / aᵢ) ≥ 36/13
+-/
+lemma usa2001_p1_lemma {α} (s : Finset α) (sz : s.card = 6) (gen : α -> ℕ) (gt : ∀ i ∈ s, gen i > 0) (sum : (∑ i ∈ s, gen i) ≤ 13) : (36:ℝ)/(13:ℝ) ≤ (∑ i ∈ s, 1 / (gen i:ℝ)) := by
+  let f := λ (i : α) ↦ Real.sqrt (gen i : ℝ)
+  let g := λ (i : α) ↦ (1 : ℝ) / Real.sqrt (gen i : ℝ)
+  have h := Finset.sum_mul_sq_le_sq_mul_sq s f g
+  unfold f g at h
+  have : (∑ x ∈ s, √(gen x : ℝ) * (1 / √(gen x : ℝ))) = ∑ x ∈ s, 1 := by
+    apply Finset.sum_congr rfl; intro x hx; have := gt x hx; field_simp
+  rw [this] at h; simp only [Finset.sum_const, sz, nsmul_eq_mul, Nat.cast_ofNat, mul_one, Nat.cast_nonneg, Real.sq_sqrt,
+    one_div, inv_pow] at h;
+  have : (∑ x ∈ s, (gen x:ℝ)⁻¹) = (∑ x ∈ s, 1 / (gen x : ℝ)) := by
+    apply Finset.sum_congr rfl; intro x hx; have := gt x hx; field_simp
+  rw [this] at h;
+  set aa := (∑ x ∈ s, (gen x : ℝ)) with haa
+  set bb := (∑ x ∈ s, (1 / (gen x : ℝ))) with hbb
+  rify at sum; rw [←haa] at sum; norm_num at h; 
+  have : aa ≥ 0 := by rw [haa]; apply Finset.sum_nonneg; intro i hi; simp only [Nat.cast_nonneg]
+  have : bb ≥ 0 := by rw [hbb]; apply Finset.sum_nonneg; intro i hi; simp only [one_div, inv_nonneg, Nat.cast_nonneg]
+  field_simp; trans aa*bb;
+  · exact h
+  · nlinarith
+
+snip end
+
+set_option maxHeartbeats 1234567 in
 problem usa2001_p1 : IsLeast possible_num_colors min_colors := by
   -- Informal solution from
   -- https://artofproblemsolving.com/wiki/index.php/2001_USAMO_Problems/Problem_1
@@ -50,10 +82,50 @@ problem usa2001_p1 : IsLeast possible_num_colors min_colors := by
     use f
     constructor
     · intro i
-      fin_cases i <;> simp (config := {decide := true}) [f]
-    · intro x y i j hij hx hy
-      sorry
-  · sorry
-
-
+      fin_cases i <;> simp (config := { decide := true }) only [Fin.isValue, Fin.zero_eta, Finset.mem_insert, not_false_eq_true,
+    Finset.card_insert_of_notMem, Finset.mem_singleton, Finset.card_singleton, Nat.reduceAdd, f]
+    · intro x y i j hij hx hy hxy
+      unfold min_colors at x y
+      fin_cases i <;> fin_cases j <;> dsimp [f] at hx <;> dsimp [f] at hy <;> dsimp at hij <;> dsimp [f]
+      all_goals clear f ; try contradiction
+      all_goals fin_cases hx <;> fin_cases hy
+      all_goals (first | decide | contradiction)
+  · rw [min_colors, mem_lowerBounds]
+    by_contra! ⟨n, ⟨f, ⟨h1, h2⟩⟩, ha_lt⟩
+    suffices (22 : ℝ) < n by norm_cast at this; omega
+    clear ha_lt
+    let count color i := if color ∈ f i then 1 else 0
+    let count_k : Fin n → ℕ := λ color ↦ ∑ i : Fin 8, count color i
+    have : (∑ (k : Fin n), ∑ i : Fin 8, ((count k i):ℝ) / ((count_k k):ℝ)) ≤ n := by
+      have nsum : n = ∑ (k : Fin n), 1 := by simp only [Finset.sum_const, Finset.card_univ, Fintype.card_fin, smul_eq_mul, mul_one]
+      conv => rhs; rw [nsum]
+      push_cast
+      gcongr with i a; unfold count_k; rw [← Finset.sum_div]; norm_cast
+      by_cases h : (∑ x, count i x) = 0
+      · rw [h]; simp only [CharP.cast_eq_zero, div_zero, zero_le_one]
+      · apply le_of_eq; rw [div_self]; norm_cast
+    refine lt_of_lt_of_le ?_ this; clear this
+    rw [Finset.sum_comm]
+    apply lt_of_lt_of_le (by linarith : (22 < ∑ (i : Fin 8), ((36:ℝ) / (13:ℝ))))
+    gcongr with i a
+    rw [← Finset.sum_subset (by simp : f i ⊆ Finset.univ) (by intros; simp [count, *])]
+    have : ∀ k ∈ f i, (count k i) = 1 := by intro k hk; simp [count, hk]
+    rw [Finset.sum_congr (g := λ k ↦ 1 / ((count_k k):ℝ)) rfl (λ k hk ↦ by congr; norm_cast; exact this k hk)]
+    refine usa2001_p1_lemma _ (h1 i) count_k ?_ ?_
+    · intro ii hii
+      apply Finset.sum_pos'
+      · intros; simp [count]
+      · use i; simp [count, hii]
+    have : (∑ x ∈ f i, count x i) = 6 := by simp [count, *]
+    rw [Finset.sum_comm, ← Finset.sum_erase_add (h := a), this]
+    simp only [Nat.reduceLeDiff, ge_iff_le]
+    have : (∑ x ∈ Finset.univ.erase i, 1) = 7 := by simp
+    rw [← this]
+    gcongr with j a
+    rw [Finset.mem_erase] at a;
+    by_contra!
+    rw [← Finset.sum_filter, Finset.sum_const, smul_eq_mul, mul_one, Finset.one_lt_card] at this
+    simp only [Finset.mem_filter] at this
+    obtain ⟨ex, ⟨⟨p1, p4⟩, ⟨ey, ⟨⟨p2, p5⟩, p3⟩⟩⟩⟩ := this
+    refine h2 ex ey i j a.1.symm p1 p2 p3 ⟨p4,p5⟩
 end Usa2001P1
