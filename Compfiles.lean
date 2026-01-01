1b561beaf2215f79cd1c618faa0bb42fb436656c
Sese Mueller <sese4dasbinich@gmail.com>
2026-01-01 16:58:19 +0100

    Adds 1992P5 into Compfiles.lean

--- Compfiles.lean
+++ Compfiles.lean
@@ -93,6 +93,7 @@ import Compfiles.Imo1991P5
 import Compfiles.Imo1991P6
 import Compfiles.Imo1992P1
 import Compfiles.Imo1992P2
+import Compfiles.IMO1992P5
 import Compfiles.Imo1993P5
 import Compfiles.Imo1994P1
 import Compfiles.Imo1994P4
