81031582a4ded7bbf52c62443673407bb034d64e
lean-tom <252114661+lean-tom@users.noreply.github.com>
2026-01-02 09:05:35 +0900

    feat : add IMO 1971 P6 (#143)
    * feat: add IMO 1971 P6
    
    * style: update Compfiles.lean to include Imo1971P6
    
    * refactor: move snip end after aux lemma

--- Compfiles.lean
+++ Compfiles.lean
@@ -43,6 +43,7 @@ import Compfiles.Imo1970P4
 import Compfiles.Imo1970P6
 import Compfiles.Imo1971P3
 import Compfiles.Imo1971P5
+import Compfiles.Imo1971P6
 import Compfiles.Imo1972P1
 import Compfiles.Imo1972P3
 import Compfiles.Imo1972P4
