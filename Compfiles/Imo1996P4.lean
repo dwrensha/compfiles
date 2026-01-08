cd2af993bc3567732fa871a2700e48a1af5436e6
markusrydh <markus.rydh@gmail.com>
2026-01-08 23:46:28 +0100

    Imo1996p4 - add credit (#150)

--- Compfiles/Imo1996P4.lean
+++ Compfiles/Imo1996P4.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 The Compfiles Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors:
+Authors: Markus Rydh
 -/
 
 import Mathlib.Tactic
