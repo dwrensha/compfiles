afe5c058bffd1bf24ec7eeee410e9649d4bd3247
David Renshaw <dwrenshaw@gmail.com>
2026-01-21 07:52:12 -0500

    fix ordering in Compfiles.lean

--- Compfiles.lean
+++ Compfiles.lean
@@ -95,9 +95,9 @@ import Compfiles.Imo1991P5
 import Compfiles.Imo1991P6
 import Compfiles.Imo1992P1
 import Compfiles.Imo1992P2
+import Compfiles.Imo1992P5
 import Compfiles.Imo1992P6
 import Compfiles.Imo1993P1
-import Compfiles.Imo1992P5
 import Compfiles.Imo1993P5
 import Compfiles.Imo1994P1
 import Compfiles.Imo1994P4
