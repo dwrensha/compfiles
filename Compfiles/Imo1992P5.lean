9fdead86ddc3f623eab0411a2ee511f6270ca346
David Renshaw <dwrenshaw@gmail.com>
2026-01-21 08:31:13 -0500

    [Imo1992P5] clean up imports

--- Compfiles/Imo1992P5.lean
+++ Compfiles/Imo1992P5.lean
@@ -4,9 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sebastian Willmann (with assistance from Github Copilot and Aristotle)
 -/
 
-import Mathlib.Data.Real.Sqrt
-
-import Lean
+import Mathlib
 
 import ProblemExtraction
 
