6c5187c1ab43930b0e7713a985d4aa75a726699e
markusrydh <markus.rydh@gmail.com>
2026-02-09 22:16:53 +0100

    [Imo1994p6] Formalization and solution (#164)
    * [Imo1994p6] Formalization and solution
    
    * [Imo1994P6] Fixed formalization
    
    Now including positive integers condition.

--- Compfiles.lean
+++ Compfiles.lean
@@ -106,6 +106,7 @@ import Compfiles.Imo1993P5
 import Compfiles.Imo1994P1
 import Compfiles.Imo1994P4
 import Compfiles.Imo1994P5
+import Compfiles.Imo1994P6
 import Compfiles.Imo1995P2
 import Compfiles.Imo1995P4
 import Compfiles.Imo1996P3
