e8fbbe0d3698deb2c7557cb952717db3bfe1b57d
David Renshaw <dwrenshaw@gmail.com>
2026-02-15 12:04:57 -0500

    [Imo2025P2] formatting

--- Compfiles/Imo2025P2.lean
+++ Compfiles/Imo2025P2.lean
@@ -35,17 +35,22 @@ namespace Imo2025P2
 variable {V Pt : Type*} [NormedAddCommGroup V] [InnerProductSpace ℝ V] [MetricSpace Pt]
 variable [NormedAddTorsor V Pt] [Fact (finrank ℝ V = 2)]
 
-problem imo2025_p2 {M N A B C D P E F H : Pt} {Ω Γ : Sphere Pt} (Ω_center_eq_M : Ω.center = M)
-    (Γ_center_eq_N : Γ.center = N) (Ω_radius_lt_Γ_radius : Ω.radius < Γ.radius)
-    (A_mem_inter : A ∈ (Ω ∩ Γ : Set Pt)) (B_mem_inter : B ∈ (Ω ∩ Γ : Set Pt)) (A_ne_B : A ≠ B)
-    (M_ne_N : M ≠ N) (C_mem_inter : C ∈ (line[ℝ, M, N] ∩ Ω : Set Pt))
-    (D_mem_inter : D ∈ (line[ℝ, M, N] ∩ Γ : Set Pt)) (sbtw_C_M_N_D : [C, M, N, D].Sbtw ℝ)
+problem imo2025_p2 {M N A B C D P E F H : Pt} {Ω Γ : Sphere Pt}
+    (Ω_center_eq_M : Ω.center = M) (Γ_center_eq_N : Γ.center = N)
+    (Ω_radius_lt_Γ_radius : Ω.radius < Γ.radius)
+    (A_mem_inter : A ∈ (Ω ∩ Γ : Set Pt)) (B_mem_inter : B ∈ (Ω ∩ Γ : Set Pt))
+    (A_ne_B : A ≠ B) (M_ne_N : M ≠ N)
+    (C_mem_inter : C ∈ (line[ℝ, M, N] ∩ Ω : Set Pt))
+    (D_mem_inter : D ∈ (line[ℝ, M, N] ∩ Γ : Set Pt))
+    (sbtw_C_M_N_D : [C, M, N, D].Sbtw ℝ)
     (affineIndependent_ACD : AffineIndependent ℝ ![A, C, D])
-    (P_eq_circumcenter : P = (⟨_, affineIndependent_ACD⟩ : Triangle ℝ Pt).circumcenter)
+    (P_eq_circumcenter :
+      P = (⟨_, affineIndependent_ACD⟩ : Triangle ℝ Pt).circumcenter)
     (E_mem_inter : E ∈ (line[ℝ, A, P] ∩ Ω : Set Pt)) (E_ne_A : E ≠ A)
     (F_mem_inter : F ∈ (line[ℝ, A, P] ∩ Γ : Set Pt)) (F_ne_A : F ≠ A)
     (affineIndependent_PMN : AffineIndependent ℝ ![P, M, N])
-    (H_eq_orthocenter : H = Triangle.orthocenter (⟨_, affineIndependent_PMN⟩ : Triangle ℝ Pt)) :
+    (H_eq_orthocenter :
+      H = Triangle.orthocenter (⟨_, affineIndependent_PMN⟩ : Triangle ℝ Pt)) :
     ∃ affineIndependent_BEF : AffineIndependent ℝ ![B, E, F],
       (⟨_, affineIndependent_BEF⟩ : Triangle ℝ Pt).circumsphere.IsTangent
         (AffineSubspace.mk' H line[ℝ, A, P].direction) := by
