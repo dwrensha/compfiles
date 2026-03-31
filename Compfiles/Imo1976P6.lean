f3c19ba7adf95d11547dd308c6b1d1bd2d0fe099
David Renshaw <dwrenshaw@gmail.com>
2026-03-31 09:07:16 -0400

    [Imo1976P6] simplify

--- Compfiles/Imo1976P6.lean
+++ Compfiles/Imo1976P6.lean
@@ -26,19 +26,18 @@ problem imo1976_p6 (u : ℕ → ℝ)
     (h₁ : u 1 = 5 / 2)
     (h₂ : ∀ n, u (n + 2) = u (n + 1) * ((u n) ^ 2 - 2) - u 1) :
       ∀ n > 0, ⌊u n⌋  = (2 : ℝ) ^ ((2^n - (-1 : ℝ) ^ n) / 3) := by
-  set_option backward.isDefEq.respectTransparency false in
   have h_ind : ∀ n, u n = 2 ^ ((2 ^ n - (-1) ^ n) / 3 : ℝ) +
                           2 ^ ((-2 ^ n + (-1) ^ n) / 3 : ℝ) := by
     intro n
-    induction' n using Nat.strong_induction_on with n ih;
+    induction' n using Nat.strong_induction_on with n ih
     rcases n with ( _ | _ | n ) <;> norm_num
     · exact h₀
     · exact h₁
     simp_all only [lt_add_iff_pos_left, add_pos_iff, Nat.ofNat_pos, or_true, pow_zero,
                    sub_self, zero_div, Real.rpow_zero, neg_add_cancel,
                    zero_lt_one, pow_one, sub_neg_eq_add, lt_add_iff_pos_right]
-    ring_nf at * ; norm_num at *;
-    rw [ih n (by lia)] ; ring_nf; norm_num [ sq, ← Real.rpow_add ] ; ring_nf;
+    ring_nf at * ; norm_num at *
+    rw [ih n (by lia)] ; ring_nf; norm_num [ sq, ← Real.rpow_add ] ; ring_nf
     obtain h | h := Nat.even_or_odd n <;> norm_num [h.neg_one_pow] <;> ring_nf
   intro n hn_pos
   have h_int : ∃ k : ℤ, (2 : ℝ) ^ ((2 ^ n - (-1) ^ n) / 3 : ℝ) = k := by
@@ -48,23 +47,16 @@ problem imo1976_p6 (u : ℕ → ℝ)
                    pow_one, sub_neg_eq_add, gt_iff_lt, Int.reduceNeg]
     obtain ⟨w, h⟩ := h_int
     use 2 ^ w.natAbs
-    obtain a | a := w <;> norm_num [ Real.rpow_def_of_pos ] at *
-    · simp_all only [Int.reduceNeg]
-      norm_num [ ← h, Real.exp_mul, Real.exp_log ];
-      norm_num [ show ( 2 ^ n - ( -1 ) ^ n : ℝ ) = 3 * a by exact_mod_cast h ];
-    · obtain ⟨w, h_1⟩ := Nat.even_or_odd' n
+    obtain a | a := w
+    · norm_num1 at *
       simp_all only [Int.reduceNeg]
-      cases h_1 with
-      | inl h_2 =>
-        subst h_2
-        simp_all only [Nat.ofNat_pos, mul_pos_iff_of_pos_left, Int.reduceNeg, even_two,
-                       Even.mul_right, Even.neg_pow, one_pow]
+      norm_num [ ← h, Real.exp_mul, Real.exp_log ]
+      norm_num [ show ( 2 ^ n - ( -1 ) ^ n : ℝ ) = 3 * a by exact_mod_cast h ]
+    · obtain ⟨w, rfl | rfl⟩ := Nat.even_or_odd' n
+      · simp only [even_two, Even.mul_right, Even.neg_pow, one_pow] at h
         lia
-      | inr h_3 =>
-        subst h_3
-        exfalso; clear h₂ h_ind hn_pos
-        rw [Int.negSucc_eq, Odd.neg_one_pow ⟨w, rfl⟩] at h
-        simp only [Int.reduceNeg, sub_neg_eq_add] at h
+      · exfalso; clear* -h
+        rw [Odd.neg_one_pow ⟨w, rfl⟩] at h
         lia
   simp_all only [pow_zero, sub_self, zero_div, Real.rpow_zero, neg_add_cancel,
                  pow_one, sub_neg_eq_add, gt_iff_lt]
@@ -75,7 +67,7 @@ problem imo1976_p6 (u : ℕ → ℝ)
   · positivity
   · rw [ Real.rpow_lt_one_iff ] <;> norm_num
     rcases Nat.even_or_odd' n with ⟨ k, rfl | rfl ⟩ <;> norm_num [ pow_add, pow_mul ] at *;
-    · linarith [ pow_le_pow_right₀ ( by norm_num : ( 1 : ℝ ) ≤ 4 ) hn_pos ];
+    · linarith [ pow_le_pow_right₀ ( by norm_num : ( 1 : ℝ ) ≤ 4 ) hn_pos ]
     · linarith [ pow_pos ( by norm_num : ( 0 : ℝ ) < 4 ) k ]
 
 end Imo1976P6
