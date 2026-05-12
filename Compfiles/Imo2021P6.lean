b8109334e9a0ab10901ea3d60f70ad8c97504904
markusrydh <markus.rydh@gmail.com>
2026-05-13 00:29:59 +0200

    Changed imports

--- Compfiles/Imo2021P6.lean
+++ Compfiles/Imo2021P6.lean
@@ -4,8 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Renshaw, Markus Rydh
 -/
 
-import Mathlib.Tactic
-import Mathlib.NumberTheory.SiegelsLemma
+import Mathlib
 
 import ProblemExtraction
 
