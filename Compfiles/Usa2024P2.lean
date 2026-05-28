e9ea8bc0c8ffe294b482b1184cd9d1cc77738886
Karl Mehltretter <kmehltretter@gmail.com>
2026-05-28 07:59:11 +0200

    [Usa2024P2] fix statement

--- Compfiles/Usa2024P2.lean
+++ Compfiles/Usa2024P2.lean
@@ -14,8 +14,8 @@ problem_file { tags := [.Combinatorics] }
 /-!
 # USA Mathematical Olympiad 2024, Problem 2
 
-Let S₁, S₂, ..., Sₙ be finite sets of integers whose intersection
-is not empty. For each non-empty T ⊆ {S₁, S₂, ..., Sₙ}, the size of
+Let S₁, S₂, ..., S₁₀₀ be finite sets of integers whose intersection
+is not empty. For each non-empty T ⊆ {S₁, S₂, ..., S₁₀₀}, the size of
 the intersection of the sets in T is a multiple of the number of
 sets in T. What is the least possible number of elements that are in
 at least 50 sets?
@@ -29,16 +29,16 @@ structure Good (S : Fin 100 → Set ℤ) : Prop where
   finite : ∀ i, (S i).Finite
   nonempty_inter : ⋂ i, S i ≠ ∅
   card : ∀ T : Finset (Fin 100), T.Nonempty →
-                 ∃ k : ℕ, (⋂ i ∈ T, S i).ncard * k = T.card
+                 ∃ k : ℕ, (⋂ i ∈ T, S i).ncard = k * T.card
 
 -- z is in at least k of the sets S.
 abbrev InAtLeastKSubsets (S : Fin 100 → Set ℤ) (k : ℕ) (z : ℤ) : Prop :=
   k ≤ {i : Fin 100 | z ∈ S i }.ncard
 
-problem usa2024_p2 (n : ℕ) :
+problem usa2024_p2 :
     IsLeast
       { k | ∃ S, Good S ∧
-             k = {z : ℤ | InAtLeastKSubsets S k z }.ncard } solution :=
+             k = {z : ℤ | InAtLeastKSubsets S 50 z }.ncard } solution :=
   by sorry
 
 
