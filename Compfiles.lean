1d61f53248bc1adb4b4ce1477c695a4d4e72d025
Daniel Liao <omsidaniel@gmail.com>
2026-06-26 04:28:08 -0700

    add 2 usamo problems (#217)
    * added 3 usamo problems
    
    * small fix
    
    * forgot to update compfiles.lean
    
    * forgot one
    
    * fix uppercase name
    
    * fix uppercase name

--- Compfiles.lean
+++ Compfiles.lean
@@ -315,5 +315,7 @@ import Compfiles.Usa2023P2
 import Compfiles.Usa2023P4
 import Compfiles.Usa2023P5
 import Compfiles.Usa2024P2
+import Compfiles.Usa2025P4
 import Compfiles.Usa2026P1
+import Compfiles.Usa2026P4
 import Compfiles.ZeroesOnesAndTwos
