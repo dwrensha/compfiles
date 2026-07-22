da869a16117f169c14fa6c655fca3c051367fdd3
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2026-07-22 12:08:34 +0800

    complete sorry-less proof

--- Compfiles/Usa1980P3.lean
+++ Compfiles/Usa1980P3.lean
@@ -39,22 +39,60 @@ def f (x y z A B C : ℝ) : Fin 3 → ℂ
 
 end
 
+variable {x y z A B C : ℝ}
+
+lemma movire (n : ℕ) (z : ℂ) : Complex.exp (z * Complex.I) ^ n = Complex.exp (↑n * z * Complex.I) := by
+  simpa [Complex.cos_add_sin_I] using Complex.cos_add_sin_mul_I_pow n z
+
 lemma P_def (k : ℕ) : P k = X (0) ^ k + X (1) ^ k + X (2) ^ k := by
   simp [P, psum, Fin.sum_univ_three]
 
-lemma S_zero : S 0 = C 1 := by simp [S]
+open Real in
+lemma sum_f_im (habc: ∃ k : ℤ, A + B + C = k * π) : (∏ i, f x y z A B C i).im = 0 := by
+  obtain ⟨k, h⟩ := habc
+  refine Complex.sq_nonneg_iff.mp ?_
+  simp only [f, Fin.prod_univ_three]
+  -- need to do this awkward maneuver instead of using `suffices` to avoid invoking LE ℂ
+  have :
+    ↑x * Complex.exp (↑A * Complex.I) * (↑y * Complex.exp (↑B * Complex.I)) * (↑z * Complex.exp (↑C * Complex.I))
+      = ↑x * ↑y * ↑z * (Complex.exp (↑A * Complex.I) * Complex.exp (↑B * Complex.I) * Complex.exp (↑C * Complex.I)) := by field_simp
+  rw [this, mul_pow]
+  norm_cast
+  simp_rw [← Complex.exp_add, ← Complex.exp_nat_mul]
+  rw [Complex.exp_eq_one_iff.mpr ⟨k, ?_⟩]
+  · norm_cast; simpa using sq_nonneg _
+  · simp_rw [← add_mul, ← Complex.ofReal_add, h]
+    push_cast
+    field_simp
+
+lemma S_zero : S 0 = MvPolynomial.C 1 := by simp [S]
 
 lemma S_one : S 1 = X (0) + X (1) + X (2) := by simp [S, Fin.sum_univ_three]
 
 lemma S_two : S 2 = X (0) * X (1) + X (1) * X (2) + X (0) * X (2) := by
-  simp [S, esymm]
-  sorry
+  have : Finset.powersetCard 2 (Finset.univ: Finset (Fin 3)) = {{0, 1}, {1, 2}, {0, 2}} :=
+    Finset.symmDiff_eq_empty.mp rfl
+  simp [S, esymm, this]
+  rw [Finset.sum_insert (by decide), Finset.sum_insert (by decide), Finset.sum_singleton]
+  simp [add_assoc]
 
+example : (2: MvPolynomial (Fin 3) ℂ) = MvPolynomial.C 2 := by
+  rfl
+
+lemma S_two' : (2: MvPolynomial (Fin 3) ℂ) * S 2 = (S 1 * P 1 - P 2) := by
+  have : Finset.filter (·.1 < 2) (Finset.antidiagonal 2) = {(0,2),(1,1)} := by
+    rw [Finset.Nat.antidiagonal_eq_image]
+    simp [Finset.range, Finset.filter_insert, Finset.image, Finset.filter_singleton]
+    decide
+  have newton := mul_esymm_eq_sum (Fin 3) ℂ 2
+  rw [Odd.neg_one_pow (by decide)] at newton
+  simpa [P, S, ↓this, sub_eq_add_neg] using newton
+
+@[deprecated "Only S_three_im is necessary"]
 lemma S_three : S 3 = X (0) * X (1) * X (2) := by
-  simp [S, esymm]
   have : Finset.powersetCard 3 (Finset.univ: Finset (Fin 3)) = {Finset.univ} :=
     Finset.val_eq_singleton_iff.mp rfl
-  simp [this, Fin.prod_univ_three]
+  simp [S, esymm, this, Fin.prod_univ_three]
 
 lemma S_more' (k: ℕ) : S (k + 4) = 0 := by
   have : Finset.powersetCard (k + 4) (Finset.univ : Finset (Fin 3)) = ∅ := by simp
@@ -64,9 +102,6 @@ lemma S_more' (k: ℕ) : S (k + 4) = 0 := by
 lemma S_more (k: ℕ) (h: k > 3) : S k = 0 := by
   have : Finset.powersetCard k (Finset.univ : Finset (Fin 3)) = ∅ := by simp [h]
   simp [S, esymm, this]
-
-lemma movire (n : ℕ) (z : ℂ) : Complex.exp (z * Complex.I) ^ n = Complex.exp (↑n * z * Complex.I) := by
-  simpa [Complex.cos_add_sin_I] using Complex.cos_add_sin_mul_I_pow n z
 
 open Real in
 lemma P_iff {x y z A B C : ℝ} (n: ℕ) : x^n * sin (n*A) + y^n * sin (n*B) + z^n * sin (n*C) = 0
@@ -122,36 +157,34 @@ lemma P_more (k : ℕ) : P (k + 4) = S 1 * P (k + 3) - S 2 * P (k + 2) + S 3 * P
   rw [← prop]
   ring_nf
 
-lemma newton_sum (a b c : ℂ) (k : ℕ) :
-  let p (k: ℕ) := a ^ k + b ^ k + c ^ k
-  let s₁ := a + b + c
-  let s₂ := a * b + b * c + a * c
-  let s₃ := a * b * c
-  p (k + 3) = s₁ * p (k + 2) - s₂ * p (k + 1) + s₃ * p k := by
-  intro p s₁ s₂ s₃
-  let σ := ({a, b, c} : Finset ℂ)
-  let l := [a, b, c]
-  have := psum_eq_mul_esymm_sub_sum (Fin 3) ℂ 3 (by lia)
-  apply_fun (eval (l[·]) ·) at this
-  have psum_3 (f : Fin 3 → ℂ) : eval f (psum (Fin 3) ℂ 3) = (f 0) ^ 3 + (f 1) ^ 3 + (f 2) ^ 3 := by
-    sorry
-  simp only [eval_sub, eval_mul, eval_pow, eval_neg, eval_sum, eval_prod, eval_X, RingHom.map_one, map_natCast] at this
+open Real
 
-  simp only [psum_3] at this
-  simp only [esymm, l, psum] at this
-  -- rw [Finset.Nat.sum_antidiagonal_eq_sum_range_succ] at this
-  simp [Fin.sum_univ_three] at this
-  rw [← Finset.univ_filter_card_eq] at this
+lemma S_one_im (h1: x*sin A + y*sin B + z*sin C = 0) : ((eval <| f x y z A B C) (S 1)).im = 0 := by
+  simp [Fin.sum_univ_three, f, h1]
 
-  have : ∑ x : σ, (x : ℂ) ^ 3 = a ^ 3 + b ^ 3 + c ^ 3 := by calc
-    ∑ x : σ, (x : ℂ) ^ 3
-    _ = a ^ 3 + b ^ 3 + c ^ 3 := by
-      unfold σ
-      simp [Finset.image_image, Function.comp_def]
-      rw [Finset.sum_insert, Finset.sum_insert]
-      simp [← Finset.univ_eq_attach, add_assoc]
-      simp
-  sorry
+lemma P_one_im (h1: x*sin A + y*sin B + z*sin C = 0) : ((eval <| f x y z A B C) (P 1)).im = 0 := by
+  simpa [P] using S_one_im h1
+
+lemma P_two_im (h2: x^2 * sin (2*A) + y^2 * sin (2*B) + z^2 * sin (2*C) = 0) : ((eval <| f x y z A B C) (P 2)).im = 0 := by
+  simp only [P, psum, Fin.sum_univ_three, eval_add, eval_pow, eval_X, f, mul_pow, movire]
+  simp_rw [← Complex.cos_add_sin_I]
+  norm_cast
+  simp [-Complex.ofReal_cos, -Complex.ofReal_sin]
+  norm_cast
+  linarith
+
+lemma S_two_im (h1: x*sin A + y*sin B + z*sin C = 0) (h2: x^2 * sin (2*A) + y^2 * sin (2*B) + z^2 * sin (2*C) = 0) : ((eval <| f x y z A B C) (S 2)).im = 0 := by
+  have : (2 : MvPolynomial (Fin 3) ℂ) = MvPolynomial.C 2 := by rfl
+  have : S 2 = MvPolynomial.C (1 / 2) * (2 * S 2) := by
+    simp [this, ← mul_assoc, ← C_mul]
+  rw [this]
+  simp [-psum_one, -esymm_one, S_two', S_one_im h1, P_two_im h2, P_one_im h1]
+
+lemma S_three_im (habc: ∃ k : ℤ, A + B + C = k * π) : ((eval <| f x y z A B C) (S 3)).im = 0 := by
+  have : Finset.powersetCard 3 (Finset.univ: Finset (Fin 3)) = {Finset.univ} :=
+    Finset.val_eq_singleton_iff.mp rfl
+  simp [S, esymm, this, sum_f_im habc]
+
 section end
 snip end
 
@@ -163,82 +196,17 @@ problem usa1980_p3 (x y z A B C: ℝ) (habc: ∃ k : ℤ, A + B + C = k * π)
   (h1: x*sin A + y*sin B + z*sin C = 0)
   (h2: x^2 * sin (2*A) + y^2 * sin (2*B) + z^2 * sin (2*C) = 0)
   : ∀ n : ℕ, x^n * sin (n*A) + y^n * sin (n*B) + z^n * sin (n*C) = 0 := by
-  have h_Movire := Complex.cos_add_sin_mul_I_pow
-  simp_rw [Complex.cos_add_sin_I] at h_Movire
-  let a := x * Complex.exp (A * Complex.I)
-  let b := y * Complex.exp (B * Complex.I)
-  let c := z * Complex.exp (C * Complex.I)
-  -- We don't actually need to name these
-  let s₁ := a + b + c
-  let s₂ := a * b + b * c + c * a
-  let s₃ := a * b * c
-  lift s₃ to ℝ with s₃' hs₃
-  · rcases habc with ⟨k, h⟩
-    refine Complex.sq_nonneg_iff.mp ?_
-    simp only [s₃, a, b, c]
-    -- need to do this awkward maneuver instead of using `suffices` to avoid invoking LE ℂ
-    have :
-      ↑x * Complex.exp (↑A * Complex.I) * (↑y * Complex.exp (↑B * Complex.I)) * (↑z * Complex.exp (↑C * Complex.I))
-       = ↑x * ↑y * ↑z * (Complex.exp (↑A * Complex.I) * Complex.exp (↑B * Complex.I) * Complex.exp (↑C * Complex.I)) := by field_simp
-    rw [this, mul_pow]
-    norm_cast
-    simp_rw [← Complex.exp_add, ← Complex.exp_nat_mul]
-    rw [Complex.exp_eq_one_iff.mpr ⟨k, ?_⟩]
-    · norm_cast; simpa using sq_nonneg _
-    · simp_rw [← add_mul, ← Complex.ofReal_add, h]
-      push_cast
-      field_simp
-  lift s₁ to ℝ with s₁' hs₁
-  · simp only [s₁, a, b, c]
-    rw [Complex.exp_eq_exp_re_mul_sin_add_cos (A * _),Complex.exp_eq_exp_re_mul_sin_add_cos (B * _), Complex.exp_eq_exp_re_mul_sin_add_cos (C * _)]
-    simp [Complex.sin_ofReal_re, h1]
-  lift a^2 + b^2 + c^2 to ℝ with s₄' hs₄
-  · simp only [a, b, c, mul_pow, h_Movire]
-    simp_rw [← Complex.cos_add_sin_I]
-    norm_cast
-    simp [-Complex.ofReal_cos, -Complex.ofReal_sin]
-    norm_cast
-    linarith
-  lift s₂ to ℝ with s₂' hs₂
-  · suffices h : s₂ = (s₁' ^ 2 - s₄') / 2 by rw [h]; norm_cast
-    simp only [hs₁, s₁, s₂, hs₄]
-    ring_nf
-  let p (k : ℕ) := a ^ k + b ^ k + c ^ k
-  let p' (k : ℕ) : MvPolynomial ({a, b, c}: Finset ℂ) ℂ := X ⟨a, by simp⟩ ^ k
-  let σ := ({a, b, c} : Finset ℂ)
-  -- we have defined p as a function ℂ → ℂ, and want to use the results of rings
-  have m := ({a, b, c} : Multiset ℂ)
-  have := m.esymm 3
-  have sum (i : ℕ) : p (i+3) = s₁ * p (i+2) - s₂ * p (i+1) + s₃ * p i := by
-    -- have := MvPolynomial.sum_antidiagonal_card_esymm_psum_eq_zero ({a, b, c} : Finset ℂ) ℂ
-    -- have := psum_eq_mul_esymm_sub_sum σ ℂ 3 (by lia)
-    -- apply_fun (eval (fun s => ↑s) ·) at this
-    -- simp [esymm, σ, psum] at this
-    -- simp at this
-    -- rw [esymm_eq_multiset_esymm] at this
-
-    -- rw [MvPolynomial.ext_iff] at this
-    -- simp [Finset.sum] at this
-    sorry
-  have h_equiv (n : ℕ) : x^n * sin (n*A) + y^n * sin (n*B) + z^n * sin (n*C) = 0 ↔ (p n).im = 0 := by
-    simp [p, a, b, c, mul_pow, h_Movire]
-    norm_cast at ⊢
-    simp
-    norm_cast
-    simp only [Complex.exp_ofReal_mul_I_im]
+  simp_rw [P_iff]
   intro n
-  induction n using Nat.stepInduction 3 with
+  induction n using Nat.stepInduction 4 with
   | base n hn =>
-    rcases n with (rfl | rfl | rfl | _) <;> try lia
-    simp [↓h_equiv, p]
+    rcases n with (rfl | rfl | rfl | rfl | _) <;> try lia
+    · simp
+    · exact P_one_im h1
+    · exact P_two_im h2
+    · simp [-esymm_one, -psum_one, P_three, S_one_im h1, S_two_im h1 h2, S_three_im habc, P_one_im h1, P_two_im h2]
   | step n ih =>
-    rw [h_equiv]
-    norm_cast at ih
-    simp_rw [h_equiv] at ih
-    -- have := newton_sum a b c n
-    -- simp at this
-    -- simp only [p, this]
-    -- rw [this]
-    simpa [sum n, ← hs₁, ← hs₂, ← hs₃, ih] using Or.inr $ ih 0 (by lia)
+    simp [-esymm_one, -psum_one, P_more, ih, S_one_im h1, S_two_im h1 h2, S_three_im habc]
+
 
 end Usa1980P3
