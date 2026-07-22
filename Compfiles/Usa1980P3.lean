b8e3eeebf28c38db4bb999916317d73f85db70b5
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2026-07-22 12:26:07 +0800

    fix import

--- Compfiles/Usa1980P3.lean
+++ Compfiles/Usa1980P3.lean
@@ -3,7 +3,7 @@ Copyright (c) 2026 pacmanboss256. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Pacmanboss256
 -/
-import Mathlib.Tactic
+import Mathlib
 
 import ProblemExtraction
 
