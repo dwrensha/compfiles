7ed432bcc9b9e0417b8afb1b21377cf9fbdb5c92
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2026-07-25 17:50:10 +0800

    finish up body of theorem

--- Compfiles/Usa2017P5.lean
+++ Compfiles/Usa2017P5.lean
@@ -60,14 +60,14 @@ instance : Coe (ℕ × ℕ) (ℤ × ℤ) := ⟨fun ⟨x, y⟩ => ⟨x, y⟩⟩
 abbrev transpose (p : ℤ × ℤ) {c : ℝ} (l: labelling c) : labelling c := by
   let f (q : ℤ × ℤ) := p + q
   use l ∘ f
+  obtain ⟨l, fin, pos, hdist⟩ := l
   and_intros
-  · have fin := l.2.1
-    rw [Set.finite_iff_bddAbove, bddAbove_def] at fin ⊢
+  · rw [Set.finite_iff_bddAbove, bddAbove_def] at fin ⊢
     aesop
-  · intros; simpa using l.2.2.1 _
+  · intros; simpa using pos _
   · intro p1 p2 ne lbl
     apply_fun f at ne using (by intro _ _; grind : Function.Injective f)
-    simpa [dist, f] using l.2.2.2 ne lbl
+    simpa [dist, f] using hdist ne lbl
 
 abbrev flip (n : ℕ) {c : ℝ} (l: labelling c) : labelling c := by
   let f (p: ℤ × ℤ) := (2 ^ n - 1 - p.1, p.2)
@@ -272,6 +272,210 @@ lemma square_squeeze' (n : ℕ) (l : labelling √2)
       have p₃_label : 2 * (n + 1) < l p₃ := by lia
       use ⟨⟨v₃.1 + x₃, by grind⟩, ⟨v₃.2 + y₃, by grind⟩⟩
 
+def checkerboard : ℤ × ℤ → ℕ
+| (x, y) => if (x + y) % 2 = 1 then 1 else 0
+
+section
+variable  (p₁ p₂ : ℤ × ℤ)
+lemma a (h: checkerboard p₁ = 1) (h2: checkerboard p₁ = checkerboard p₂) : ∃ a b : ℤ, p₁ = (p₂.1 + a + b, p₂.2 + a - b) := by
+  obtain ⟨x₁, y₁⟩ := p₁
+  obtain ⟨x₂, y₂⟩ := p₂
+  simp [h] at h2
+  simp [checkerboard] at h h2
+  rw [← Int.odd_iff] at h h2
+  rw [odd_iff_exists_bit1] at h h2
+  rcases h with ⟨a, ha⟩
+  rcases h2 with ⟨b, hb⟩
+  use a - b
+  use (x₁ - y₁ + x₂ - y₂) / 2
+  simp
+  constructor
+  sorry
+
+example (h1 : p₁ ≠ p₂) (h: checkerboard p₁ = 1) (h2 : checkerboard p₁ = checkerboard p₂) : √2 ^ 1 ≤ dist p₁ p₂ := by
+  rcases a p₁ p₂ h h2 with ⟨a, b, rfl⟩
+  rw [dist]
+  push_cast
+  ring_nf
+  refine sqrt_le_sqrt ?_
+  suffices h : |a| ≠ 0 ∨ |b| ≠ 0 by
+    norm_cast
+    simp only [ne_eq, abs_eq_zero, ← sq_pos_iff] at h
+    rcases h with h | h
+    all_goals nlinarith
+  grind
+end
+
+lemma dist_ne {p₁ p₂ : ℤ × ℤ} (h : p₁ ≠ p₂) : 1 ≤ dist p₁ p₂ := by
+  obtain ⟨x₁, y₁⟩ := p₁
+  obtain ⟨x₂, y₂⟩ := p₂
+  rw [dist]
+  refine one_le_sqrt.mpr ?_
+  norm_cast
+  simp at h
+  by_cases h2 : x₁ = x₂
+  · simp [h2]
+    grind
+  · rw [← sq_abs]
+    have : 1 ≤ |x₂ - x₁| ^ 2 := by refine one_le_pow₀ (by grind)
+    nlinarith
+
+lemma dist_scale {a b c d : ℤ} : dist (a * 2, b * 2) (c * 2, d * 2) = dist (a, b) (c, d) * 2 := by
+  simp_rw [dist]
+  norm_cast
+  simp_rw [← sub_mul, mul_pow, ← add_mul, Int.cast_mul]
+  push_cast
+  rw [sqrt_mul (by nlinarith)]
+  ring
+
+-- We take care of 3/4 of the space in one iteration, removing the need to fiddle with the parity of being aligned with the grid
+-- this does mean that we need an N twice as large
+def ply : ℤ × ℤ → ℕ
+| (a, b) => by
+  if Odd (a + b) then exact 1
+  else if Odd a ∧ Odd b then exact 2
+  else exact 0
+
+example (a b: ℤ) (h: ply (a, b) = 0) : Even (a + b) := by
+  simp [ply, ite_eq_iff] at h
+  sorry
+
+def labeltest : ℕ → ℤ × ℤ → ℕ
+| 0, _ => 1
+| limit + 1, (a, b) =>
+  if Odd (a + b) then 1
+  else if Odd a ∧ Odd b then 2
+  else 2 + labeltest limit (a / 2, b / 2)
+
+#eval (List.range 30) |>.map (List.range 30 |>.map fun x => labeltest 2 ⟨x, ·⟩)
+
+lemma label_max (limit : ℕ) (p : ℤ × ℤ) : labeltest limit p ≤ 2 * limit + 1 := by
+  induction limit generalizing p with
+  | zero => simp [labeltest]
+  | succ limit ih =>
+    rw [labeltest]
+    repeat (split; lia)
+    specialize ih (p.1 / 2, p.2 / 2)
+    lia
+
+lemma label_finite (limit : ℕ) : (Set.range <| labeltest limit).Finite := by
+  simp_rw [Set.finite_iff_bddAbove, bddAbove_def, Set.mem_range]
+  use 2 * limit + 1
+  rintro _ ⟨p, rfl⟩
+  exact label_max _ _
+
+lemma label_pos (limit : ℕ) (p : ℤ × ℤ) : 0 < labeltest limit p := by
+  induction limit
+  · simp [labeltest]
+  · rw [labeltest]
+    lia
+
+variable (limit : ℕ) (p : ℤ × ℤ)
+lemma label_cases : labeltest (limit + 1) p = 1 ∨ labeltest (limit + 1) p = 2 ∨ labeltest (limit + 1) p = 2 + labeltest limit (p.1 / 2, p.2 / 2) := by
+  obtain ⟨a, b⟩ := p
+  if h : Odd (a + b) then simp [h, labeltest]
+  else if h': Odd a ∧ Odd b then grind [labeltest]
+  else simp [h, h', labeltest]
+
+lemma label_one_iff : labeltest (limit + 1) p = 1 ↔ Odd (p.1 + p.2) := by
+  grind [labeltest]
+
+lemma label_two_iff : labeltest (limit + 1) p = 2 ↔ Odd p.1 ∧ Odd p.2 := by
+  grind [labeltest, label_pos]
+
+lemma label_more_iff : labeltest (limit + 1) p = 2 + labeltest limit (p.1 / 2, p.2 / 2) ↔ Even p.1 ∧ Even p.2 := by
+  grind [labeltest, label_pos]
+
+-- lemma label_more_iff' : labeltest (limit + 1) (a, b) = 2 + labeltest limit (p.1 / 2, p.2 / 2) ↔ Even p.1 ∧ Even p.2 := by
+--   grind [labeltest, label_pos]
+
+lemma label_max_iff (h : labeltest limit p = 2 * limit + 1) : ∃ a b : ℤ, p = (a * 2 ^ limit, b * 2 ^ limit) := by
+  induction limit generalizing p with
+  | zero => exact ⟨p.1, p.2, by simp⟩
+  | succ limit ih =>
+    have ⟨a, b, hab⟩ := ih (p.1 / 2, p.2 / 2) (by simp [labeltest] at h; lia)
+    refine ⟨a, b, by grind [labeltest]⟩
+
+lemma label_congr {limit : ℕ} {p₁ p₂ : ℤ × ℤ} (h : labeltest (limit + 1) p₁ = labeltest (limit + 1) p₂)
+  : (Odd (p₁.1 + p₁.2) ∧ Odd (p₂.1 + p₂.2))
+    ∨ (Odd p₁.1 ∧ Odd p₁.2 ∧ Odd p₂.1 ∧ Odd p₂.2)
+    ∨ (Even p₁.1 ∧ Even p₁.2 ∧ Even p₂.1 ∧ Even p₂.2) := by
+  grind [labeltest, label_pos]
+
+lemma label_dist (limit v : ℕ) (p₁ p₂ : ℤ × ℤ) (h_ne : p₁ ≠ p₂)
+  (hp₁ : labeltest limit p₁ = v) (hp₂ : labeltest limit p₂ = v)
+  (hv : v ≠ 2 * limit + 1) : √2 ^ (labeltest limit p₁) ≤ dist p₁ p₂ := by
+  induction limit generalizing v p₁ p₂ with
+  | zero => grind [labeltest]
+  | succ limit ih =>
+    rcases label_congr <| hp₁.trans hp₂.symm with ⟨h₁, h₂⟩ | ⟨h₁, h₂, h₃, h₄⟩ | ⟨h₁, h₂, h₃, h₄⟩
+    · rw [label_one_iff _ _ |>.mpr h₁]
+      rw [pow_one, dist, sqrt_le_iff]
+      refine ⟨sqrt_nonneg _, ?_⟩
+      rw [sq_sqrt (by nlinarith)]
+      norm_cast
+      by_cases h : p₁.1 ≠ p₂.1 ∧ p₁.2 ≠ p₂.2
+      · have : 1 ≤ (p₂.1 - p₁.1) ^ 2 := by
+          rw [← sq_abs]
+          exact one_le_pow₀ (by grind)
+        have : 1 ≤ (p₂.2 - p₁.2) ^ 2 := by
+          rw [← sq_abs]
+          exact one_le_pow₀ (by grind)
+        linarith
+      · push +distrib Not at h
+        rcases h with h | h
+        · rw [h] at h₁ ⊢
+          have : 2 ≤ |p₂.2 - p₁.2| := by grind
+          have : 2 ≤ |p₂.2 - p₁.2| ^ 2 := by nlinarith
+          nth_rw 2 [← sq_abs]
+          linarith
+        · rw [h] at h₁ ⊢
+          have : 2 ≤ |p₂.1 - p₁.1| := by grind
+          have : 2 ≤ |p₂.1 - p₁.1| ^ 2 := by nlinarith
+          rw [← sq_abs]
+          linarith
+    · rw [label_two_iff _ _ |>.mpr ⟨h₁, h₂⟩, dist]
+      simp
+      rw [le_sqrt' zero_lt_two]
+      norm_cast
+      simp
+
+
+
+      sorry
+    · rw [label_more_iff _ _ |>.mpr ⟨h₁, h₂⟩]
+      have hp1 := label_more_iff limit p₁ |>.mpr ⟨h₁, h₂⟩
+      have hp2 := label_more_iff limit p₂ |>.mpr ⟨h₃, h₄⟩
+      specialize ih (labeltest limit (p₁.1 / 2, p₁.2 / 2)) (p₁.1 / 2, p₁.2 / 2) (p₂.1 / 2, p₂.2 / 2) (by grind) rfl (by lia) (by lia)
+      ring_nf
+      rw [sq_sqrt zero_le_two]
+      rw [dist] at ih
+      push_cast [
+        Int.cast_div_charZero <| even_iff_two_dvd.mp h₁,
+        Int.cast_div_charZero <| even_iff_two_dvd.mp h₂,
+        Int.cast_div_charZero <| even_iff_two_dvd.mp h₃,
+        Int.cast_div_charZero <| even_iff_two_dvd.mp h₄] at ih
+      simp_rw [div_sub_div_same, div_pow, ← add_div] at ih
+      rw [sqrt_div (by nlinarith), sqrt_sq (by linarith)] at ih
+      grind [dist]
+
+lemma label_dist_max (limit v : ℕ) (p₁ p₂ : ℤ × ℤ) (h : p₁ ≠ p₂)
+  (hp₁ : labeltest limit p₁ = 2 * limit + 1) (hp₂ : labeltest limit p₂ = 2 * limit + 1)
+  : √2 ^ (2 * limit) ≤ dist p₁ p₂ := by
+  induction limit generalizing p₁ p₂ with
+  | zero => simp [dist_ne h]
+  | succ limit ih =>
+    obtain ⟨a1, b1, rfl⟩ := label_max_iff (limit + 1) p₁ hp₁
+    obtain ⟨a2, b2, rfl⟩ := label_max_iff (limit + 1) p₂ hp₂
+    simp_rw [pow_succ, ← mul_assoc]
+    rw [dist_scale]
+    rw [Nat.mul_add_one, pow_add]
+    rw [sq_sqrt zero_le_two]
+    rw [mul_le_mul_iff_left₀ zero_lt_two]
+    specialize ih (a1 * 2 ^ limit, b1 * 2 ^ limit) (a2 * 2 ^ limit, b2 * 2 ^ limit) (by grind)
+
+    sorry
+
 snip end
 
 problem usa2017_p5 (c : ℝ) :
@@ -288,7 +492,7 @@ problem usa2017_p5 (c : ℝ) :
   · intro c_lt
     -- calculate an upper bound
     have ⟨n, hn⟩ : ∃ n : ℕ, n > (Real.log √2) / (Real.log √2 - Real.log c) := exists_nat_gt _
-    have : c ^ n < √2 ^ ((n: ℤ) - 1) := by
+    have c_bound : c ^ n < √2 ^ ((n: ℤ) - 1) := by
       rw [zpow_natCast_sub_one₀ (by simp), Real.pow_lt_iff_lt_log c_pos (by simp)]
       rw [log_div (by simp) (by simp), log_pow, lt_sub_comm, ← mul_sub, ← div_lt_iff₀ ?_]
       · norm_cast
@@ -297,9 +501,30 @@ problem usa2017_p5 (c : ℝ) :
         field_simp
         exact c_lt
     -- the construction here is basically a truncation of an infinite one,
-    -- use Nat.findGreatest?
-
-    sorry
+    refine ⟨labeltest (n+1), label_finite _, label_pos _, ?_⟩
+    intro p₁ p₂ h_ne h_label
+    have := label_max (n+1) p₁
+    rcases this.eq_or_lt with eq | lt
+    · have := label_dist_max (n+1) (labeltest (n+1) p₁) p₁ p₂ h_ne eq (h_label.symm.trans eq)
+      rw [eq]
+      -- this is a bit contrived because we used the tight bound from the solution
+      have := calc c ^ (2 * (n + 1) + 1)
+        _ = (c ^ n) ^ 2 * c ^ 3 := by ring
+        _ < (√2 ^ ((n: ℤ) - 1)) ^ (2 : ℤ) * √2 ^ 3 := by
+          refine mul_lt_mul_of_nonneg_of_pos ?_ ?_ (sq_nonneg _) (by simp)
+          · exact pow_lt_pow_left₀ c_bound (pow_nonneg c_pos.le _) (by decide)
+          · exact pow_le_pow_left₀ c_pos.le c_lt.le _
+        _ = √2 ^ (2 * ((n : ℤ) - 1) + 3) := by
+          rw [← zpow_mul, ← zpow_natCast, ← zpow_add' (by lia)]
+          ring_nf
+        _ < √2 ^ (2 * (n + 1) : ℤ) := zpow_lt_zpow_iff_right₀ one_lt_sqrt_two |>.mpr (by linarith)
+        _ ≤ dist p₁ p₂ := by norm_cast
+      exact this.le
+    · have := label_dist (n+1) (labeltest (n+1) p₁) p₁ p₂ h_ne rfl h_label.symm lt.ne
+      calc
+        c ^ labeltest (n + 1) p₁
+        _ ≤ √2 ^ labeltest (n + 1) p₁ := pow_le_pow_iff_left₀ c_pos.le (sqrt_nonneg _) (label_pos _ _ |>.ne.symm) |>.mpr c_lt.le
+        _ ≤ dist p₁ p₂ := this
   · contrapose
     -- Any counterexample we have for c = √2 works for c > √2 as well
     wlog hc : √2 = c with rest
