991c61720c2fc4c712837167e83bd0631887e52e
pacmanboss256 <pacmanboss972@gmail.com>
2026-07-28 13:32:23 -0500

    add module to new proof

--- Compfiles/Usa2016P4.lean
+++ Compfiles/Usa2016P4.lean
@@ -4,9 +4,14 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Pacmanboss256
 -/
 
-import Mathlib.Tactic
-import ProblemExtraction
+module
+
+public import Mathlib.Tactic
+
+public import ProblemExtraction
 import Mathlib.Analysis.SpecialFunctions.Log.Base
+
+@[expose] public section
 
 /-!
 # USA Mathematical Olympiad 2016 P4
