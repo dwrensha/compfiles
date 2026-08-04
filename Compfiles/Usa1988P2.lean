a9984811dc3ec724955ddd43a191b3a6e2b7182b
pacmanboss256 <61611109+pacmanboss256@users.noreply.github.com>
2026-08-04 11:58:02 -0500

    Update Compfiles/Usa1988P2.lean
    Co-authored-by: Hillosanation <88579655+Hillosanation@users.noreply.github.com>

--- Compfiles/Usa1988P2.lean
+++ Compfiles/Usa1988P2.lean
@@ -62,25 +62,8 @@ problem usa1988_p2 (a b c r s t k : ℝ) (hk : k = a^2 - 3*b)(hpol: X^3 + C a * 
   constructor
   · linarith
   rw [hk]
-  ring_nf
-  move_add [← r^2]
-  rw [sub_eq_add_neg, sub_eq_add_neg]
-  repeat rw [add_assoc]
-  rw [add_le_add_iff_left]
-  ring_nf
-  rw [add_comm, neg_add_eq_sub]
-  nth_rw 3 [sub_eq_add_neg]
-  rw [add_le_add_iff_left, mul_two]
-  simp
-  rw [sub_eq_add_neg, sub_eq_add_neg, add_comm]
-  rw [show (r * t + (-(r * s) + -(r * t) + -(s * t) + s ^ 2) = s^2 -(r * s) - (s * t)) by ring_nf]
-  rw [← add_le_add_iff_right (r*t), neg_add_cancel, ← neg_le_neg_iff, neg_zero]
-  rw [show (-(s ^ 2 - r * s - s * t + r * t) = (r-s)*(s-t)) by ring_nf]
-  rw [mul_nonneg_iff]
-  left
-  constructor
-  · linarith
-  linarith
+  suffices h : 0 ≤ (r-s)*(s-t) by linear_combination h
+  refine mul_nonneg ?_ ?_ <;> linarith
 
 
 end USA1988P2
