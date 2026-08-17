23897b22bb446c88395d04374b4def8bcc40911e
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2026-08-17 17:12:44 +0800

    scope out needed lemmas

--- Compfiles/Usa2026P4.lean
+++ Compfiles/Usa2026P4.lean
@@ -39,26 +39,25 @@ def is_solitary (n : ℕ) : Prop :=
 snip begin
 -- We follow Solution 1 by sillybone and Hpenguin from Art of Problem Solving.
 
-lemma not_solitary_zero : ¬ is_solitary 0 := by simp [is_solitary]
+-- lemma not_solitary_zero : ¬ is_solitary 0 := by simp [is_solitary]
 
 lemma not_solitary_two_zeros {k : ℕ} : ¬ is_solitary (2 * 10 ^ k) := by
   rw [is_solitary]
   push Not
   intro _
   use (2 * 10 ^ k), 0
-  constructor
-  rfl
-  constructor
-  induction k with
-  | zero => simp [has_digit_one]
-  | succ k ih =>
-    specialize ih (by omega)
-    simp [has_digit_one, Nat.mul_mod, Nat.pow_mod]
-    rw [Nat.pow_add_one 10 k]
-    rw [Nat.mul_div_assoc 2 (Nat.dvd_mul_left _ _)]
-    rw [Nat.mul_div_left (10 ^ k) (by decide)]
-    rwa [has_digit_one] at ih
-  simp [has_digit_one]
+  and_intros
+  · rfl
+  · induction k with
+    | zero => simp [has_digit_one]
+    | succ k ih =>
+      specialize ih (by omega)
+      simp [has_digit_one, Nat.mul_mod, Nat.pow_mod]
+      rw [Nat.pow_add_one 10 k]
+      rw [Nat.mul_div_assoc 2 (Nat.dvd_mul_left _ _)]
+      rw [Nat.mul_div_left (10 ^ k) (by decide)]
+      rwa [has_digit_one] at ih
+  · simp [has_digit_one]
 
 lemma digit_squeeze {m k : ℕ} (hm : m ∈ Set.Ico (10 ^ k) (2 * 10 ^ k)) : 1 ∈ Nat.digits 10 m := by
   induction k generalizing m with
@@ -112,45 +111,43 @@ lemma digits_last_mem {m k : ℕ} (h1 : 0 < m) (h2 : 10 ^ k ≤ m) (h3 : m / 10 
 --     rw [List.append_left_inj]
 --     sorry
 
--- lemma digits_div_subset (a k : ℕ) : Nat.digits 10 (a % 10 ^ (k + 1)) ⊆ Nat.digits 10 a := by
---   wlog! h : a % 10 ^ (k + 1) ≠ 0
---   · simp [h]
---   induction k with
---   | zero =>
---     rw [Nat.digits_def' (by decide) (by omega)]
---     simp
---     rw [Nat.digits_def' (by decide) (by omega)]
---     exact List.mem_cons_self
---   | succ k ih =>
---     specialize ih sorry
---     rw [Nat.digits_def' (by decide)]
---     refine List.cons_subset.mpr ⟨?_, ?_⟩
---     rw [Nat.mod_mod_of_dvd]
+lemma digits_div_subset (a k : ℕ) : Nat.digits 10 (a % 10 ^ (k + 1)) ⊆ Nat.digits 10 a := by
+  wlog! h : a % 10 ^ (k + 1) ≠ 0
+  · simp [h]
+  induction k with
+  | zero =>
+    rw [Nat.digits_def' (by decide) (by omega)]
+    simp
+    rw [Nat.digits_def' (by decide) (by omega)]
+    exact List.mem_cons_self
+  | succ k ih =>
+    specialize ih sorry
+    rw [Nat.digits_def' (by decide)]
+    refine List.cons_subset.mpr ⟨?_, ?_⟩
+    rw [Nat.mod_mod_of_dvd]
 
---     rw [Nat.digits_def' (by decide)]
---     · exact List.mem_cons_self
---     · rw [Nat.ne_zero_iff_zero_lt] at h
---       cases a
---       · simp at h
---       · simp
---     · exact dvd_of_mul_left_eq _ rfl
+    rw [Nat.digits_def' (by decide)]
+    · exact List.mem_cons_self
+    · rw [Nat.ne_zero_iff_zero_lt] at h
+      cases a
+      · simp at h
+      · simp
+    · exact dvd_of_mul_left_eq _ rfl
 
---     nth_rw 2 [Nat.digits_def' (by decide)]
---     refine List.subset_cons_of_subset (a % 10) ?_
+    nth_rw 2 [Nat.digits_def' (by decide)]
+    refine List.subset_cons_of_subset (a % 10) ?_
 
---     convert ih
---     rw [@Nat.mod_pow_succ]
---     rw [Nat.digits_def' (by decide)] at ih
---     all_goals sorry
+    convert ih
+    rw [@Nat.mod_pow_succ]
+    rw [Nat.digits_def' (by decide)] at ih
+    all_goals sorry
 
 lemma solitary_form (k : ℕ) : is_solitary (2 * 10 ^ k - 1) := by
   refine ⟨by grind, fun a b h => ?_⟩
   wlog! w : b ≤ a
   · exact this k b a (by rwa [add_comm]) w.le |>.symm
-  have h1 : 10 ^ k ≤ a := by omega
   have : a ≤ 2 * 10 ^ k - 1 := by omega
-  have h2 : a < 2 * 10 ^ k := (Nat.le_sub_one_iff_lt <| Nat.pos_of_neZero _).mp this
-  exact Or.inl <| digit_squeeze ⟨h1, h2⟩
+  exact Or.inl <| digit_squeeze ⟨by omega, (Nat.le_sub_one_iff_lt <| Nat.pos_of_neZero _).mp (this)⟩
 
 lemma solitary_one : is_solitary 1 := solitary_form 0
 
@@ -216,23 +213,94 @@ lemma solitary_extend (n k : ℕ) (hn1 : 0 < n) (hn2 : n < 10 ^ k) : is_solitary
       · rw [← Nat.one_le_div_iff (Nat.zero_lt_of_lt hn2), hxdef]
     · omega
 
-lemma nines (k a : ℕ) : a ∈ Nat.digits 10 ((10 ^ (k + 1)) - 1) ↔ a = 9 := by
+-- lemma nines (k a : ℕ) : a ∈ Nat.digits 10 ((10 ^ (k + 1)) - 1) ↔ a = 9 := by
+--   induction k with
+--   | zero => simp
+--   | succ k ih =>
+--     rw [Nat.digits_def' (by decide) (by grind)]
+--     have (n : ℕ) : 9 = (10 ^ (n + 1 + 1) - 1) % 10 := by
+--       rw [← Nat.add_mod_right, ← Nat.sub_add_comm <| Nat.one_le_pow' _ _]
+--       simp [Nat.add_mod, Nat.pow_mod]
+--     constructor
+--     · rw [List.mem_cons]
+--       rintro (h | h)
+--       · rwa [this]
+--       · have (n : ℕ) : (10 ^ (n + 1 + 1) - 1) / 10 = 10 ^ (n + 1) - 1 := by omega
+--         rw [this] at h
+--         exact ih.mp h
+--     · rintro rfl
+--       refine List.mem_cons.mpr <| Or.inl <| this _
+
+lemma nines' (k : ℕ) : 10 ^ k - 1 = Nat.ofDigits 10 (List.replicate k 9) := by
   induction k with
   | zero => simp
   | succ k ih =>
-    rw [Nat.digits_def' (by decide) (by grind)]
-    have (n : ℕ) : 9 = (10 ^ (n + 1 + 1) - 1) % 10 := by
-      rw [← Nat.add_mod_right, ← Nat.sub_add_comm <| Nat.one_le_pow' _ _]
-      simp [Nat.add_mod, Nat.pow_mod]
-    constructor
-    · rw [List.mem_cons]
-      rintro (h | h)
-      · rwa [this]
-      · have (n : ℕ) : (10 ^ (n + 1 + 1) - 1) / 10 = 10 ^ (n + 1) - 1 := by omega
-        rw [this] at h
-        exact ih.mp h
-    · rintro rfl
-      refine List.mem_cons.mpr <| Or.inl <| this _
+    rw [Nat.add_comm k 1, List.replicate_add, List.replicate_one, Nat.ofDigits_append]
+    simp [← ih]
+    grind
+
+lemma chop_off_leading (m k : ℕ) (hm : m ∈ Finset.Ico (10 ^ k) (10 ^ (k + 1)))
+    : m = Nat.ofDigits 10 ((Nat.digits 10 m).dropLast ++ [m / 10 ^ k]) := by
+  rw [Finset.mem_Ico] at hm
+  induction k generalizing m with
+  | zero =>
+    rw [Nat.digits_of_lt 10 m (by omega) (by omega)]
+    simp
+  | succ k ih =>
+    rw [Nat.digits_def' (by decide) (by omega)]
+    rw [List.dropLast_cons_of_ne_nil ?_]
+    on_goal 2 =>
+      refine Nat.digits_ne_nil_iff_ne_zero.mpr ?_
+      omega
+    rw [List.cons_append]
+    rw [Nat.ofDigits_cons]
+    specialize ih (m / 10) (by omega)
+    rw [Nat.div_div_eq_div_mul, ← Nat.pow_add_one'] at ih
+    rw [← ih]
+    omega
+
+abbrev correction (m k : ℕ) :=
+  Nat.digits 10 (m - (10 ^ k - 1)) |>.take k |>.map (if · = 1 then 1 else 0) |> Nat.ofDigits 10
+
+lemma correction_le {m k : ℕ} (hm : m ∈ Finset.Ico (10 ^ k) (10 ^ (k + 1))) : correction m k ≤ 10 ^ k - 1 := by
+  cases k with
+  | zero =>
+    rw [Finset.mem_Ico] at hm
+    rw [correction, Nat.digits_of_lt]
+    · simp
+    · omega
+    · omega
+  | succ k =>
+    rw [Nat.le_sub_one_iff_lt <| Nat.pos_of_neZero _]
+    calc correction m (k + 1)
+      _ < 10 ^ _ :=
+        Nat.ofDigits_lt_base_pow_length (by decide) (by grind)
+      _ ≤ 10 ^ (k + 1) := by
+        rw [Nat.pow_le_pow_iff_right (by decide), List.length_map]
+        apply List.length_take_le
+
+lemma nines_sub_correction (m k : ℕ) : Nat.ofDigits 10 (List.replicate k 9) - correction m k =
+    (Nat.digits 10 (m - (10 ^ k - 1)) |>.take k |>.map (if · = 1 then 8 else 9) |> Nat.ofDigits 10) := by
+  sorry
+
+lemma nines_sub_correction_not_has_digit_one (m k : ℕ) : ¬has_digit_one (10 ^ k - 1 - correction m k) := by
+  cases k with
+  | zero => simp [has_digit_one]
+  | succ k =>
+    rw [nines', nines_sub_correction]
+    simp [has_digit_one]
+    rw [Nat.digits_ofDigits _ (by decide)]
+    · suffices 1 ∉ List.map (fun x ↦ if x = 1 then 8 else 9) (Nat.digits 10 (m - (10 ^ (k + 1) - 1))) by
+        contrapose this
+        exact List.mem_of_mem_take this
+      simp [List.mem_map, ite_eq_iff]
+    · grind
+    intro h
+    rw [List.getLast_take h]
+    simp [ite_eq_iff]
+
+lemma add_correction_not_has_digit_one (m k : ℕ) : ¬has_digit_one (m - (10 ^ k - 1) + correction m k) := by
+  sorry
 
 lemma solitary_iff {m k : ℕ} (hm : m ∈ Finset.Ico (10 ^ k) (10 ^ (k + 1))) :
     is_solitary m ↔ m = 2 * 10 ^ k - 1 ∨ (∃ n ∈ Finset.Ico 1 (10 ^ k), is_solitary n ∧ m = 2 * 10 ^ k + n) := by
@@ -272,16 +340,19 @@ lemma solitary_iff {m k : ℕ} (hm : m ∈ Finset.Ico (10 ^ k) (10 ^ (k + 1))) :
         zify [h1, h2] at this ⊢
         omega
       · omega
-    set correction := Nat.digits 10 (m - (10 ^ k - 1)) |>.map (if · = 1 then 1 else 0) |> Nat.ofDigits 10 with hc
+    -- nudge all ones in b by taking from a
+    set corr := correction m k with hc
     absurd solitary
     unfold is_solitary
     push Not
     intro _
-    use 10 ^ k - 1 - correction, m - (10 ^ k - 1) + correction
-    constructor
-    rw [Nat.add_left_comm, Nat.sub_add_cancel ?_, Nat.sub_add_cancel (by omega)]
-    sorry
-    sorry
+    use 10 ^ k - 1 - corr, m - (10 ^ k - 1) + corr
+    and_intros
+    · rw [Nat.add_left_comm, Nat.sub_add_cancel ?_, Nat.sub_add_cancel (by omega)]
+      rw [← Finset.mem_Ico] at hm
+      exact correction_le hm
+    · exact nines_sub_correction_not_has_digit_one m k
+    · exact add_correction_not_has_digit_one m k
   · simp only [Finset.mem_Ico]
     rintro (rfl | ⟨n, ⟨npos, hnk⟩, hn, rfl⟩)
     · exact solitary_form k
